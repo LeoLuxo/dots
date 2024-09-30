15c5004d5a08ae6dc7cf9f090d88e598e3bfbb5e
Chloé G <contact@leoluxo.eu>
2024-09-30 13:41:07 +0200

    15 current  2024-09-30 13:41:03  24.11.20240926.1925c60   6.10.5                          *

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -1,20 +1,14 @@
 #!/usr/bin/env bash
-#
-# I believe there are a few ways to do this:
-#
-#    1. My current way, using a minimal /etc/nixos/configuration.nix that just imports my config from my home directory (see it in the gist)
-#    2. Symlinking to your own configuration.nix in your home directory (I think I tried and abandoned this and links made relative paths weird)
-#    3. My new favourite way: as @clot27 says, you can provide nixos-rebuild with a path to the config, allowing it to be entirely inside your dotfies, with zero bootstrapping of files required.
-#       `nixos-rebuild switch -I nixos-config=path/to/configuration.nix`
-#    4. If you uses a flake as your primary config, you can specify a path to `configuration.nix` in it and then `nixos-rebuild switch —flake` path/to/directory
-# As I hope was clear from the video, I am new to nixos, and there may be other, better, options, in which case I'd love to know them! (I'll update the gist if so)
+
+# Original script by 0atman / No boilerplate
+# https://gist.github.com/0atman/1a5133b842f929ba4c1e195ee67599d5
 
 sudo echo "Running as sudo"
 
 # A rebuild script that commits on a successful build
 set -e
 
-# cd to your config dir
+# cd to our config dir
 pushd ~/dots/ &>/dev/null
 
 # Early return if no changes were detected (thanks @singiamtel!)
@@ -24,28 +18,33 @@ if git diff --quiet .; then
     exit 0
 fi
 
-# Autoformat your nix files
+# Autoformat nix files
 alejandra . &>/dev/null \
   || ( alejandra . ; echo "formatting failed!" && exit 1)
 
-# Shows your changes
+# Shows changes
 git diff -U0 .
 
 echo "NixOS Rebuilding..."
 
-# Rebuild, output simplified errors, log trackebacks
-sudo nixos-rebuild switch --flake .#$USER &>rebuild.log \
+
+# For some reason nix can't see non-git added files
+git add .
+
+# Rebuild and log everything to a file
+# On error, output simplified errors
+sudo nixos-rebuild switch --show-trace --flake .#$USER &>rebuild.log \
   || (cat rebuild.log | grep --color error && exit 1)
 
 # Get current generation metadata
 current=$(nixos-rebuild list-generations | grep current)
 
 # Commit all changes witih the generation metadata
-git add .
 git commit -am "$current"
+git push
 
 
-# Back to where you were
+# Back to where we were
 popd &>/dev/null
 
 # Notify all OK!
