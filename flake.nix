87dbb653bf3ff75a94eb75ebd5ba3faefec67f0c
Chloé G <contact@chlookie.net>
2024-09-30 12:27:23 +0200

    12 current  2024-09-30 12:27:19  24.11.20240926.1925c60   6.10.5                          *

--- flake.nix
+++ flake.nix
@@ -5,20 +5,36 @@
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    nixos-hardware .      url = "github:nixos/nixos-hardware/master";
+    nixos-hardware.url = "github:nixos/nixos-hardware/master";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nxpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     nixpkgs,
     nixos-hardware,
+    home-manager,
     ...
   }: {
-    nixosConfigurations."pancake" = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./configuration.nix
-        nixos-hardware.nixosModules.microsoft-surface-pro-intel
-      ];
+    nixosConfigurations = {
+      "pancake" = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          nixos-hardware.nixosModules.microsoft-surface-pro-intel
+
+          ./configuration.nix
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.pancake = import ./home.nix;
+          }
+        ];
+      };
     };
   };
 }
