bc0f0091a6a8c1d2a82d375ed0bd1ed59dd8b596
LeoLuxo <leo@gamboa.eu>
2024-09-30 13:58:13 +0200

    16 current  2024-09-30 13:58:09  24.11.20240926.1925c60   6.10.5                          *

--- flake.nix
+++ flake.nix
@@ -13,27 +13,14 @@
     };
   };
 
-  outputs = {
-    nixpkgs,
-    nixos-hardware,
-    home-manager,
-    ...
-  }: {
+  outputs = {nixpkgs, ...} @ inputs: {
     nixosConfigurations = {
       "pancake" = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
-          nixos-hardware.nixosModules.microsoft-surface-pro-intel
-
-          ./hosts/pancake/configuration.nix
-
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.pancake = import ./hosts/pancake/home.nix;
-          }
+          ./hosts/pancake
         ];
+        specialArgs = inputs;
       };
     };
   };
