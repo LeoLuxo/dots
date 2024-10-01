fd9617008a34681fc4a322a4c6efec6b2912adf8
Chloé G <contact@leoluxo.eu>
2024-10-01 13:01:57 +0200

    37 current  2024-10-01 13:01:52  24.11.20240926.1925c60   6.10.5                          *

--- hosts/pancake/home.nix
+++ hosts/pancake/home.nix
@@ -141,39 +141,46 @@
 
   };
 
-  # dconf.settings = {
-  #   "/org/gnome/desktop/peripherals/touchpad" = {
-  #     natural-scroll = true;
-  #   };
+  dconf.settings = {
+    "org/gnome/desktop/peripherals/touchpad" = {
+      natural-scroll = true;
+      two-finger-scrolling-enabled = true;
+    };
 
-  #   "/org/gnome/desktop/interface/enable-hot-corners" = false;
+    "org/gnome/desktop/interface" = {
+      enable-hot-corners = false;
+    };
 
-  #   "/org/gnome/mutter/" = {
-  #     edge-tiling = true;
-  #     dynamic-workspaces = true;
-  #   };
+    "org/gnome/mutter" = {
+      edge-tiling = true;
+      dynamic-workspaces = true;
+    };
 
-  #   "org/gnome/settings-daemon/plugins/media-keys" = {
-  #     custom-keybindings = [
-  #       "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/terminal/"
-  #       "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/screenshot/"
-  #     ];
-  #   };
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+      custom-keybindings = [
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/terminal/"
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/screenshot/"
+      ];
+    };
 
-  #   "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/terminal" = {
-  #     binding = "<Super>t";
-  #     command = "kgx";
-  #     name = "GNOME Console";
-  #   };
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/terminal" = {
+      binding = "<Super>t";
+      command = "kgx";
+      name = "GNOME Console";
+    };
 
-  #   "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/screenshot" = {
-  #     binding = "<Super>s";
-  #     command = "gnome-screenshot --area --clipboard -f /dev/null";
-  #     name = "Instant screenshot";
-  #   };
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/screenshot" = {
+      binding = "<Super>s";
+      command = "gnome-screenshot --area --clipboard -f /dev/null";
+      name = "Instant screenshot";
+    };
 
-  #   "org/gnome/shell/window-switcher" = {
-  #     current-workspace-only = true;
-  #   };
-  # };
+    "org/gnome/shell/window-switcher" = {
+      current-workspace-only = true;
+    };
+
+    "org/gnome/desktop/sound" = {
+      event-sounds = false;
+    };
+  };
 }
