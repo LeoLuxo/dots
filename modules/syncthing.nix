8b912117a727b36a9684cd5f06ea9beafec496a5
Chloé G <contact@leoluxo.eu>
2024-10-01 20:22:10 +0200

    47 current  2024-10-01 20:22:03  24.11.20240926.1925c60   6.10.5

---
+++ modules/syncthing.nix
@@ -0,0 +1,9 @@
+{ ... }:
+{
+  # Move to home-manager when the PR gets merged
+  # https://github.com/nix-community/home-manager/pull/5616
+  services.syncthing = {
+    enable = true;
+    user = "pancake";
+  };
+}
