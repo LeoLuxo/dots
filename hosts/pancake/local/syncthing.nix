3a027801a28c51c10e424d28cd2e1ddddbd3d5f4
Chloé G <contact@chlookie.net>
2024-10-01 23:32:43 +0200

    52 current  2024-10-01 23:32:37  24.11.20240926.1925c60   6.10.5

---
+++ hosts/pancake/local/syncthing.nix
@@ -0,0 +1,47 @@
+{ config, ... }:
+{
+  imports = [
+    ../../../modules/syncthing-common.nix
+  ];
+
+  services.syncthing = {
+    # The path where synchronised directories will exist.
+    dataDir = "/stuff";
+
+    # The path where the settings and keys will exist.
+    configDir = "/var/lib/syncthing";
+
+    # Together, the key and cert define the device id
+    key = config.age.secrets.syncthing-pancake-key.path;
+    cert = config.age.secrets.syncthing-pancake-cert.path;
+
+    settings = {
+      gui = {
+        # Don't care that it's public in the nix store
+        user = "qwe";
+        password = "qwe";
+      };
+
+      # Don't care that device ids are public in the nix store
+      devices = {
+        "neon" = {
+          id = builtins.readFile config.age.secrets.syncthing-neon-id.path;
+        };
+        # "celestia" = {
+        #   id = "DEVICE-ID-GOES-HERE";
+        # };
+      };
+
+      folders = {
+        "Obsidian" = {
+          id = "zzaui-egygo";
+          path = "/stuff/obsidian";
+          devices = [
+            "neon"
+          ];
+        };
+      };
+
+    };
+  };
+}
---
+++ modules/syncthing-common.nix
@@ -0,0 +1,34 @@
+{ ... }:
+{
+  services.syncthing = {
+    enable = true;
+
+    # By default, a user named syncthing will be created whose home directory is dataDir.
+    # user = "syncthing";
+
+    # Overrides any devices/folders added or deleted through the WebUI
+    overrideDevices = true;
+    overrideFolders = true;
+
+    # Whether to auto-launch Syncthing as a system service.
+    systemService = true;
+
+    options = {
+      # Anonymous data usage
+      urAccepted = 1;
+
+      # Relay servers
+      relaysEnabled = true;
+    };
+  };
+
+  # Open ports in the firewall
+  # networking.firewall.allowedTCPPorts = [
+  #   8384
+  #   22000
+  # ];
+  # networking.firewall.allowedUDPPorts = [
+  #   22000
+  #   21027
+  # ];
+}
