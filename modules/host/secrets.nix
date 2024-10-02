a6249da25982fee713ac213837c175726b75906f
Chloé G <contact@leoluxo.eu>
2024-10-02 19:02:42 +0200

    63 current  2024-10-02 19:02:35  24.11.20240926.1925c60   6.10.5

--- modules/host/secrets.nix
+++ modules/host/secrets.nix
@@ -8,15 +8,17 @@ in
     identityPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
 
     secrets = {
-      pancake-user-password.file = secretsPath + "/userpwds/pancake.age";
+      "pancake-user-password".file = secretsPath + "/userpwds/pancake.age";
 
-      syncthing-pancake-id.file = secretsPath + "/syncthing/pancake/id.age";
-      syncthing-pancake-cert.file = secretsPath + "/syncthing/pancake/cert.pem.age";
-      syncthing-pancake-key.file = secretsPath + "/syncthing/pancake/key.pem.age";
+      "syncthing-celestia-id".file = secretsPath + "/syncthing/celestia/id.age";
 
-      syncthing-neon-id.file = secretsPath + "/syncthing/neon/id.age";
-      syncthing-neon-cert.file = secretsPath + "/syncthing/neon/cert.pem.age";
-      syncthing-neon-key.file = secretsPath + "/syncthing/neon/key.pem.age";
+      "syncthing-pancake-id".file = secretsPath + "/syncthing/pancake/id.age";
+      "syncthing-pancake-cert".file = secretsPath + "/syncthing/pancake/cert.pem.age";
+      "syncthing-pancake-key".file = secretsPath + "/syncthing/pancake/key.pem.age";
+
+      "syncthing-neon-id".file = secretsPath + "/syncthing/neon/id.age";
+      "syncthing-neon-cert".file = secretsPath + "/syncthing/neon/cert.pem.age";
+      "syncthing-neon-key".file = secretsPath + "/syncthing/neon/key.pem.age";
     };
   };
 }
