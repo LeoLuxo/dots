17a8d1a3144ebb96d53da1c0bc60500ca8f9006c
LeoLuxo <leo@gamboa.eu>
2024-10-02 19:33:00 +0200

    Bruh moment

--- modules/host/secrets.nix
+++ modules/host/secrets.nix
@@ -1,24 +1,26 @@
 { ... }:
+
+with builtins;
+with lib;
 let
   secretsPath = /etc/nixos/secrets;
+  secretsFile = "${secretsPath}/secrets.nix";
 in
 {
   age = {
     # For some reason I need to explicitly override it
     identityPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
 
-    secrets = {
-      "pancake-user-password".file = secretsPath + "/userpwds/pancake.age";
-
-      "syncthing-celestia-id".file = secretsPath + "/syncthing/celestia/id.age";
-
-      "syncthing-pancake-id".file = secretsPath + "/syncthing/pancake/id.age";
-      "syncthing-pancake-cert".file = secretsPath + "/syncthing/pancake/cert.pem.age";
-      "syncthing-pancake-key".file = secretsPath + "/syncthing/pancake/key.pem.age";
-
-      "syncthing-neon-id".file = secretsPath + "/syncthing/neon/id.age";
-      "syncthing-neon-cert".file = secretsPath + "/syncthing/neon/cert.pem.age";
-      "syncthing-neon-key".file = secretsPath + "/syncthing/neon/key.pem.age";
-    };
+    secrets =
+      if pathExists secretsFile then
+        mapAttrs' (
+          n: _:
+          nameValuePair (removeSuffix ".age" n) {
+            file = "${secretsDir}/${n}";
+            owner = payas;
+          }
+        ) (import secretsFile)
+      else
+        { };
   };
 }
