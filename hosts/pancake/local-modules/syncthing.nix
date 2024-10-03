829d03d133d446618323a5e977abbc9d710048f6
LeoLuxo <leo@gamboa.eu>
2024-10-03 09:20:18 +0200

    81 current  2024-10-03 09:17:35  24.11.20240926.1925c60   6.10.5

--- hosts/pancake/local-modules/syncthing.nix
+++ hosts/pancake/local-modules/syncthing.nix
@@ -34,13 +34,13 @@ in
         password = "qwe";
       };
 
+      # Don't care that the device ids end up in cleartext on the nix store
       devices = {
         "neon".id = strings.trim (traceVal (readFile secrets."syncthing/neon/id".path));
         "celestia".id = strings.trim (traceVal (readFile secrets."syncthing/celestia/id".path));
-        # "neon".id = "WKZDG5X-W2DJB2N-3A7CS2H-VQDKBN2-RFDLM6P-KGZN4D6-KI2SD3E-3ZMNQAT";
-        # "celestia".id = "FEEK44G-XI3OFWE-TTTSDUC-WCTTXRX-JYGVGKG-AJDLL5I-FWEEQR4-H6YQ7QX";
       };
 
+      # Folders
       folders = {
         "Obsidian" = {
           id = "zzaui-egygo";
@@ -91,23 +91,4 @@ in
     printf "**/workspace*.json\n" > /stuff/obsidian/.stignore
     printf "**/target/\n" > /stuff/uni_courses/.stignore
   '';
-
-  # system.activationScripts = {
-  #   "syncthing-stignore".text = ''
-  #     printf "**/workspace*.json\n" > /stuff/obsidian/.stignore
-  #     printf "**/target/\n" > /stuff/uni_courses/.stignore
-  #   '';
-
-  #   # Replace device ids at activation time in the config file, otherwise they might not be decrypted yet
-  #   "syncthing-device-ids".text = ''
-  #     nix-shell -p sudo coreutils --run sudo sed -i -e s/===PLACEHOLDER_ID_NEON===/$(cat ${
-  #       secrets."syncthing/neon/id".path
-  #     })/g ${syncthingFolder}/config.xml
-
-  #     nix-shell -p sudo coreutils --run sudo sed -i -e s/===PLACEHOLDER_ID_CELESTIA===/$(cat ${
-  #       secrets."syncthing/celestia/id".path
-  #     })/g ${syncthingFolder}/config.xml
-  #   '';
-  # };
-
 }
