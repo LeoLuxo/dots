14a3c215ad9a7e78f192d1e494b4e9136085dd7e
Chloé G <contact@chlookie.net>
2024-10-04 09:03:10 +0200

    93 current  2024-10-04 09:03:01  24.11.20240926.1925c60   6.10.5

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -7,22 +7,22 @@
 # and set bash as the interpreter
 # https://stackoverflow.com/a/64599687
 
-# Original script by 0atman / No boilerplate
+# Based on the script by 0atman / No boilerplate
 # https://gist.github.com/0atman/1a5133b842f929ba4c1e195ee67599d5
 
 # Makes bash error-out if any exit code is non-zero
 set -e
 
 # Some color
-PURPLE='\033[1;35m'
+BLUE='\033[1;34m'
 GREEN='\033[1;32m'
 RESET='\033[0m'
 
 # Force sudo early and cache it so I don't have to enter password later in the script
-sudo echo -e "${PURPLE}Running as superuser${RESET}"
+sudo echo -e "${BLUE}Running as superuser${RESET}"
 
 # cd to our config dir
-pushd /etc/nixos/dots &>/dev/null
+pushd /etc/nixos/dots 1>/dev/null
 
 # Autoformat nix files
 nixfmt --quiet . ||
@@ -39,10 +39,10 @@ nixfmt --quiet . ||
 # fi
 
 # Shows changes
-echo -e "${PURPLE}Files changed:${RESET}"
+echo -e "${BLUE}Files changed:${RESET}"
 git --no-pager diff --name-only .
 
-echo -e "${PURPLE}NixOS Rebuilding...${RESET}"
+echo -e "${BLUE}NixOS Rebuilding...${RESET}"
 
 # For some reason nix can't see non-git added files
 git add .
@@ -57,17 +57,17 @@ sudo nixos-rebuild switch --impure --flake .#$HOSTNAME "$@" ||
 # Get current generation metadata
 current=$(nixos-rebuild list-generations | grep current | sed s/\*//g)
 
-echo -e "${PURPLE}Current generation: ${RESET}${current}"
-echo -e "${PURPLE}Committing...${RESET}"
+echo -e "${BLUE}Current generation: ${RESET}${current}"
+echo -e "${BLUE}Committing...${RESET}"
 
 # Commit all changes with the generation metadata
-git commit -am "$current" &>/dev/null
+git commit -am "$current" 1>/dev/null
 
-echo -e "${PURPLE}Pushing...${RESET}"
-git push &>/dev/null
+echo -e "${BLUE}Pushing...${RESET}"
+git push 1>/dev/null
 
 # Back to where we were
-popd &>/dev/null
+popd 1>/dev/null
 
 # Notify all OK!
 echo -e "${GREEN}DONE!${RESET}"
