650ac443d0b247d97ee02906d34ead51ba10ad00
Chloé G <contact@leoluxo.eu>
2024-10-04 17:46:44 +0200

    Revert "dconf does not exist"
    This reverts commit b92f3711762b5885a2aae8851ce7ec592efd1ac4.

---
+++ modules/user/dconf.nix
@@ -0,0 +1,57 @@
+{ ... }:
+{
+  dconf.settings = {
+    "org/gnome/desktop/peripherals/touchpad" = {
+      natural-scroll = true;
+      two-finger-scrolling-enabled = true;
+    };
+
+    "org/gnome/desktop/interface" = {
+      enable-hot-corners = false;
+    };
+
+    "org/gnome/mutter" = {
+      edge-tiling = true;
+      dynamic-workspaces = true;
+    };
+
+    "org/gnome/desktop/wm/keybindings" = {
+      switch-applications = [ ];
+      switch-applications-backward = [ ];
+
+      switch-windows = [ "<Alt>Tab" ];
+      switch-windows-backward = [ "<Shift><Alt>Tab" ];
+    };
+
+    "org/gnome/shell/keybindings" = {
+      toggle-quick-settings = [ ];
+    };
+
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+      custom-keybindings = [
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/terminal/"
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/screenshot/"
+      ];
+    };
+
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/terminal" = {
+      binding = "<Super>t";
+      command = "kgx";
+      name = "GNOME Console";
+    };
+
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/screenshot" = {
+      binding = "<Super>s";
+      command = "snip";
+      name = "Instant screenshot";
+    };
+
+    "org/gnome/shell/window-switcher" = {
+      current-workspace-only = true;
+    };
+
+    "org/gnome/desktop/sound" = {
+      event-sounds = false;
+    };
+  };
+}
