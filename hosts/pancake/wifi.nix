a9f026025ba920f114f93a70cc5789ec0c43fdad
LeoLuxo <leo@gamboa.eu>
2024-10-04 08:50:08 +0200

    90 current  2024-10-04 08:50:02  24.11.20240926.1925c60   6.10.5

---
+++ hosts/pancake/wifi.nix
@@ -0,0 +1,72 @@
+{ config, ... }:
+{
+  networking.networkmanager = {
+    # Enable networking with NetworkManager
+    enable = true;
+
+    ensureProfiles = {
+      environmentFiles = [
+        config.age.secrets."networkmanager-env".path
+      ];
+
+      profiles = {
+        "Celeste Mountain" = {
+          connection = {
+            id = "Celeste Mountain";
+            interface-name = "wlp0s20f3";
+            type = "wifi";
+            uuid = "4b899b53-7cd4-48c5-a7d5-56b598c6b9aa";
+          };
+          ipv4 = {
+            method = "auto";
+          };
+          ipv6 = {
+            addr-gen-mode = "default";
+            method = "auto";
+          };
+          proxy = { };
+          wifi = {
+            mode = "infrastructure";
+            ssid = "Celeste Mountain";
+          };
+          wifi-security = {
+            auth-alg = "open";
+            key-mgmt = "wpa-psk";
+            psk = "$CELESTE_MOUNTAIN_PSK";
+          };
+        };
+
+        eduroam = {
+          "802-1x" = {
+            ca-cert = "./eduroam-ca.pem";
+            eap = "peap;";
+            identity = "au752692@uni.au.dk";
+            password = "$EDUROAM_PSK";
+            phase1-peapver = "1";
+            phase2-auth = "mschapv2";
+          };
+          connection = {
+            id = "eduroam";
+            type = "wifi";
+            uuid = "2203ce9e-afc1-4e35-9a0c-92fa749bb33a";
+          };
+          ipv4 = {
+            method = "auto";
+          };
+          ipv6 = {
+            addr-gen-mode = "stable-privacy";
+            method = "auto";
+          };
+          proxy = { };
+          wifi = {
+            mode = "infrastructure";
+            ssid = "eduroam";
+          };
+          wifi-security = {
+            key-mgmt = "wpa-eap";
+          };
+        };
+      };
+    };
+  };
+}
