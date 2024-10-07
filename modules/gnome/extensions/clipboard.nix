464190550801816abaf0406e462b48671c1f9141
Chloé G <contact@chlookie.net>
2024-10-07 17:23:50 +0200

    126 current  2024-10-07 17:23:43  24.11.20240926.1925c60  6.10.5
    modules/gnome/extensions/clipboard.nix
    modules/gnome/gnome.nix

--- modules/gnome/extensions/clipboard.nix
+++ modules/gnome/extensions/clipboard.nix
@@ -9,8 +9,8 @@ with lib;
 
 {
   environment.systemPackages = with pkgs; [
-    # Manage wallpapers with ease
-    gnomeExtensions.clipboard-indicator
+    # Gnome Clipboard History is a clipboard manager GNOME extension that saves items you've copied into an easily accessible, searchable history panel.
+    gnomeExtensions.clipboard-history
   ];
 
   home-manager.users.${user} =
@@ -20,23 +20,23 @@ with lib;
     {
       dconf.settings = {
         "org/gnome/shell" = {
-          enabled-extensions = [ "clipboard-indicator@tudmotu.com" ];
+          # enabled-extensions = [ ];
         };
 
         "org/gnome/shell/extensions/clipboard-indicator" = {
-          cache-size = 50;
-          clear-on-boot = true;
-          confirm-clear = true;
-          display-mode = 2;
-          history-size = 50;
-          move-item-first = false;
-          paste-button = true;
-          pinned-on-bottom = true;
-          preview-size = 50;
+          # cache-size = 50;
+          # clear-on-boot = true;
+          # confirm-clear = true;
+          # display-mode = 2;
+          # history-size = 50;
+          # move-item-first = false;
+          # paste-button = true;
+          # pinned-on-bottom = true;
+          # preview-size = 50;
 
-          toggle-menu = [ "<Super>v" ];
-          prev-entry = [ "<Super>comma" ];
-          next-entry = [ "<Super>period" ];
+          # toggle-menu = [ "<Super>v" ];
+          # prev-entry = [ "<Super>comma" ];
+          # next-entry = [ "<Super>period" ];
         };
 
       };
