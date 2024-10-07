9d76d3b861f60d9aa806d6ee7b485b5e1babad24
LeoLuxo <leo@gamboa.eu>
2024-10-07 12:04:17 +0200

    114 current  2024-10-07 12:04:11  24.11.20240926.1925c60  6.10.5
    modules/gnome/gnome.nix
    scripts/rebuild.sh

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -57,7 +57,7 @@ sudo nixos-rebuild switch --impure --flake .#$HOSTNAME "$@" ||
 # Get current generation metadata
 current_gen=$(nixos-rebuild list-generations | grep current | sed s/\*//g)
 
-echo -e "${BLUE}Current generation: ${RESET}\n${msg}"
+echo -e "${BLUE}Current generation: ${RESET}\n${current_gen}"
 echo -e "${BLUE}Committing...${RESET}"
 
 # Commit all changes with the generation metadata
