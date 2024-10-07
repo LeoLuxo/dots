7909396bfc123956a8d1db96bcde3082318279c0
LeoLuxo <leo@gamboa.eu>
2024-10-07 20:13:21 +0200

    130 current  2024-10-07 20:13:15  24.11.20240926.1925c60  6.10.5
    modules/discord/discord.nix
    modules/discord/overlay.nix

--- modules/discord/discord.nix
+++ modules/discord/discord.nix
@@ -1,6 +1,7 @@
 {
   pkgs,
   user,
+  iconSet,
   ...
 }:
 
@@ -12,5 +13,51 @@
     ];
   };
 
-  nixpkgs.overlays = [ (import ./overlay.nix) ];
+  nixpkgs.overlays = [
+    (final: prev: {
+      vesktop = prev.vesktop.overrideAttrs (oldAttrs: {
+
+        # Overwrite the desktop item so the app name is "Discord"
+        desktopItems = (
+          prev.makeDesktopItem {
+            name = "vesktop";
+            desktopName = "Discord";
+            exec = "vesktop %U";
+            icon = "vesktop";
+            startupWMClass = "Discord";
+            genericName = "Internet Messenger";
+            keywords = [
+              "discord"
+              "vencord"
+              "vesktop"
+              "electron"
+              "chat"
+            ];
+            categories = [
+              "Network"
+              "InstantMessaging"
+              "Chat"
+            ];
+          }
+        );
+
+        # Add a prebuild action to overwrite the tray icon
+        preBuild =
+          oldAttrs.preBuild
+          + ''
+            pushd build
+            cp ${iconSet.discord-white} Icon.png
+            popd
+          '';
+
+        # Add a preinstall action to overwrite the app icons
+        preInstall = ''
+          pushd build
+          rm icon_*x32.png
+          cp ${iconSet.discord-white} icon_512x512x32.png
+          popd
+        '';
+      });
+    })
+  ];
 }
