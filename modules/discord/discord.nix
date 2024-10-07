20bb735100f8dffb2ba49b3198075e00f9b0694b
Chloé G <contact@chlookie.net>
2024-10-07 19:45:15 +0200

    129 current  2024-10-07 19:45:08  24.11.20240926.1925c60  6.10.5
    modules/discord/default.nix

--- modules/discord/discord.nix
+++ modules/discord/discord.nix
@@ -1,11 +1,16 @@
-{ pkgs, user, ... }:
+{
+  pkgs,
+  user,
+  ...
+}:
 
 {
-
   home-manager.users.${user} = {
     home.packages = with pkgs; [
       # Discord fork that fixes streaming issues on linux
       vesktop
     ];
   };
+
+  nixpkgs.overlays = [ (import ./overlay.nix) ];
 }
