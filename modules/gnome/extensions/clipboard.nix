f84599a4dc11faaa79b754a033e5e676272d7089
Chloé G <contact@chlookie.net>
2024-10-07 15:52:04 +0200

    124 current  2024-10-07 15:51:58  24.11.20240926.1925c60  6.10.5
    modules/gnome/extensions/clipboard.nix

--- modules/gnome/extensions/clipboard.nix
+++ modules/gnome/extensions/clipboard.nix
@@ -20,13 +20,23 @@ with lib;
     {
       dconf.settings = {
         "org/gnome/shell" = {
-          enabled-extensions = [ "wallhub@sakithb.github.io" ];
+          enabled-extensions = [ "clipboard-indicator@tudmotu.com" ];
         };
 
         "org/gnome/shell/extensions/clipboard-indicator" = {
+          cache-size = 50;
+          clear-on-boot = true;
+          confirm-clear = true;
+          display-mode = 2;
+          history-size = 50;
+          move-item-first = false;
+          paste-button = true;
+          pinned-on-bottom = true;
+          preview-size = 50;
+
           toggle-menu = [ "<Super>v" ];
-          next-entry = [ "<Super>down" ];
-          previous-entry = [ "<Super>up" ];
+          prev-entry = [ "<Super>comma" ];
+          next-entry = [ "<Super>period" ];
         };
 
       };
