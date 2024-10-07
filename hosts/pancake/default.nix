846b69c7f99d5ab685aa10b9602bd8026a4cd134
LeoLuxo <leo@gamboa.eu>
2024-10-07 22:10:58 +0200

    138 current  2024-10-07 22:10:51  24.11.20240926.1925c60  6.10.5
    hosts/pancake/default.nix
    modules/gnome/extensions/clipboard.nix

--- hosts/pancake/default.nix
+++ hosts/pancake/default.nix
@@ -42,8 +42,6 @@
 
       obsidian
 
-      clipse
-
       # # It is sometimes useful to fine-tune packages, for example, by applying
       # # overrides. You can do that directly here, just don't forget the
       # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
