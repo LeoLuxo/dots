7c4d4b369a64a57fd4eaa21ebd370fadeff202cd
LeoLuxo <leo@gamboa.eu>
2024-10-07 22:36:45 +0200

    140 current  2024-10-07 22:36:36  24.11.20240926.1925c60  6.10.5
    modules/gnome/gnome.nix

---
+++ modules/gnome/extensions/translator.nix
@@ -0,0 +1,32 @@
+{
+  pkgs,
+  user,
+  lib,
+  ...
+}:
+
+with lib;
+
+{
+  environment.systemPackages = with pkgs; [
+    # Translate extension for Gnome-Shell - based on translate-shell, inspired by Tudmotu's clipboard-indicator and gufoe's text-translator.
+    gnomeExtensions.translate-indicator
+  ];
+
+  home-manager.users.${user} =
+    { lib, ... }:
+    # with lib.hm.gvariant;
+
+    {
+      dconf.settings = {
+        "org/gnome/shell" = {
+          # enabled-extensions = [ "clipboard-indicator@tudmotu.com" ];
+        };
+
+        "org/gnome/shell/extensions/translate-indicator" =
+          {
+          };
+
+      };
+    };
+}
