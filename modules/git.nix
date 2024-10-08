786872b27f85fd62777295b757a116ac4c93c758
LeoLuxo <leo@gamboa.eu>
2024-10-08 15:16:47 +0200

    152 current  2024-10-08 15:07:29  24.11.20240926.1925c60  6.10.5
    modules/git.nix

--- modules/git.nix
+++ modules/git.nix
@@ -2,43 +2,41 @@
 
 {
   home-manager.users.${user} = {
-    programs = {
-      git = {
-        enable = true;
+    programs.git = {
+      enable = true;
 
-        userEmail = "contact@me.leoluxo.eu";
-        userName = "LeoLuxo";
+      userEmail = "contact@me.leoluxo.eu";
+      userName = "LeoLuxo";
 
-        extraConfig = {
-          init.defaultBranch = "main";
+      extraConfig = {
+        init.defaultBranch = "main";
 
-          # Disable safe directory checks
-          safe.directory = "*";
+        # Disable safe directory checks
+        safe.directory = "*";
 
-          # Some options transferred from my windows times, no idea if they're relevant here :shrug:
-          core = {
-            # Don't hide the .git directory on windows
-            hideDotFiles = false;
-            # Set the text editor
-            editor = "code";
-            # Disable the CRLF warning (but still auto convert)
-            safecrlf = false;
-          };
+        # Some options transferred from my windows times, no idea if they're relevant here :shrug:
+        core = {
+          # Don't hide the .git directory on windows
+          hideDotFiles = false;
+          # Set the text editor
+          editor = "code";
+          # Disable the CRLF warning (but still auto convert)
+          safecrlf = false;
+        };
 
-          # Automatically setup remote when pushing on a repo that was cloned (iirc?)
-          push.autoSetupRemote = true;
+        # Automatically setup remote when pushing on a repo that was cloned (iirc?)
+        push.autoSetupRemote = true;
 
-          # Force using the CMD credentials prompt instead of a window
-          credential.modalprompt = false;
+        # Force using the CMD credentials prompt instead of a window
+        credential.modalprompt = false;
 
-          pull.rebase = false;
+        pull.rebase = false;
 
-          # Enforce SSH
-          url = {
-            "ssh://git@github.com/".insteadOf = "https://github.com/";
-            "ssh://git@gitlab.com/".insteadOf = "https://gitlab.com/";
-            "ssh://git@bitbucket.org/".insteadOf = "https://bitbucket.org/";
-          };
+        # Enforce SSH
+        url = {
+          "ssh://git@github.com/".insteadOf = "https://github.com/";
+          "ssh://git@gitlab.com/".insteadOf = "https://gitlab.com/";
+          "ssh://git@bitbucket.org/".insteadOf = "https://bitbucket.org/";
         };
       };
     };
