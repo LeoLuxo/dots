45a8b622eb81f591c3bba8bfa153f6e4106049cd
LeoLuxo <leo@gamboa.eu>
2024-10-08 15:16:47 +0200

    152 current  2024-10-08 15:07:29  24.11.20240926.1925c60  6.10.5
    modules/git.nix

---
+++ modules/kdeconnect.nix
@@ -0,0 +1,26 @@
+{ user, ... }:
+
+{
+  home-manager.users.${user} = {
+    services.kdeconnect = {
+      enable = true;
+      indicator = true;
+    };
+  };
+
+  networking.firewall = {
+    enable = true;
+    allowedTCPPortRanges = [
+      {
+        from = 1714;
+        to = 1764;
+      }
+    ];
+    allowedUDPPortRanges = [
+      {
+        from = 1714;
+        to = 1764;
+      }
+    ];
+  };
+}
