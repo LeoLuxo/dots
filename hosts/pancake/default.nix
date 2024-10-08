839abb484d67f6c276b18fefc4dbcc7eecd62ef6
LeoLuxo <leo@gamboa.eu>
2024-10-08 17:58:46 +0200

    163 current  2024-10-08 17:58:37  24.11.20240926.1925c60  6.10.5
    hosts/pancake/default.nix
    modules/base.nix

--- hosts/pancake/default.nix
+++ hosts/pancake/default.nix
@@ -47,6 +47,8 @@
       textpieces
       hieroglyphic
 
+      teams
+
       # # It is sometimes useful to fine-tune packages, for example, by applying
       # # overrides. You can do that directly here, just don't forget the
       # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
