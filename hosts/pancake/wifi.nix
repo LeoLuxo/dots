be5fa9f70cf4443c1bb2fed9e81e24fb1e91ac67
LeoLuxo <leo@gamboa.eu>
2024-10-08 11:27:39 +0200

    144 current  2024-10-08 11:27:32  24.11.20240926.1925c60  6.10.5
    hosts/pancake/wifi.nix

--- hosts/pancake/wifi.nix
+++ hosts/pancake/wifi.nix
@@ -37,54 +37,54 @@
           };
         };
 
-        eduroam = {
-          "802-1x" = {
-            ca-cert = "/etc/NetworkManager/system-connections/eduroam-ca.pem";
-            eap = "peap;";
-            identity = "$EDUROAM_IDENTITY";
-            password = "$EDUROAM_PASSWORD";
-            phase1-peapver = "1";
-            phase2-auth = "mschapv2";
-          };
-          connection = {
-            id = "eduroam";
-            type = "wifi";
-            uuid = "2203ce9e-afc1-4e35-9a0c-92fa749bb33a";
-          };
-          ipv4 = {
-            method = "auto";
-          };
-          ipv6 = {
-            addr-gen-mode = "stable-privacy";
-            method = "auto";
-          };
-          proxy = { };
-          wifi = {
-            mode = "infrastructure";
-            ssid = "eduroam";
-          };
-          wifi-security = {
-            key-mgmt = "wpa-eap";
-          };
-        };
+        # eduroam = {
+        #   "802-1x" = {
+        #     ca-cert = "/etc/NetworkManager/system-connections/eduroam-ca.pem";
+        #     eap = "peap;";
+        #     identity = "$EDUROAM_IDENTITY";
+        #     password = "$EDUROAM_PASSWORD";
+        #     phase1-peapver = "1";
+        #     phase2-auth = "mschapv2";
+        #   };
+        #   connection = {
+        #     id = "eduroam";
+        #     type = "wifi";
+        #     uuid = "2203ce9e-afc1-4e35-9a0c-92fa749bb33a";
+        #   };
+        #   ipv4 = {
+        #     method = "auto";
+        #   };
+        #   ipv6 = {
+        #     addr-gen-mode = "stable-privacy";
+        #     method = "auto";
+        #   };
+        #   proxy = { };
+        #   wifi = {
+        #     mode = "infrastructure";
+        #     ssid = "eduroam";
+        #   };
+        #   wifi-security = {
+        #     key-mgmt = "wpa-eap";
+        #   };
+        # };
       };
     };
   };
 
-  systemd.services.eduroam-ca = {
-    wantedBy = [ "NetworkManager-ensure-profiles.service" ];
-    enable = true;
-    serviceConfig = {
-      User = "root";
-      Group = "root";
-      Type = "oneshot";
-    };
-    script = ''
-      cp "${
-        config.age.secrets."wifi/eduroam-ca.pem".path
-      }" /etc/NetworkManager/system-connections/eduroam-ca.pem
-      chown root:root /etc/NetworkManager/system-connections/eduroam-ca.pem
-      chmod 600 /etc/NetworkManager/system-connections/eduroam-ca.pem
-    '';
-  };
+  # systemd.services.eduroam-ca = {
+  #   wantedBy = [ "NetworkManager-ensure-profiles.service" ];
+  #   enable = true;
+  #   serviceConfig = {
+  #     User = "root";
+  #     Group = "root";
+  #     Type = "oneshot";
+  #   };
+  #   script = ''
+  #     cp "${
+  #       config.age.secrets."wifi/eduroam-ca.pem".path
+  #     }" /etc/NetworkManager/system-connections/eduroam-ca.pem
+  #     chown root:root /etc/NetworkManager/system-connections/eduroam-ca.pem
+  #     chmod 600 /etc/NetworkManager/system-connections/eduroam-ca.pem
+  #   '';
+  # };
 }
