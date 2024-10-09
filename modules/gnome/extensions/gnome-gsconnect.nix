6ab80ad41e242b08da0e93ae1159eac2f6f72fe4
Chloé G <contact@chlookie.net>
2024-10-09 12:01:35 +0200

    174 current  2024-10-09 12:01:26  24.11.20240926.1925c60  6.10.5
    modules/gnome/extensions/gnome-gsconnect.nix
    modules/gnome/extensions/gnome-just-perfection.nix
    modules/gnome/gnome.nix

--- modules/gnome/extensions/gnome-gsconnect.nix
+++ modules/gnome/extensions/gnome-gsconnect.nix
@@ -18,21 +18,21 @@ with lib;
     package = pkgs.gnomeExtensions.gsconnect;
   };
 
-  networking.firewall = {
-    enable = true;
-    allowedTCPPortRanges = [
-      {
-        from = 1714;
-        to = 1764;
-      }
-    ];
-    allowedUDPPortRanges = [
-      {
-        from = 1714;
-        to = 1764;
-      }
-    ];
-  };
+  # networking.firewall = {
+  #   enable = true;
+  #   allowedTCPPortRanges = [
+  #     {
+  #       from = 1714;
+  #       to = 1764;
+  #     }
+  #   ];
+  #   allowedUDPPortRanges = [
+  #     {
+  #       from = 1714;
+  #       to = 1764;
+  #     }
+  #   ];
+  # };
 
   home-manager.users.${user} =
     { lib, ... }:
