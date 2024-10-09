9a4957e5a5f549b7fedbaf37133da502e33049d6
Chloé G <contact@leoluxo.eu>
2024-10-09 12:01:35 +0200

    174 current  2024-10-09 12:01:26  24.11.20240926.1925c60  6.10.5
    modules/gnome/extensions/gnome-gsconnect.nix
    modules/gnome/extensions/gnome-just-perfection.nix
    modules/gnome/gnome.nix

--- modules/gnome/extensions/gnome-just-perfection.nix
+++ modules/gnome/extensions/gnome-just-perfection.nix
@@ -22,7 +22,56 @@ with lib;
     {
       dconf.settings = {
         "org/gnome/shell" = {
-          enabled-extensions = [ "wallhub@sakithb.github.io" ];
+          enabled-extensions = [ "just-perfection-desktop@just-perfection" ];
+        };
+
+        "org/gnome/shell/extensions/just-perfection" = {
+          accessibility-menu = false;
+          activities-button = true;
+          alt-tab-window-preview-size = 0;
+          animation = 5;
+          background-menu = true;
+          clock-menu = true;
+          clock-menu-position = 0;
+          clock-menu-position-offset = 2;
+          controls-manager-spacing-size = 0;
+          dash = false;
+          dash-icon-size = 0;
+          double-super-to-appgrid = true;
+          keyboard-layout = false;
+          max-displayed-search-results = 0;
+          notification-banner-position = 1;
+          osd = true;
+          osd-position = 2;
+          panel = true;
+          panel-in-overview = true;
+          panel-notification-icon = true;
+          panel-size = 0;
+          power-icon = true;
+          quick-settings = true;
+          quick-settings-dark-mode = false;
+          ripple-box = true;
+          search = true;
+          show-apps-button = false;
+          startup-status = 0;
+          switcher-popup-delay = false;
+          theme = false;
+          top-panel-position = 0;
+          weather = false;
+          window-demands-attention-focus = true;
+          window-maximized-on-create = false;
+          window-picker-icon = true;
+          window-preview-caption = true;
+          window-preview-close-button = true;
+          workspace = true;
+          workspace-background-corner-size = 0;
+          workspace-peek = true;
+          workspace-popup = true;
+          workspace-switcher-should-show = true;
+          workspace-switcher-size = 20;
+          workspace-wrap-around = false;
+          workspaces-in-app-grid = true;
+          world-clock = false;
         };
       };
     };
