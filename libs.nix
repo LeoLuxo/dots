f1471af15ff665e23a4e42d5a7f6acfc4ebb548d
LeoLuxo <leo@gamboa.eu>
2024-10-13 22:01:47 +0200

    191 current  2024-10-13 22:01:38  24.11.20240926.1925c60  6.10.5
    assets/wallpapers/wallpaper-nixos.png
    assets/wallpapers/wallpaper-sky.jpg
    hosts.nix
    hosts/pancake/default.nix
    libs.nix
    modules/boot-customization.nix
    modules/common-base.nix
    modules/gnome/extensions/gnome-blur-my-shell.nix
    modules/gnome/extensions/gnome-clipboard.nix
    modules/gnome/extensions/gnome-emojis.nix
    modules/gnome/extensions/gnome-gsconnect.nix
    modules/gnome/extensions/gnome-just-perfection.nix
    modules/gnome/extensions/gnome-rounded-corners.nix
    modules/gnome/extensions/gnome-system-monitor.nix
    modules/gnome/extensions/gnome-wallhub.nix
    modules/gnome/extensions/gnome-weather.nix
    modules/themes/theme-catppuccin.nix

--- libs.nix
+++ libs.nix
@@ -15,7 +15,7 @@ rec {
       "svg"
     ] false;
 
-    icons = findFiles ./assets [
+    icons = findFiles ./assets/icons [
       "ico"
       "icns"
     ] false;
@@ -41,53 +41,124 @@ rec {
     dir: extensions: allowDefault:
     let
       extRegex = "(${strings.concatStrings (strings.intersperse "|" extensions)})";
-      findFilesInner =
+      ignore = name: {
+        name = "";
+        value = null;
+      };
+
+      findFilesRecursive =
         dir:
-        concatLists (
-          attrValues (
-            mapAttrs (
-              name: type:
-              let
-                extMatch = (match "(.*)\\.${extRegex}" name);
-              in
-              # If regular file, then add it to the file list only if the extension regex matches
-              if type == "regular" then
-                if extMatch == null then
-                  [ ]
-                else
-                  [
-                    {
-                      name = elemAt extMatch 0;
-                      value = dir + "/${name}";
-                    }
-                  ]
-              # If directory, ...
-              else if type == "directory" then
-                let
-                  # ... then search for a default.ext file
-                  files = readDir (dir + "/${name}");
-                  defaultFiles = map (ext: "default.${ext}") extensions;
-                  hasDefault = any (defaultFile: files ? ${defaultFile}) defaultFiles;
-                in
-                # if it exists, add the directory to our file list
-                if allowDefault && hasDefault then
-                  [
-                    {
-                      inherit name;
-                      value = dir + "/${name}";
-                    }
-                  ]
-                else
-                  # otherwise search recursively in the directory
-                  findFilesInner (dir + "/${name}")
+        attrsets.filterAttrs (n: v: v != null) (
+          attrsets.mapAttrs' (
+            fileName: type:
+            let
+              extMatch = (match "(.*)\\.${extRegex}" fileName);
+              filePath = dir + "/${fileName}";
+            in
+            # If regular file, then add it to the file list only if the extension regex matches
+            if type == "regular" then
+              if extMatch == null then
+                ignore fileName
               else
-                # any other file types we ignore (i.e. symlink and unknown)
-                [ ]
-            ) (readDir dir)
-          )
+                {
+                  name = elemAt extMatch 0;
+                  value = filePath;
+                }
+            # If directory, ...
+            else if type == "directory" then
+              let
+                # ... then search for a default.ext file
+                files = readDir filePath;
+                defaultFiles = map (ext: "default.${ext}") extensions;
+                hasDefault = any (defaultFile: files ? ${defaultFile}) defaultFiles;
+              in
+              # if it exists, add the directory to our file list
+              if allowDefault && hasDefault then
+                {
+                  name = fileName;
+                  value = filePath;
+                }
+              else
+                # otherwise search recursively in the directory
+                # and map the results to a nested set with the name of the folder as top key
+                {
+                  name = fileName;
+                  value = findFilesRecursive filePath;
+                }
+            else
+              # any other file types we ignore (i.e. symlink and unknown)
+              ignore fileName
+          ) (readDir dir)
         );
     in
-    listToAttrs (findFilesInner dir);
+    findFilesRecursive dir;
+
+  # let
+  #   findFilesInner =
+  #     dir:
+  #     concatLists (
+  #       attrValues (
+  #         mapAttrs (
+  #           fileName: type:
+  #           let
+  #             extMatch = (match "(.*)\\.${extRegex}" fileName);
+  #           in
+  #           # If regular file, then add it to the file list only if the extension regex matches
+  #           if type == "regular" then
+  #             if extMatch == null then
+  #               [ ]
+  #             else
+  #               [
+  #                 {
+  #                   name = elemAt extMatch 0;
+  #                   value = dir + "/${fileName}";
+  #                 }
+  #               ]
+  #           # If directory, ...
+  #           else if type == "directory" then
+  #             let
+  #               # ... then search for a default.ext file
+  #               files = readDir (dir + "/${fileName}");
+  #               defaultFiles = map (ext: "default.${ext}") extensions;
+  #               hasDefault = any (defaultFile: files ? ${defaultFile}) defaultFiles;
+  #             in
+  #             # if it exists, add the directory to our file list
+  #             if allowDefault && hasDefault then
+  #               [
+  #                 {
+  #                   name = fileName;
+  #                   value = dir + "/${fileName}";
+  #                 }
+  #               ]
+  #             else
+  #               # otherwise search recursively in the directory
+  #               # and map the results to a nested set with the name of the folder as top key
+  #               [
+  #                 {
+  #                 name = fileName;
+  #                 value = 
+  #                 }
+  #               ]
+  #               map (
+  #                 { name, value }:
+  #                 {
+  #                   name = fileName;
+  #                   value = {
+  #                     ${name} = value;
+  #                   };
+  #                 }
+  #               ) (traceValSeq (findFilesInner (dir + "/${fileName}")))
+  #           else
+  #             # any other file types we ignore (i.e. symlink and unknown)
+  #             [ ]
+  #         ) (readDir dir)
+  #       )
+  #     );
+  # in
+  # listToAttrs (findFilesInner dir);
+
+  # Shortcut to easily import whole directories structures
+  importDirectory = path: mapAttrs (name: value: import value) (findFiles path [ "nix" ] false);
 
   # Function to create a nixos host config
   mkHost =
@@ -95,19 +166,16 @@ rec {
       user,
       hostName,
       system,
-      module,
+      modules,
     }:
     nixosSystem {
       inherit system;
-      modules = [
-        module
-        ./secrets.nix
-      ];
+      modules = [ ./secrets.nix ] ++ modules;
       specialArgs = inputs // {
         # Constants
         inherit user system hostName;
         # Helper libs
-        inherit directories;
+        inherit directories findFiles importDirectory;
         scriptBin = scriptBin system;
       };
     };
