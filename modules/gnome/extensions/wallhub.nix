3cf4a2e37efe29808a75fca59ef87300c0b1eafb
LeoLuxo <leo@gamboa.eu>
2024-10-14 18:26:13 +0200

    pancake 206 current  2024-10-14 18:26:10  24.11.20240926.1925c60  6.10.5
    hosts/pancake/default.nix
    modules/gnome/extensions/blur-my-shell.nix
    modules/gnome/extensions/clipboard.nix
    modules/gnome/extensions/ddterm.nix
    modules/gnome/extensions/emojis.nix
    modules/gnome/extensions/gsconnect.nix
    modules/gnome/extensions/just-perfection.nix
    modules/gnome/extensions/rounded-corners.nix
    modules/gnome/extensions/system-monitor.nix
    modules/gnome/extensions/wallhub.nix
    modules/gnome/extensions/weather.nix
    modules/gnome/gnome.nix
    modules/terminals/guake.nix

--- modules/gnome/extensions/wallhub.nix
+++ modules/gnome/extensions/wallhub.nix
@@ -8,16 +8,16 @@
 with lib;
 
 {
-  environment.systemPackages = with pkgs; [
-    dconf
-    gnomeExtensions.wallhub
-  ];
+  programs.dconf.enable = true;
 
   home-manager.users.${user} =
     { lib, ... }:
     # with lib.hm.gvariant;
-
     {
+      home.packages = with pkgs; [
+        gnomeExtensions.wallhub
+      ];
+
       dconf.settings = {
         "org/gnome/shell" = {
           enabled-extensions = [ "wallhub@sakithb.github.io" ];
