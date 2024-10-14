b3f933b283a5e40fbc784f30487459e0d5164795
Chloé G <contact@chlookie.net>
2024-10-14 10:19:41 +0200

    196 current  2024-10-14 10:19:32  24.11.20240926.1925c60  6.10.5
    modules/style/static-wallpapers.nix

--- modules/style/static-wallpapers.nix
+++ modules/style/static-wallpapers.nix
@@ -6,13 +6,17 @@
 }:
 
 {
+
+  # On activation move existing files by appending the given file extension rather than exiting with an error.
+  home-manager.backupFileExtension = "bak";
+
   home-manager.users.${user} = {
     xdg = {
       enable = true;
 
       # Symlink all static wallpapers into the gnome wallpaper directory
       # ~/.local/share/backgrounds
-      dataFile."test".source = lib.traceValSeq directories.images.wallpapers._dir;
+      dataFile."backgrounds".source = directories.images.wallpapers._dir;
     };
   };
 }
