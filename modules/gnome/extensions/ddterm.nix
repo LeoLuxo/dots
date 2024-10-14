ae097402d6ee4e699d7eb3c27e9aa56f408cf127
Chloé G <contact@chlookie.net>
2024-10-14 12:18:44 +0200

    pancake 200 current  2024-10-14 12:18:37  24.11.20240926.1925c60  6.10.5
    hosts/pancake/default.nix
    modules/style/static-wallpapers.nix

---
+++ modules/gnome/extensions/ddterm.nix
@@ -0,0 +1,32 @@
+{
+  pkgs,
+  user,
+  lib,
+  ...
+}:
+
+with lib;
+
+{
+  environment.systemPackages = with pkgs; [
+    dconf
+    gnomeExtensions.ddterm
+  ];
+
+  home-manager.users.${user} =
+    { lib, ... }:
+    # with lib.hm.gvariant;
+
+    {
+      dconf.settings = {
+        "org/gnome/shell" = {
+          # enabled-extensions = [ "clipboard-indicator@tudmotu.com" ];
+        };
+
+        "org/gnome/shell/extensions/clipboard-indicator" =
+          {
+          };
+
+      };
+    };
+}
