8bf5d8d59c946843a8a656abb906ad7e44d6b68f
LeoLuxo <leo@gamboa.eu>
2024-10-16 14:46:42 +0200

    pancake 222 current  2024-10-16 14:46:37  24.11.20240926.1925c60  6.10.5
    modules/gnome/gnome.nix
    modules/snip.nix
    modules/translation/deepl/default.nix
    modules/translation/gnome-dialect.nix

--- modules/translation/deepl/default.nix
+++ modules/translation/deepl/default.nix
@@ -1,28 +1,22 @@
-{ pkgs, user, ... }:
-
 {
-  imports = [ ./deepl-linux-electron.nix ];
+  pkgs,
+  user,
+  ...
+}:
+{
+  imports = [
+    ./deepl-linux-electron.nix
 
-  # programs.dconf.enable = true;
+    # (mkGnomeKeybind {
+    #   name = "Instant translate";
+    #   binding = "<Super>d";
+    #   command = "dialect --selection";
+    # })
+  ];
 
   home-manager.users.${user} = {
     home.packages = with pkgs; [
       deepl-linux-electron
     ];
-
-    dconf.settings = {
-      # Custom shortcut
-      # "org/gnome/settings-daemon/plugins/media-keys" = {
-      #   custom-keybindings = [
-      #     "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/translate/"
-      #   ];
-      # };
-
-      # "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/translate" = {
-      #   binding = "<Super>d";
-      #   command = "dialect --selection";
-      #   name = "Instant translate";
-      # };
-    };
   };
 }
