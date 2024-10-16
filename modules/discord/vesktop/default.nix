6fa7464c5b8c64fb45a6e8627e86c983a0876083
Chloé G <contact@leoluxo.eu>
2024-10-16 13:06:44 +0200

    pancake 216 current  2024-10-16 13:06:40  24.11.20240926.1925c60  6.10.5
    modules/discord/default.nix
    modules/discord/vesktop/default.nix
    modules/discord/vesktop/disable_update_checking.patch
    modules/discord/vesktop/package.nix
    modules/discord/vesktop/use_system_vencord.patch

--- modules/discord/vesktop/default.nix
+++ modules/discord/vesktop/default.nix
@@ -1,90 +1,14 @@
+{ pkgs, user, ... }:
 {
-  pkgs,
-  user,
-  directories,
-  ...
-}:
+  imports = [
+    ./icons-overlay.nix
+    ./keybinds-overlay.nix
+  ];
 
-{
   home-manager.users.${user} = {
     home.packages = with pkgs; [
       # Discord fork that fixes streaming issues on linux
       vesktop
-      # (pkgs.callPackage ./package.nix { })
     ];
   };
-
-  nixpkgs.overlays = [
-    (final: prev: {
-      vesktop = prev.vesktop.overrideAttrs (
-        finalAttrs: oldAttrs: {
-          src = prev.fetchFromGitHub {
-            owner = "PolisanTheEasyNick";
-            repo = "Vesktop";
-            rev = "3a84dbc0d28a8152284d82004b1315e7fe03778a";
-            hash = "sha256-i+i0oOLST72cMWwtSHJnVDaWojMA3g7TXGvBBewGBcE=";
-          };
-
-          pnpmDeps = prev.pnpm_9.fetchDeps {
-            inherit (finalAttrs)
-              pname
-              version
-              src
-              patches
-              ;
-            hash = "sha256-Y55CeMKXZALes7X8uwsnFI6QVExHU8AbvU/gxCvTLHs=";
-          };
-
-          # patches = [
-          #   (prev.fetchpatch {
-          #     url = "https://patch-diff.githubusercontent.com/raw/Vencord/Vesktop/pull/609.patch";
-          #     hash = "sha256-UaAYbBmMN3/kYVUwNV0/tH7aNZk32JnaUwjsAaZqXwk=";
-          #   })
-          # ];
-
-          # Overwrite the desktop item so the app name is "Discord"
-          desktopItems = (
-            prev.makeDesktopItem {
-              name = "vesktop";
-              desktopName = "Discord";
-              exec = "vesktop %U";
-              icon = "vesktop";
-              startupWMClass = "Discord";
-              genericName = "Internet Messenger";
-              keywords = [
-                "discord"
-                "vencord"
-                "vesktop"
-                "electron"
-                "chat"
-              ];
-              categories = [
-                "Network"
-                "InstantMessaging"
-                "Chat"
-              ];
-            }
-          );
-
-          # Add a prebuild action to overwrite the tray and app icons and the dancing anime gif
-          preBuild =
-            oldAttrs.preBuild
-            + ''
-              cp -f "${directories.images.discord-logo-white}" build/Icon.png
-
-              cp -f "${directories.images.discord-logo-white}" static/icon.png
-              cp -f "${directories.icons.discord}" static/icon.ico
-
-              cp -f "${directories.images.bongo-cat}" static/shiggy.gif
-            '';
-
-          # Add a preinstall action to overwrite the app icons and the dancing anime gif
-          preInstall = ''
-            rm build/icon_*x32.png
-            cp "${directories.images.discord-logo-white}" build/icon_512x512x32.png
-          '';
-        }
-      );
-    })
-  ];
 }
---
+++ modules/discord/vesktop/keybinds-overlay.nix
@@ -0,0 +1,39 @@
+{ ... }:
+{
+  # Overlay to patch global keybinds
+  # TODO: Remove when the PR is merged
+
+  nixpkgs.overlays = [
+    (final: prev: {
+      vesktop = prev.vesktop.overrideAttrs (
+        finalAttrs: oldAttrs: {
+          # Patching isn't working for some reason so just replace the entire source with the PR repo
+          src = prev.fetchFromGitHub {
+            owner = "PolisanTheEasyNick";
+            repo = "Vesktop";
+            rev = "3a84dbc0d28a8152284d82004b1315e7fe03778a";
+            hash = "sha256-i+i0oOLST72cMWwtSHJnVDaWojMA3g7TXGvBBewGBcE=";
+          };
+
+          # patches = [
+          #   (prev.fetchpatch {
+          #     url = "https://patch-diff.githubusercontent.com/raw/Vencord/Vesktop/pull/609.patch";
+          #     hash = "sha256-UaAYbBmMN3/kYVUwNV0/tH7aNZk32JnaUwjsAaZqXwk=";
+          #   })
+          # ];
+
+          # Make sure the dependencies get updated as well
+          pnpmDeps = prev.pnpm_9.fetchDeps {
+            inherit (finalAttrs)
+              pname
+              version
+              src
+              patches
+              ;
+            hash = "sha256-Y55CeMKXZALes7X8uwsnFI6QVExHU8AbvU/gxCvTLHs=";
+          };
+        }
+      );
+    })
+  ];
+}
