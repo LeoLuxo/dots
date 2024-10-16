ba8e9c9763e8301a8259a0224e60c1a1c4d86732
Chloé G <contact@chlookie.net>
2024-10-16 13:06:44 +0200

    pancake 216 current  2024-10-16 13:06:40  24.11.20240926.1925c60  6.10.5
    modules/discord/default.nix
    modules/discord/vesktop/default.nix
    modules/discord/vesktop/disable_update_checking.patch
    modules/discord/vesktop/package.nix
    modules/discord/vesktop/use_system_vencord.patch

---
+++ modules/discord/vesktop/icons-overlay.nix
@@ -0,0 +1,56 @@
+{ directories, ... }:
+{
+  # Overlay to override visual aspects of the app
+
+  nixpkgs.overlays = [
+    (final: prev: {
+      vesktop = prev.vesktop.overrideAttrs (
+        finalAttrs: oldAttrs: {
+          # Overwrite the desktop item so the app name is "Discord"
+          desktopItems = (
+            prev.makeDesktopItem {
+              name = "vesktop";
+              desktopName = "Discord";
+              exec = "vesktop %U";
+              icon = "vesktop";
+              startupWMClass = "Discord";
+              genericName = "Internet Messenger";
+              keywords = [
+                "discord"
+                "vencord"
+                "vesktop"
+                "electron"
+                "chat"
+              ];
+              categories = [
+                "Network"
+                "InstantMessaging"
+                "Chat"
+              ];
+            }
+          );
+
+          # Add a prebuild action to overwrite the tray and app icons and the dancing anime gif
+          preBuild =
+            (oldAttrs.preBuild or "")
+            + ''
+              cp -f "${directories.images.discord-logo-white}" build/Icon.png
+
+              cp -f "${directories.images.discord-logo-white}" static/icon.png
+              cp -f "${directories.icons.discord}" static/icon.ico
+
+              cp -f "${directories.images.bongo-cat}" static/shiggy.gif
+            '';
+
+          # Add a preinstall action to overwrite the app icons and the dancing anime gif
+          preInstall =
+            (oldAttrs.preInstall or "")
+            + ''
+              rm build/icon_*x32.png
+              cp "${directories.images.discord-logo-white}" build/icon_512x512x32.png
+            '';
+        }
+      );
+    })
+  ];
+}
