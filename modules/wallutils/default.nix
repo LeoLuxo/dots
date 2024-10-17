965c5f5da7e986f67ebcd78be3a56495893ada3c
Chloé G <contact@chlookie.net>
2024-10-17 22:46:26 +0200

    pancake 245 current  2024-10-17 22:46:20  24.11.20240926.1925c60  6.10.5
    modules/style/timed-wallpapers.nix
    modules/wallutils/default.nix
    modules/wallutils/module.nix

--- modules/wallutils/default.nix
+++ modules/wallutils/default.nix
@@ -1,7 +1,13 @@
-{ ... }:
+{ pkgs, user, ... }:
 {
   imports = [
     ./module.nix
     ./fix-dark-mode.nix
   ];
+
+  home-manager.users.${user} = {
+    home.packages = with pkgs; [
+      wallutils
+    ];
+  };
 }
