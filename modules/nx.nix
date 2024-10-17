ca9376cc26b813bd51a4d5d493cd7746aa68903e
Chloé G <contact@chlookie.net>
2024-10-17 17:24:51 +0200

    pancake 234 current  2024-10-17 17:24:45  24.11.20240926.1925c60  6.10.5
    modules/nx.nix
    modules/vscode.nix

--- modules/nx.nix
+++ modules/nx.nix
@@ -5,15 +5,15 @@
   ...
 }:
 {
+  environment.variables = {
+    # Set the location of the config repo
+    NX_REPO = "/etc/nixos/dots";
+
+    # Set the location of the file used for dconf-diff
+    DCONF_DIFF = "~/.dconf_activation";
+  };
+
   home-manager.users.${user} = {
-    home.sessionVariables = {
-      # Set the location of the config repo
-      NX_REPO = "/etc/nixos/dots";
-
-      # Set the location of the file used for dconf-diff
-      DCONF_DIFF = "/home/${user}/.dconf_activation";
-    };
-
     # Add all scripts from the nx directory
     home.packages = lib.traceVal (builtins.map (nx: nx { }) (lib.attrsets.attrValues scriptBin.nx));
   };
