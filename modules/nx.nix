ef35f090822409156bf481a9a396e27fbb3033e5
LeoLuxo <leo@gamboa.eu>
2024-10-17 16:32:11 +0200

    pancake 233 current  2024-10-17 16:32:06  24.11.20240926.1925c60  6.10.5
    hosts/pancake/default.nix
    libs.nix
    modules/dconf-diff.nix
    modules/nx.nix
    scripts/dconf-diff.sh
    scripts/nx/nx-code.sh
    scripts/nx/nx-rebuild.sh
    scripts/snip.sh

--- modules/nx.nix
+++ modules/nx.nix
@@ -9,9 +9,13 @@
     home.sessionVariables = {
       # Set the location of the config repo
       NX_REPO = "/etc/nixos/dots";
+
+      # Set the location of the file used for dconf-diff
+      DCONF_DIFF = "/home/${user}/.dconf_activation";
     };
 
     # Add all scripts from the nx directory
     home.packages = lib.traceVal (builtins.map (nx: nx { }) (lib.attrsets.attrValues scriptBin.nx));
   };
+
 }
