9bf6d3e18a7c15d3bfd30108c6bc545296f04728
Chloé G <contact@chlookie.net>
2024-10-18 13:42:51 +0200

    pancake 254 current  2024-10-18 13:42:44  24.11.20240926.1925c60  6.10.5
    hosts/pancake/default.nix
    modules/style/timed-wallpapers.nix
    modules/wallutils/default.nix
    modules/wallutils/module.nix

---
+++ modules/style/wallpapers/default.nix
@@ -0,0 +1,41 @@
+{
+  config,
+  lib,
+  ...
+}:
+
+with lib;
+
+let
+  cfg = config.wallpaper;
+in
+{
+  options.wallpaper = {
+    image = mkOption {
+      type = types.nullOr types.path;
+      default = null;
+    };
+
+    enable = mkOption {
+      type = types.bool;
+      default = cfg.image != null;
+    };
+
+    is-timed = mkOption {
+      type = types.bool;
+      default = strings.hasSuffix ".heic" cfg.image;
+    };
+
+    packages = types.submodule.options {
+      wallutils = mkOption {
+        type = types.package;
+        default = pkgs.wallutils;
+        defaultText = "pkgs.wallutils";
+      };
+    };
+  };
+
+  # config = mkIf cfg.enable {
+  #   wallpaper.is-static = !cfg.is-timed;
+  # };
+}
