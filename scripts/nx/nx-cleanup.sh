bc5de1cf332fd66f645ff4914acada84eca5ab91
Chloé G <contact@chlookie.net>
2024-11-05 15:07:27 +0100

    pancake 273 current  2024-11-05 15:07:22  24.11.20240926.1925c60  6.10.5
    modules/nx.nix
    scripts/nx/nx-cleanup.sh
    scripts/nx/nx-code.sh

--- scripts/nx/nx-cleanup.sh
+++ scripts/nx/nx-cleanup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Hard link nix store
-nix-store --optimise
+sudo nix-store --optimise
 
 # Clean up old NixOS generations and garbage-collect the nix store
 sudo nix-collect-garbage -d --delete-older-than 2d
--- scripts/nx/nx-code.sh
+++ scripts/nx/nx-code.sh
@@ -2,3 +2,4 @@
 
 # Open the config repo in vscode/text editor
 $EDITOR $NX_REPO
+$EDITOR $NX_TODO --reuse-window
