50236df488c10ff7bec9f4191b2092b3c2bb4bd1
Chloé G <contact@leoluxo.eu>
2024-11-06 11:23:45 +0100

    pancake 275 current  2024-11-05 17:52:43  24.11.20240926.1925c60  6.10.5
    modules/direnv.nix

--- modules/direnv.nix
+++ modules/direnv.nix
@@ -1,15 +1,6 @@
-{ pkgs, ... }:
+{ ... }:
 {
-  imports = [ ];
-
-  programs.direnv = {
-    package = pkgs.direnv;
-    silent = false;
-    loadInNixShell = true;
-    direnvrcExtra = "";
-    nix-direnv = {
-      enable = true;
-      package = pkgs.nix-direnv;
-    };
+  programs.direnv.nix-direnv = {
+    enable = true;
   };
 }
