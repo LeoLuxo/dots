7478b38323672d761c938d6cde306d056077081e
Chloé G <contact@chlookie.net>
2024-11-08 11:27:26 +0100

    pancake 281 current  2024-11-08 11:26:32  24.11.20240926.1925c60  6.10.5
    hosts/pancake/default.nix
    modules/dev/coq/default.nix
    modules/dev/coq/vscoq-language-server.nix
    modules/dev/iris/default.nix
    modules/dev/iris/iris-coq.nix

---
+++ modules/dev/coq.nix
@@ -0,0 +1,19 @@
+{ pkgs, user, ... }:
+{
+  imports = [ ];
+  home-manager.users.${user} = {
+    home.packages = with pkgs; [
+      coq
+      coqPackages.vscoq-language-server
+    ];
+  };
+
+  nixpkgs.overlays = [
+    (final: prev: {
+      # Add vscoq language server (for some reason can't fetch the one from nixpkgs?)
+      # vscoq-language-server = pkgs.callPackage ./vscoq-language-server.nix { };
+
+      coqPackages = pkgs.mkCoqPackages pkgs.coq_8_19;
+    })
+  ];
+}
