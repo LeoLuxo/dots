ea80c42137410147ac85c44d241a5b2c5d32ee91
Chloé G <contact@leoluxo.eu>
2024-11-19 11:48:05 +0100

    pancake 285 current  2024-11-19 11:48:03  24.11.20240926.1925c60  6.10.5
    modules/syncthing.nix

---
+++ modules/syncthing/desktop.nix
@@ -0,0 +1,36 @@
+{
+  lib,
+  stdenv,
+  makeDesktopItem,
+  copyDesktopItems,
+  directories,
+}:
+stdenv.mkDerivation (finalAttrs: {
+  pname = "syncthing-desktop-icon";
+  version = "0.0.0";
+
+  dontUnpack = true;
+
+  installPhase = lib.optionalString stdenv.hostPlatform.isLinux ''
+    mkdir -p $out
+
+    install -Dm0644 "${directories.images.syncthing}" $out/share/icons/hicolor/2048x2048/apps/syncthing.png
+  '';
+
+  nativeBuildInputs = [ copyDesktopItems ];
+
+  desktopItems = lib.optional stdenv.hostPlatform.isLinux (makeDesktopItem {
+    name = "syncthing";
+    desktopName = "Syncthing";
+    exec = "firefox \"http://127.0.0.1:8384/\"";
+    icon = "syncthing";
+    keywords = [
+      "syncthing"
+    ];
+    categories = [
+      "Network"
+      "FileTransfer"
+      "P2P"
+    ];
+  });
+})
