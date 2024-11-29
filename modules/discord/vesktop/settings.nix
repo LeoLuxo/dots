cda1d350ea2a038238c7b2a89f66b9f9648fb02f
LeoLuxo <leo@gamboa.eu>
2024-11-29 11:12:18 +0100

    pancake 292 current  2024-11-29 11:12:16  24.11.20240926.1925c60  6.10.5
    modules/discord/vesktop/default.nix

---
+++ modules/discord/vesktop/settings.nix
@@ -0,0 +1,76 @@
+{
+  user,
+  ...
+}:
+{
+  home-manager.users.${user}.xdg.configFile = {
+    "vesktop/settings.json".text = builtins.toJSON {
+      discordBranch = "stable";
+      minimizeToTray = true;
+      arRPC = true;
+      splashColor = "rgb(198, 208, 245)";
+      splashBackground = "rgb(48, 52, 70)";
+      spellCheckLanguages = [ ];
+    };
+
+    "vesktop/settings/settings.json".text = builtins.toJSON {
+      autoUpdate = false;
+      autoUpdateNotification = false;
+      useQuickCss = true;
+      themeLinks = [
+        "https://catppuccin.github.io/discord/dist/catppuccin-frappe-blue.theme.css"
+      ];
+      enabledThemes = [ ];
+      enableReactDevtools = false;
+      frameless = false;
+      transparent = false;
+      winCtrlQ = false;
+      disableMinSize = false;
+      winNativeTitleBar = false;
+      plugins = {
+        CommandsAPI = {
+          enabled = true;
+        };
+        MessageAccessoriesAPI = {
+          enabled = true;
+        };
+        UserSettingsAPI = {
+          enabled = true;
+        };
+        CrashHandler = {
+          enabled = true;
+        };
+        WebKeybinds = {
+          enabled = true;
+        };
+        WebScreenShareFixes = {
+          enabled = true;
+        };
+        NoTrack = {
+          enabled = true;
+          disableAnalytics = true;
+        };
+        WebContextMenus = {
+          enabled = true;
+          addBack = true;
+        };
+        Settings = {
+          enabled = true;
+          settingsLocation = "aboveNitro";
+        };
+      };
+      notifications = {
+        timeout = 5000;
+        position = "bottom-right";
+        useNative = "not-focused";
+        logLimit = 50;
+      };
+      cloud = {
+        authenticated = false;
+        url = "https://api.vencord.dev/";
+        settingsSync = false;
+        settingsSyncVersion = 1730282253502;
+      };
+    };
+  };
+}
