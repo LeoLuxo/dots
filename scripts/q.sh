2cb5571a3722945d615944b1cf7c65e88d9a8512
Chloé G <contact@chlookie.net>
2024-11-30 18:57:38 +0100

    pancake 315 current  2024-11-30 18:57:35  24.11.20240926.1925c60  6.10.5
    M	scripts/q.sh

--- scripts/q.sh
+++ scripts/q.sh
@@ -1,30 +1,24 @@
 #!/usr/bin/env bash
 
+RED='\033[1;31m'
 BLUE='\033[1;34m'
 PURPLE='\033[1;35m'
 RESET='\033[0m'
 
-echo $@
-echo $1
-
 file() {
-	echo file arg: $1
 	echo -e "${PURPLE}File contents:${RESET}"
 	highlight -O ansi --force "$1"
 }
 
 dir() {
-	echo dir arg: $1
 	echo -e "${PURPLE}Directory listing:${RESET}"
 	ls -Fhsla --color=always "$1"
 }
 
 checkpath() {
-	echo checkpath arg: $1
-
 	if [[ -L $1 ]]; then
 		TARGET=$(readlink -f $1)
-		echo -e "${PURPLE}Symlink points to ->\n${BLUE}${TARGET}${RESET}"
+		echo -e "${PURPLE}-> ${BLUE}${TARGET}${RESET}"
 
 		if [[ -d $TARGET ]]; then
 			dir "$TARGET"
