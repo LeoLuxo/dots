bd4207017828913c55b51214d5da7e1d1a65cac8
LeoLuxo <leo@gamboa.eu>
2024-11-30 18:11:21 +0100

    pancake 306 current  2024-11-30 18:11:18  24.11.20240926.1925c60  6.10.5
    M	modules/terminal-utils.nix
    M	scripts/q.sh

--- scripts/q.sh
+++ scripts/q.sh
@@ -1,10 +1,27 @@
 #!/usr/bin/env bash
 
-if [[ -d $1 ]]; then
-	echo "$1 is a directory"
-elif [[ -f $1 ]]; then
-	echo "$1 is a file"
-else
-	echo "$1 is not valid"
-	exit 1
-fi
+BLUE='\033[1;34m'
+PURPLE='\033[1;35m'
+RESET='\033[0m'
+
+checkfile() {
+	if [[ -L $1 ]]; then
+		TARGET=$(readlink -f $1)
+		echo -e "${PURPLE}Symlink points to ->\n${BLUE}${TARGET}${RESET}"
+
+		checkfile $TARGET
+	elif [[ -d $1 ]]; then
+		echo -e "${PURPLE}Directory listing:${RESET}"
+
+		ls -Fhsla
+	elif [[ -f $1 ]]; then
+		echo -e "${PURPLE}File contents:${RESET}"
+
+		highlight -O ansi --force
+	else
+		echo "$1 is not valid"
+		exit 1
+	fi
+}
+
+checkfile $1
