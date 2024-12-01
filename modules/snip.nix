641b3af99ba9dee5db9ef28e8534ee0631c70650
LeoLuxo <leo@gamboa.eu>
2024-12-01 18:52:51 +0100

    pancake 324 current  2024-12-01 18:52:46  24.11.20240926.1925c60  6.10.5
    M	hosts/pancake/default.nix
    M	modules/snip.nix
    A	modules/wl-clipboard.nix
    M	scripts/snip.sh

--- modules/snip.nix
+++ modules/snip.nix
@@ -16,11 +16,6 @@
 
   home-manager.users.${user} = {
     home.packages = with pkgs; [
-      # Not putting these deps in the script because I don't want to wait to screenshot if they're missing
-      # gnome-screenshot
-      # wl-clipboard
-
-      # The script
       (scriptBin.snip {
         deps = [
           gnome-screenshot
