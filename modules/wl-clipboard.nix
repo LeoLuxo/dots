fa87c5a4272fd79547dd7e88f64b2b5925b18172
LeoLuxo <leo@gamboa.eu>
2024-12-01 19:01:12 +0100

    pancake 325 current  2024-12-01 19:01:07  24.11.20240926.1925c60  6.10.5
    M	modules/nx.nix
    M	modules/wl-clipboard.nix
    D	scripts/nx/nx-cd.sh

--- modules/wl-clipboard.nix
+++ modules/wl-clipboard.nix
@@ -1,25 +1,17 @@
 {
   pkgs,
   user,
-  writeScriptWithDeps,
   ...
 }:
 {
   home-manager.users.${user} = {
     home.packages = with pkgs; [
       wl-clipboard
-
-      (writeScriptWithDeps {
-        name = "copy";
-        text = ''wl-copy $@'';
-        deps = [ wl-clipboard ];
-      })
-
-      (writeScriptWithDeps {
-        name = "paste";
-        text = ''wl-paste $@'';
-        deps = [ wl-clipboard ];
-      })
     ];
+
+    home.shellAliases = {
+      copy = "wl-copy";
+      paste = "wl-paste";
+    };
   };
 }
