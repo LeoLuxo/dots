265a2acb860f6dbcc1d78e2dd44299c5bf4aa424
Chloé G <contact@chlookie.net>
2024-12-03 23:04:31 +0100

    pancake 348 current  2024-12-03 23:04:27  24.11.20240926.1925c60  6.10.5
    M	hosts/pancake/default.nix
    D	modules/neofetch.nix
    M	modules/nix-utils.nix
    D	scripts/nx/,,.sh
    M	scripts/nx/nx-cleanup.sh
    D	scripts/please.sh

--- scripts/nx/nx-cleanup.sh
+++ scripts/nx/nx-cleanup.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 # Clean up old NixOS generations and garbage-collect the nix store
-sudo nix-collect-garbage -d --delete-older-than 2d
+nh clean all --keep 10 --keep-since 7d
+# sudo nix-collect-garbage -d --delete-older-than 2d
 
 # Hard link nix store
 sudo nix-store --optimise
