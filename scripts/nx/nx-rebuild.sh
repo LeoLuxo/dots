6f07472be5758b36c3ceb226dba531642de701e3
LeoLuxo <leo@gamboa.eu>
2024-12-03 19:00:32 +0100

    pancake 336 current  2024-12-03 19:00:28  24.11.20240926.1925c60  6.10.5
    M	scripts/nx/nx-rebuild.sh

--- scripts/nx/nx-rebuild.sh
+++ scripts/nx/nx-rebuild.sh
@@ -50,7 +50,7 @@ echo -e "${CYAN}NixOS Rebuilding...${RESET}"
 # Rebuild, and if errors occur make sure to exit
 # tarball-ttl 0 forces the tarball cache to be stale and re-downloaded
 # warn dirty disables the goddamn git dirty tree message
-nh os switch . -- \
+nh os switch . --no-nom -- \
 	--impure \
 	--option tarball-ttl 0 \
 	--option warn-dirty false \
