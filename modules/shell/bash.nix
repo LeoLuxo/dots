e0d888c4ae1824de2b3babad089f1c7da52fb08a
LeoLuxo <leo@gamboa.eu>
2024-12-04 19:46:32 +0100

    pancake 357 current  2024-12-04 19:46:28  24.11.20240926.1925c60  6.10.5
    M	modules/shell/bash.nix
    M	modules/shell/zsh.nix

--- modules/shell/bash.nix
+++ modules/shell/bash.nix
@@ -1,8 +1,16 @@
 {
   user,
+  pkgs,
+  lib,
   ...
 }:
+let
+  inherit (pkgs) bash;
+in
 {
+  users.defaultUserShell = lib.mkDefault bash;
+  environment.shells = [ bash ];
+
   home-manager.users.${user} = {
     # Let home manager manage bash; needed to set sessionVariables
     programs.bash.enable = true;
--- modules/shell/zsh.nix
+++ modules/shell/zsh.nix
@@ -1,9 +1,22 @@
 {
   user,
+  pkgs,
   ...
 }:
+let
+  inherit (pkgs) zsh;
+in
 {
-  home-manager.users.${user} = {
-    programs.zsh.enable = true;
+  imports = [ ./bash.nix ];
+
+  users.defaultUserShell = zsh;
+  environment.shells = [ zsh ];
+
+  programs.zsh = {
+    enable = true;
+
+    enableCompletion = true;
+    autosuggestions.enable = true;
+    syntaxHighlighting.enable = true;
   };
 }
