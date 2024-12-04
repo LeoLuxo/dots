09e6a328d845e2daada916369dbc93129df4163e
LeoLuxo <leo@gamboa.eu>
2024-12-04 19:56:14 +0100

    pancake 358 current  2024-12-04 19:51:31  24.11.20240926.1925c60  6.10.5
    M	modules/shell/prompt/ohmyposh/default.nix
    M	modules/shell/zsh.nix

--- modules/shell/prompt/ohmyposh/default.nix
+++ modules/shell/prompt/ohmyposh/default.nix
@@ -7,6 +7,11 @@
     programs.oh-my-posh = {
       enable = true;
       settings = import ./theme.nix;
+
+      enableBashIntegration = true;
+      enableZshIntegration = true;
+      enableNushellIntegration = true;
+      enableFishIntegration = true;
     };
   };
 }
