a213a165437a4f09b16f31e56316943414cc6ed3
LeoLuxo <leo@gamboa.eu>
2024-12-04 19:30:31 +0100

    pancake 355 current  2024-12-04 19:30:27  24.11.20240926.1925c60  6.10.5
    M	modules/ohmyposh/default.nix
    A	modules/ohmyposh/theme.nix
    D	modules/ohmyposh/themes/atomic.omp.json
    D	modules/ohmyposh/themes/catppuccin.omp.json
    D	modules/ohmyposh/themes/catppuccin_frappe.omp.json
    D	modules/ohmyposh/themes/clean-detailed.omp.json
    D	modules/ohmyposh/themes/hul10.omp.json
    D	modules/ohmyposh/themes/hunk.omp.json
    D	modules/ohmyposh/themes/peppy.omp.json

--- modules/ohmyposh/default.nix
+++ modules/ohmyposh/default.nix
@@ -2,16 +2,11 @@
   user,
   ...
 }:
-
-let
-  theme = ./themes/peppy.omp.json;
-in
 {
   home-manager.users.${user} = {
     programs.oh-my-posh = {
       enable = true;
-      # settings = builtins.fromJSON (builtins.readFile theme);
-      useTheme = "catpuccin_frappe";
+      settings = import ./theme.nix;
     };
   };
 }
