33e1f2929e830f68590d3ef52d4a9c64fb262bb5
LeoLuxo <leo@gamboa.eu>
2024-12-05 20:10:26 +0100

    coffee 10 current  2024-12-05 20:10:26  24.11.20240926.1925c60   6.6.52
    M	hosts/coffee/default.nix

--- hosts/coffee/default.nix
+++ hosts/coffee/default.nix
@@ -1,4 +1,9 @@
-{ directories, ... }:
+{
+  user,
+  directories,
+  pkgs,
+  ...
+}:
 {
   imports = with directories.modules; [
     # Include local modules
@@ -8,15 +13,60 @@
     # Include global modules
     autologin
 
+    # style.bootscreen
+    style.fonts
+    style.wallpapers
+    style.themes.catppuccin
+
     gnome.gnome
+    gnome.extensions.blur-my-shell
+    gnome.extensions.clipboard
+    gnome.extensions.gsconnect
+    gnome.extensions.just-perfection
+    gnome.extensions.rounded-corners
+    # gnome.extensions.emojis
+    # gnome.extensions.weather
+
+    terminal.ddterm
+    shell.prompt.starship
+    shell.bash
+    shell.fish
+    # shell.nushell
 
     nix-utils
     snip
     terminal-utils
     wl-clipboard
 
+    obsidian
     firefox
-    git
+    discord
     vscode
+    git
+    steam
   ];
+
+  # Set wallpaper  
+  wallpaper = {
+    enable = true;
+    image = directories.wallpapers."Tree and shore";
+  };
+
+  # Set default shell
+  shell.default = "fish";
+
+  # Home-Manager config
+  home-manager.users.${user} = {
+
+    # Extra packages that don't necessarily need an entire dedicated module
+    home.packages = with pkgs; [
+      bitwarden-desktop
+
+      textpieces
+      hieroglyphic
+
+      # Original electron teams package was abandoned
+      teams-for-linux
+    ];
+  };
 }
