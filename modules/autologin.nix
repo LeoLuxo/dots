e26241de7dce6bb4357d372131eece0ed60f9162
Chloé G <contact@chlookie.net>
2024-12-05 19:49:19 +0100

    pancake 367 current  2024-12-05 19:37:20  24.11.20240926.1925c60  6.10.5
    M	hosts/coffee/default.nix
    R099	hosts/coffee/hardware-configuration.nix	hosts/coffee/hardware.nix
    D	hosts/coffee/old-configuration.nix
    A	hosts/coffee/system.nix
    M	hosts/pancake/default.nix
    D	hosts/pancake/hardware-configuration.nix
    M	hosts/pancake/hardware.nix
    A	hosts/pancake/system.nix
    A	modules/autologin.nix

---
+++ modules/autologin.nix
@@ -0,0 +1,13 @@
+{
+  user,
+  ...
+}:
+{
+  # Enable automatic login for the user.
+  services.displayManager.autoLogin.enable = true;
+  services.displayManager.autoLogin.user = user;
+
+  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
+  systemd.services."getty@tty1".enable = false;
+  systemd.services."autovt@tty1".enable = false;
+}
