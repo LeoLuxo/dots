016117592b1095b49a7f2cd1afbc31a495aba9fb
LeoLuxo <leo@gamboa.eu>
2024-12-05 19:49:19 +0100

    pancake 367 current  2024-12-05 19:37:20  24.11.20240926.1925c60  6.10.5
    M	hosts/coffee/default.nix
    R099	hosts/coffee/hardware-configuration.nix	hosts/coffee/hardware.nix
    D	hosts/coffee/old-configuration.nix
    A	hosts/coffee/system.nix
    M	hosts/pancake/default.nix
    D	hosts/pancake/hardware-configuration.nix
    M	hosts/pancake/hardware.nix
    A	hosts/pancake/system.nix
    A	modules/autologin.nix

---
+++ hosts/coffee/system.nix
@@ -0,0 +1,67 @@
+{ ... }:
+{
+
+  # Bootloader
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Enable networking with networkmanager
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Copenhagen";
+
+  # Select internationalisation properties.
+  # Use english (Ireland); see: 
+  # https://unix.stackexchange.com/questions/62316/why-is-there-no-euro-english-locale
+  i18n.defaultLocale = "en_IE.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_IE.UTF-8";
+    LC_IDENTIFICATION = "en_IE.UTF-8";
+    LC_MEASUREMENT = "en_IE.UTF-8";
+    LC_MONETARY = "en_IE.UTF-8";
+    LC_NAME = "en_IE.UTF-8";
+    LC_NUMERIC = "en_IE.UTF-8";
+    LC_PAPER = "en_IE.UTF-8";
+    LC_TELEPHONE = "en_IE.UTF-8";
+    LC_TIME = "en_IE.UTF-8";
+  };
+
+  # Enable and configure the X11 windowing system.
+  services.xserver = {
+    enable = true;
+
+    # Configure keymap in X11
+    xkb = {
+      layout = "us";
+      variant = "altgr-intl";
+    };
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+}
