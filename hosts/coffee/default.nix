da95b209ba3c4d997c4c6aa67346dca7a19846c1
Chloé G <contact@leoluxo.eu>
2024-12-05 19:57:10 +0100

    coffee 8 current   2024-12-05 19:57:10  24.11.20240926.1925c60   6.6.52
    M	hosts/coffee/default.nix

--- hosts/coffee/default.nix
+++ hosts/coffee/default.nix
@@ -1,19 +1,22 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { directories, ... }:
 {
   imports = with directories.modules; [
     # Include local modules
     ./hardware.nix
     ./system.nix
-    ./syncthing.nix
-    ./wifi.nix
 
     # Include global modules
     autologin
 
     gnome.gnome
+
+    vscode
+
+    nix-utils
+    snip
+    terminal-utils
+    wl-clipboard
+
+    git
   ];
 }
