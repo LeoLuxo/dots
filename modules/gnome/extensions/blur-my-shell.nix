99faed43c42b30e319021abb12fa6b00df6a085b
Chloé G <contact@chlookie.net>
2024-12-06 10:26:56 +0100

    coffee 21 current  2024-12-06 10:26:55  24.11.20240926.1925c60   6.6.52
    M	hosts/coffee/default.nix
    M	modules/gnome/extensions/blur-my-shell.nix

--- modules/gnome/extensions/blur-my-shell.nix
+++ modules/gnome/extensions/blur-my-shell.nix
@@ -1,78 +1,107 @@
 {
+  config,
   pkgs,
   user,
   lib,
   ...
 }:
 
-with lib;
-
 {
-  programs.dconf.enable = true;
-
-  home-manager.users.${user} =
-    { lib, ... }:
-    # with lib.hm.gvariant;
-    {
-      home.packages = with pkgs; [
-        gnomeExtensions.blur-my-shell
-      ];
-
-      dconf.settings = {
-        "org/gnome/shell" = {
-          enabled-extensions = [ "blur-my-shell@aunetx" ];
-        };
-
-        "org/gnome/shell/extensions/blur-my-shell/appfolder" = {
-          blur = true;
-          brightness = 0.6;
-          sigma = 30;
-        };
-
-        "org/gnome/shell/extensions/blur-my-shell/dash-to-dock" = {
-          blur = false;
-          brightness = 0.6;
-          pipeline = "pipeline_default_rounded";
-          sigma = 30;
-          static-blur = true;
-          style-dash-to-dock = 0;
-        };
-
-        "org/gnome/shell/extensions/blur-my-shell/hidetopbar" = {
-          compatibility = false;
-        };
-
-        "org/gnome/shell/extensions/blur-my-shell/lockscreen" = {
-          pipeline = "pipeline_default";
-        };
-
-        "org/gnome/shell/extensions/blur-my-shell/overview" = {
-          blur = true;
-          pipeline = "pipeline_default";
-          style-components = 1;
-        };
-
-        "org/gnome/shell/extensions/blur-my-shell/panel" = {
-          blur = true;
-          brightness = 0.6;
-          force-light-text = false;
-          override-background-dynamically = false;
-          pipeline = "pipeline_default";
-          sigma = 30;
-          static-blur = true;
-          style-panel = 0;
-          unblur-in-overview = true;
-        };
-
-        "org/gnome/shell/extensions/blur-my-shell/screenshot" = {
-          blur = true;
-          pipeline = "pipeline_default";
-        };
-
-        "org/gnome/shell/extensions/blur-my-shell/window-list" = {
-          brightness = 1.0;
-          sigma = 30;
+  options.gnome.blur-my-shell = with lib; {
+    enable = mkOption {
+      type = types.bool;
+      default = true;
+    };
+    app-blur = mkOption {
+      type = types.submodule {
+        options = {
+          enable = mkOption {
+            type = types.bool;
+            default = false;
+          };
         };
       };
+    };
+  };
+
+  config =
+    with lib;
+    let
+      cfg = config.gnome.blur-my-shell;
+    in
+    mkIf cfg.enable {
+      programs.dconf.enable = true;
+
+      home-manager.users.${user} =
+        { lib, ... }:
+        # with lib.hm.gvariant;
+        {
+          home.packages = with pkgs; [
+            gnomeExtensions.blur-my-shell
+          ];
+
+          dconf.settings = {
+            "org/gnome/shell" = {
+              enabled-extensions = [ "blur-my-shell@aunetx" ];
+            };
+
+            "org/gnome/shell/extensions/blur-my-shell/appfolder" = {
+              blur = true;
+              brightness = 0.6;
+              sigma = 30;
+            };
+
+            "org/gnome/shell/extensions/blur-my-shell/dash-to-dock" = {
+              blur = false;
+              brightness = 0.6;
+              pipeline = "pipeline_default_rounded";
+              sigma = 30;
+              static-blur = true;
+              style-dash-to-dock = 0;
+            };
+
+            "org/gnome/shell/extensions/blur-my-shell/hidetopbar" = {
+              compatibility = false;
+            };
+
+            "org/gnome/shell/extensions/blur-my-shell/lockscreen" = {
+              pipeline = "pipeline_default";
+            };
+
+            "org/gnome/shell/extensions/blur-my-shell/overview" = {
+              blur = true;
+              pipeline = "pipeline_default";
+              style-components = 1;
+            };
+
+            "org/gnome/shell/extensions/blur-my-shell/panel" = {
+              blur = true;
+              brightness = 0.6;
+              force-light-text = false;
+              override-background-dynamically = false;
+              pipeline = "pipeline_default";
+              sigma = 30;
+              static-blur = true;
+              style-panel = 0;
+              unblur-in-overview = true;
+            };
+
+            "org/gnome/shell/extensions/blur-my-shell/screenshot" = {
+              blur = true;
+              pipeline = "pipeline_default";
+            };
+
+            "org/gnome/shell/extensions/blur-my-shell/window-list" = {
+              brightness = 1.0;
+              sigma = 30;
+            };
+
+            "org/gnome/shell/extensions/blur-my-shell/applications" = {
+              blur = cfg.app-blur.enable;
+              dynamic-opacity = true;
+              enable-all = true;
+            };
+          };
+        };
     };
 }
