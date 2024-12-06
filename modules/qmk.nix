3b16bb509920b6b3d3ee027009518a09ee15ee0e
LeoLuxo <leo@gamboa.eu>
2024-12-06 13:42:13 +0100

    coffee 27 current  2024-12-06 13:42:12  24.11.20240926.1925c60   6.6.52
    M	hosts/coffee/default.nix
    A	modules/qmk.nix

---
+++ modules/qmk.nix
@@ -0,0 +1,18 @@
+{
+  pkgs,
+  user,
+  ...
+}:
+{
+  # https://nixos.wiki/wiki/Qmk
+
+  # Gives access to the keyboard as non-root user
+  hardware.keyboard.qmk.enable = true;
+
+  # QMK CLI
+  home-manager.users.${user} = {
+    home.packages = with pkgs; [
+      qmk
+    ];
+  };
+}
