1131f5a6ffa1a5b41855cda0f55cf072f98eeafa
Chloé G <contact@leoluxo.eu>
2024-12-06 16:28:41 +0100

    coffee 34 current  2024-12-06 16:28:40  24.11.20240926.1925c60   6.6.52
    A	hosts/coffee/audio.nix
    M	hosts/coffee/default.nix
    M	hosts/coffee/system.nix
    D	modules/music.nix

--- hosts/coffee/system.nix
+++ hosts/coffee/system.nix
@@ -1,9 +1,5 @@
-{ directories, config, ... }:
+{ config, ... }:
 {
-  imports = with directories.modules; [
-    music
-  ];
-
   # 1TB SSD
   fileSystems."/stuff" = {
     device = "/dev/disk/by-label/stuff";
@@ -102,22 +98,6 @@
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
