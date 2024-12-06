327c4fb699757c9a7ba9b164913699caff86c992
Chloé G <contact@leoluxo.eu>
2024-12-06 13:48:42 +0100

    coffee 28 current  2024-12-06 13:48:41  24.11.20240926.1925c60   6.6.52
    M	hosts/coffee/default.nix
    M	modules/gnome/extensions/system-monitor.nix

--- modules/gnome/extensions/system-monitor.nix
+++ modules/gnome/extensions/system-monitor.nix
@@ -22,14 +22,14 @@ with lib;
         "org/gnome/shell" = {
           enabled-extensions = [ "system-monitor@gnome-shell-extensions.gcampax.github.com" ];
         };
-      };
 
-      "org/gnome/shell/extensions/system-monitor" = {
-        show-cpu = true;
-        show-memory = true;
-        show-swap = false;
-        show-upload = true;
-        show-download = true;
+        "org/gnome/shell/extensions/system-monitor" = {
+          show-cpu = true;
+          show-memory = true;
+          show-swap = false;
+          show-upload = true;
+          show-download = true;
+        };
       };
     };
 }
