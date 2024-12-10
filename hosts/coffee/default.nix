fd6a30e20caff830ccbc618d72b5d418b3954b4c
Chloé G <contact@leoluxo.eu>
2024-12-10 12:03:35 +0100

    coffee 46 current  2024-12-10 12:00:03  24.11.20240926.1925c60   6.6.52
    M	hosts/coffee/default.nix
    A	hosts/coffee/software.nix
    M	hosts/pancake/default.nix
    A	hosts/pancake/software.nix

--- hosts/coffee/default.nix
+++ hosts/coffee/default.nix
@@ -1,80 +1,14 @@
 {
-  user,
-  directories,
-  pkgs,
   ...
 }:
 {
-  imports = with directories.modules; [
-    # Include local modules
+  # Include local modules
+  imports = [
+    ./software.nix
     ./hardware.nix
     ./system.nix
     ./syncthing.nix
+
     ./audio.nix
-
-    # Include global modules
-    style.bootscreen
-    style.fonts
-    style.wallpapers
-    style.themes.catppuccin
-
-    gnome.gnome
-    gnome.extensions.blur-my-shell
-    gnome.extensions.clipboard
-    gnome.extensions.gsconnect
-    gnome.extensions.just-perfection
-    gnome.extensions.rounded-corners
-    gnome.extensions.system-monitor
-    # gnome.extensions.emojis
-    # gnome.extensions.weather
-
-    terminal.ddterm
-    shell.prompt.starship
-    shell.bash
-    shell.fish
-    # shell.nushell
-
-    nix-utils
-    snip
-    terminal-utils
-    wl-clipboard
-    boot-windows
-
-    obsidian
-    firefox
-    discord
-    vscode
-    git
-    steam
-    qmk
   ];
-
-  # Extra packages that don't necessarily need an entire dedicated module
-  home-manager.users.${user}.home.packages = with pkgs; [
-    bitwarden-desktop
-
-    textpieces
-    hieroglyphic
-
-    teams-for-linux
-
-    guitarix
-  ];
-
-  # Set wallpaper  
-  wallpaper = {
-    enable = true;
-    image = directories.wallpapers."Riverside";
-  };
-
-  # Set default shell
-  shell.default = "fish";
-
-  gnome.blur-my-shell = {
-    # Enable blur for all applications
-    app-blur.enable = true;
-
-    # Set hacks to 1
-    hacks-level = "no artifact";
-  };
 }
---
+++ hosts/pancake/software.nix
@@ -0,0 +1,67 @@
+{
+  pkgs,
+  directories,
+  user,
+  ...
+}:
+{
+  imports = with directories.modules; [
+
+    # Include global modules
+    style.bootscreen
+    style.fonts
+    style.wallpapers
+    style.themes.catppuccin
+
+    gnome.gnome
+    gnome.extensions.blur-my-shell
+    gnome.extensions.clipboard
+    gnome.extensions.gsconnect
+    gnome.extensions.just-perfection
+    gnome.extensions.rounded-corners
+    # gnome.extensions.emojis
+    # gnome.extensions.weather
+
+    terminal.ddterm
+    shell.prompt.starship
+    shell.bash
+    shell.fish
+    # shell.nushell
+
+    # translation.deepl
+    translation.gnome-dialect
+
+    nix-utils
+    snip
+    terminal-utils
+    wl-clipboard
+
+    obsidian
+    firefox
+    discord
+    vscode
+    git
+    steam
+  ];
+
+  # Set wallpaper  
+  wallpaper = {
+    enable = true;
+    image = directories.wallpapers."Tree and shore";
+  };
+
+  # Set default shell
+  shell.default = "fish";
+
+  # Extra packages that don't necessarily need an entire dedicated module
+  home-manager.users.${user}.home.packages = with pkgs; [
+    bitwarden-desktop
+
+    textpieces
+    hieroglyphic
+
+    # Original electron teams package was abandoned
+    teams-for-linux
+  ];
+
+}
