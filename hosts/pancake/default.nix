d8b3c35f67c852763f5f63a7c2b1eea7927c43d4
LeoLuxo <leo@gamboa.eu>
2024-12-10 12:03:35 +0100

    coffee 46 current  2024-12-10 12:00:03  24.11.20240926.1925c60   6.6.52
    M	hosts/coffee/default.nix
    A	hosts/coffee/software.nix
    M	hosts/pancake/default.nix
    A	hosts/pancake/software.nix

--- hosts/pancake/default.nix
+++ hosts/pancake/default.nix
@@ -1,72 +1,14 @@
 {
-  pkgs,
-  directories,
-  user,
   ...
 }:
 {
-  imports = with directories.modules; [
-    # Include local modules
+  # Include local modules
+  imports = [
+    ./software.nix
     ./hardware.nix
     ./system.nix
     ./syncthing.nix
+
     ./wifi.nix
-
-    # Include global modules
-    style.bootscreen
-    style.fonts
-    style.wallpapers
-    style.themes.catppuccin
-
-    gnome.gnome
-    gnome.extensions.blur-my-shell
-    gnome.extensions.clipboard
-    gnome.extensions.gsconnect
-    gnome.extensions.just-perfection
-    gnome.extensions.rounded-corners
-    # gnome.extensions.emojis
-    # gnome.extensions.weather
-
-    terminal.ddterm
-    shell.prompt.starship
-    shell.bash
-    shell.fish
-    # shell.nushell
-
-    # translation.deepl
-    translation.gnome-dialect
-
-    nix-utils
-    snip
-    terminal-utils
-    wl-clipboard
-
-    obsidian
-    firefox
-    discord
-    vscode
-    git
-    steam
   ];
-
-  # Set wallpaper  
-  wallpaper = {
-    enable = true;
-    image = directories.wallpapers."Tree and shore";
-  };
-
-  # Set default shell
-  shell.default = "fish";
-
-  # Extra packages that don't necessarily need an entire dedicated module
-  home-manager.users.${user}.home.packages = with pkgs; [
-    bitwarden-desktop
-
-    textpieces
-    hieroglyphic
-
-    # Original electron teams package was abandoned
-    teams-for-linux
-  ];
-
 }
