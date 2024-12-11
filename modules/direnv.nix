95d51ad032b23cde80114267bcaace8d9dc1ea54
LeoLuxo <leo@gamboa.eu>
2024-12-11 15:28:48 +0100

    coffee 60 current  2024-12-11 15:28:46  24.11.20240926.1925c60   6.6.52
    M	modules/direnv.nix

--- modules/direnv.nix
+++ modules/direnv.nix
@@ -9,14 +9,6 @@
     direnvrcExtra = ''
       export NIX_CONFIG="warn-dirty = false"
       echo -e "\033[1;96mDirenv loaded"
-
-      __previous_dir="$PWD"
-      export PROMPT_COMMAND='
-        if [[ "$PWD" != "$__previous_dir" ]]; then
-          echo -e "\033[1;91mDirenv unloaded"
-          __previous_dir="$PWD"
-        fi
-      ':
     '';
   };
 }
