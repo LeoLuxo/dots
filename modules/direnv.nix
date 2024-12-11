6e6e91087f9d4b6fcd3c75537b8bc559b716a2be
Chloé G <contact@leoluxo.eu>
2024-12-11 15:33:23 +0100

    coffee 61 current  2024-12-11 15:33:22  24.11.20240926.1925c60   6.6.52
    M	modules/direnv.nix

--- modules/direnv.nix
+++ modules/direnv.nix
@@ -7,7 +7,12 @@
 
     silent = true;
     direnvrcExtra = ''
+      # Suppress the "git tree is dirty" warnings
       export NIX_CONFIG="warn-dirty = false"
+
+      # Set a higher timeout to prevent the warning "direnv is taking too long"
+      export DIR_ENV_TIMEOUT=600000
+
       echo -e "\033[1;96mDirenv loaded"
     '';
   };
