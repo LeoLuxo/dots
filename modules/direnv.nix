fd14222e4dc5d8b808f1efcec8cced8662dd921f
LeoLuxo <leo@gamboa.eu>
2024-12-11 15:13:25 +0100

    coffee 56 current  2024-12-11 15:13:24  24.11.20240926.1925c60   6.6.52
    M	modules/direnv.nix

--- modules/direnv.nix
+++ modules/direnv.nix
@@ -2,8 +2,13 @@
 {
   programs.direnv = {
     enable = true;
-    nix-direnv = {
-      enable = true;
-    };
+
+    nix-direnv.enable = true;
+
+    silent = true;
+    direnvrcExtra = ''
+      export NIX_CONFIG="warn-dirty = false"
+      echo -e "\033[1;96mDirenv reloaded"
+    '';
   };
 }
