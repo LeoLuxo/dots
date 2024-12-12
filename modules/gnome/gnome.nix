afff2f4e45b5a222d6b17999bea44237b95aaca8
Chloé G <contact@chlookie.net>
2024-12-12 10:54:34 +0100

    coffee 83 current  2024-12-12 10:54:32  24.11.20240926.1925c60   6.6.52
    M	modules/gnome/gnome.nix
    M	modules/styling/wallpaper/default.nix

--- modules/gnome/gnome.nix
+++ modules/gnome/gnome.nix
@@ -178,7 +178,7 @@ in
             "org/gnome/mutter" = {
               edge-tiling = true;
               dynamic-workspaces = true;
-              workspaces-only-on-primary = true;
+              workspaces-only-on-primary = false;
             };
 
             "org/gnome/shell/window-switcher" = {
