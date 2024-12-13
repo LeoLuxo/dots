b6dfc90bca476efe9f9b784d40e37c446b99d4dc
Chloé G <contact@leoluxo.eu>
2024-12-13 22:48:43 +0100

    coffee 115 current  2024-12-13 22:48:41  24.11.20240926.1925c60  6.6.52
    M	modules/shell/fish.nix

--- modules/shell/fish.nix
+++ modules/shell/fish.nix
@@ -26,13 +26,16 @@ in
 
       shellInit = ''
         # Bind CTRL-backspace 
-        bind \cH backward-kill-path-component
+        bind -k backspace backward-kill-path-component
 
         # Bind CTRL-W
         bind \cw backward-kill-bigword
 
         # Bind CTRL-Delete
         bind \e\[3\;5~ kill-bigword
+
+        # Rebind left to accept only a single word instead of the entire autosuggestion
+        bind \e\[C forward-word
       '';
     };
   };
