81be436d11005fbc10bcbabfca24d7664aff5901
Chloé G <contact@chlookie.net>
2024-12-13 20:09:37 +0100

    coffee 103 current  2024-12-13 20:04:12  24.11.20240926.1925c60  6.6.52
    A	modules/goldwarden.nix

---
+++ modules/goldwarden.nix
@@ -0,0 +1,20 @@
+{
+  extra-libs,
+  ...
+}:
+let
+  inherit (extra-libs) mkGlobalKeybind;
+in
+{
+  imports = [
+    (mkGlobalKeybind {
+      name = "Goldwarden";
+      binding = "<Super>u";
+      command = "dbus-send --type=method_call --dest=com.quexten.Goldwarden.autofill /com/quexten/Goldwarden com.quexten.Goldwarden.Autofill.autofill";
+    })
+  ];
+
+  programs.goldwarden = {
+    enable = true;
+  };
+}
