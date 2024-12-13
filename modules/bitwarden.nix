5e66ecb61a509fa1cc2a470cd0825e7c9d056dbb
LeoLuxo <leo@gamboa.eu>
2024-12-13 21:50:32 +0100

    coffee 106 current  2024-12-13 21:50:31  24.11.20240926.1925c60  6.6.52
    M	hosts/coffee/audio.nix
    M	modules/bitwarden.nix
    M	modules/gnome/gnome.nix

--- modules/bitwarden.nix
+++ modules/bitwarden.nix
@@ -1,16 +1,12 @@
 {
   pkgs,
-  directories,
   constants,
-  extra-libs,
   ...
 }:
 let
   inherit (constants) user;
-  inherit (extra-libs) ;
 in
 {
-  imports = with directories.modules; [ ];
   home-manager.users.${user} = {
     home.packages = with pkgs; [ bitwarden-desktop ];
   };
