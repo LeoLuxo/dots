072b8dbfa1d65d59a46541ced09dc4e74b1c03e7
Chloé G <contact@leoluxo.eu>
2024-12-14 19:56:16 +0100

    coffee 124 current  2024-12-14 19:56:14  24.11.20240926.1925c60  6.6.52
    D	modules/discord/vesktop/disable-update-check.nix
    M	modules/styling/wallpaper/default.nix
    A	modules/styling/wallpaper/fix_dark_mode.patch
    D	modules/styling/wallpaper/wallutils-fix-dark-mode.nix

--- modules/styling/wallpaper/default.nix
+++ modules/styling/wallpaper/default.nix
@@ -7,7 +7,7 @@
 }:
 
 let
-  inherit (extra-libs) sanitizePath;
+  inherit (extra-libs) sanitizePath quickPatch;
   inherit (lib)
     options
     types
@@ -26,7 +26,15 @@ let
     };
 in
 {
-  imports = [ ./wallutils-fix-dark-mode.nix ];
+  imports = [
+    # Patch to fix the bug where settimed doesn't work for the dark theme of gnome
+    # https://github.com/xyproto/wallutils/issues/44
+    # TODO: Remove when the issue gets fixed
+    (quickPatch {
+      package = "wallutils";
+      patches = [ ./fix_dark_mode.patch ];
+    })
+  ];
 
   options.styling.wallpaper = {
     enable = options.mkOption {
