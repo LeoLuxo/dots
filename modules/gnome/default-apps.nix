7ae7bd3dcf5862a563196e9f8dbd6044c7a27764
LeoLuxo <leo@gamboa.eu>
2024-12-14 21:46:14 +0100

    coffee 131 current  2024-12-14 21:46:11  24.11.20240926.1925c60  6.6.52
    M	hosts/coffee/software.nix
    M	modules/discord/vesktop/default.nix
    A	modules/gnome/default-apps.nix
    M	modules/gnome/gnome.nix
    A	modules/gnome/settings.nix

---
+++ modules/gnome/default-apps.nix
@@ -0,0 +1,40 @@
+{
+  pkgs,
+  ...
+}:
+{
+  # Disable or enable default gnome apps here
+  #! CAREFUL, LIST IS INVERTED -> commented out = KEEP
+  environment.gnome.excludePackages = with pkgs; [
+    # baobab # disk usage analyzer
+    cheese # photo booth
+    epiphany # web browser
+    gedit # text editor
+    simple-scan # document scanner
+    # totem # video player
+    yelp # help viewer
+    # evince # document viewer
+    # file-roller # archive manager
+    geary # email client
+    # seahorse # password manager
+    # gnome-connections # remote computer connection thingie
+    eog # image viewer (older I think?)
+    # loupe # newer image viewer
+    gnome-tour
+    gnome-user-docs
+    # gnome-calculator
+    # gnome-calendar
+    # gnome-characters
+    # gnome-clocks
+    # gnome-contacts
+    gnome-font-viewer
+    gnome-logs
+    gnome-maps
+    gnome-music
+    # gnome-photos
+    # gnome-screenshot
+    # gnome-system-monitor
+    gnome-weather
+    # gnome-disk-utility
+  ];
+}
