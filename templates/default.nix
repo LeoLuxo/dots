041c65e320788dee99a96037331ad82a291b7822
LeoLuxo <leo@gamboa.eu>
2024-12-16 10:23:51 +0100

    coffee 142 current  2024-12-16 10:19:39  24.11.20240926.1925c60  6.6.52
    M	templates/default.nix
    M	templates/rust/.gitignore
    A	templates/typst/.gitignore
    A	templates/typst/flake.nix
    A	templates/typst/imports.typ
    A	templates/typst/main.typ

--- templates/default.nix
+++ templates/default.nix
@@ -3,4 +3,9 @@
     path = ./rust;
     description = "Rust/Cargo project";
   };
+
+  "typst" = {
+    path = ./typst;
+    description = "Typst writing project";
+  };
 }
---
+++ templates/typst/main.typ
@@ -0,0 +1,10 @@
+#import "/imports.typ": *
+
+
+// #include "src/intro.typ"
+
+
+
+#bibliography("bibliography.bib")
+
+// #include "src/appendix.typ"
