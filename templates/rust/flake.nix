85c5b31dda9aaa8c71072a68b378d293369b8370
LeoLuxo <leo@gamboa.eu>
2024-12-16 10:13:35 +0100

    coffee 141 current  2024-12-16 10:13:32  24.11.20240926.1925c60  6.6.52
    R097	modules/git.nix	modules/git/default.nix
    A	modules/git/gitignore.nix
    A	templates/rust/.editorconfig
    A	templates/rust/.envrc
    A	templates/rust/.gitignore
    A	templates/rust/Cargo.toml
    A	templates/rust/flake.nix
    A	templates/rust/rust-toolchain.toml
    A	templates/rust/rustfmt.toml
    A	templates/rust/src/main.rs

---
+++ templates/rust/flake.nix
@@ -0,0 +1,67 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    # Used to build the project by parsing the cargo dependencies
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # Used to generate/get a specific rust toolchain to use with naersk
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      naersk,
+      fenix,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = (import nixpkgs) {
+          inherit system;
+        };
+
+        rustToolchain =
+          with fenix.packages.${system};
+          fromToolchainFile {
+            dir = ./.;
+            sha256 = "sha256-s1RPtyvDGJaX/BisLT+ifVfuhDT1nZkZ1NcK8sbwELM=";
+          };
+      in
+      rec {
+        # For `nix build` and `nix run`
+        packages.default =
+          (naersk.lib.${system}.override {
+            cargo = rustToolchain;
+            rustc = rustToolchain;
+          }).buildPackage
+            {
+              src = ./.;
+            };
+
+        # For `nix develop` and `direnv allow`
+        devShell = pkgs.mkShell {
+          inputsFrom = [ packages.default ];
+          buildInputs = with pkgs; [
+            rustToolchain
+
+            pre-commit
+            rust-analyzer
+          ];
+
+          # Needed for rust-analyser to work
+          RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+        };
+      }
+    );
+}
