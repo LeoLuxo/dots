6b41482ed2e8e89eebed89c7cbe8f373ac2f71ea
Chloé G <contact@leoluxo.eu>
2024-12-20 16:46:56 +0100

    pancake 371 current  2024-12-20 16:46:51  24.11.20240926.1925c60  6.10.5
    M	config/vesktop/vencord.json
    M	hosts/pancake/wifi.nix

--- hosts/pancake/wifi.nix
+++ hosts/pancake/wifi.nix
@@ -66,6 +66,33 @@
           };
         };
 
+        # Parent's home network
+        "Domaine des fleurs de jardin" = {
+          connection = {
+            id = "Domaine des fleurs de jardin";
+            interface-name = "wlp0s20f3";
+            type = "wifi";
+            # uuid = "7b1d293c-0466-414a-b57d-b8598d021ce4";
+          };
+          ipv4 = {
+            method = "auto";
+          };
+          ipv6 = {
+            addr-gen-mode = "default";
+            method = "auto";
+          };
+          proxy = { };
+          # wifi = {
+          #   mode = "infrastructure";
+          #   ssid = "HUAWEI-AN1XC8_5G";
+          # };
+          wifi-security = {
+            auth-alg = "open";
+            key-mgmt = "wpa-psk";
+            psk = "$DOMAINE_DES_FLEURS_DE_JARDIN_PSK";
+          };
+        };
+
         # Uni network
         eduroam = {
           "802-1x" = {
