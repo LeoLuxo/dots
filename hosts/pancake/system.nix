c696d3ac59e0c8dd9a3b4b901eb0b7d0cc09b034
Chloé G <contact@leoluxo.eu>
2024-12-21 08:59:58 +0100

    pancake 377 current  2024-12-21 08:59:52  24.11.20240926.1925c60  6.10.5
    M	hosts/pancake/system.nix

--- hosts/pancake/system.nix
+++ hosts/pancake/system.nix
@@ -10,6 +10,14 @@
     nixos-hardware.nixosModules.microsoft-surface-pro-intel
   ];
 
+  # services.xserver.upscaleDefaultCursor = true;
+  environment.variables = {
+    GDK_SCALE = "2.0";
+    GDK_DPI_SCALE = "0.5";
+    _JAVA_OPTIONS = "-Dsun.java2d.uiScale=2.0";
+    QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+  };
+
   gnome.power = {
     button-action = "suspend";
     confirm-shutdown = true;
