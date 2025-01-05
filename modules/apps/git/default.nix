6bfd8c12b5c74d2ddabf1b73f844047f52052867
Chloé G <contact@leoluxo.eu>
2025-01-05 18:30:53 +0100

    coffee 168 current  2025-01-05 18:30:50  24.11.20240926.1925c60  6.6.52
    M	modules/apps/git/default.nix

--- modules/apps/git/default.nix
+++ modules/apps/git/default.nix
@@ -67,8 +67,11 @@ in
 
         # Sign all commits using ssh key
         commit.gpgsign = true;
-        gpg.format = "ssh";
         user.signingkey = "~/.ssh/id_ed25519.pub";
+        gpg = {
+          format = "ssh";
+          ssh.allowedSignersFile = "~/.ssh/allowed_signers";
+        };
       };
     };
   };
