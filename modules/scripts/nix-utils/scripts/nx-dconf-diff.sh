cb82e6f5d6ac5311f3343b384178d743f0b02e71
Chloé G <contact@leoluxo.eu>
2025-01-07 21:08:23 +0100

    coffee 180 current  2025-01-07 21:08:22  24.11.20240926.1925c60  6.6.52
    M	modules/scripts/nix-utils/default.nix
    M	modules/scripts/nix-utils/scripts/nx-dconf-diff.sh
    M	modules/scripts/nix-utils/scripts/nx-rebuild.sh

--- modules/scripts/nix-utils/scripts/nx-dconf-diff.sh
+++ modules/scripts/nix-utils/scripts/nx-dconf-diff.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-# nx-rebuild is supposed to populate the $DCONF_DIFF file
+# nx-rebuild is supposed to populate the $NX_DCONF_DIFF file
+# but create it as empty just in case
+mkdir --parents "$(dirname "$NX_DCONF_DIFF")" && touch "$NX_DCONF_DIFF"
 
 # Show the dconf diff since the last rebuild
-difft $DCONF_DIFF <(dconf dump /)
+difft $NX_DCONF_DIFF <(dconf dump /)
