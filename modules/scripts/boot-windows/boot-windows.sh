afc7027f3c0c3f89f2c9755631375a393f09edb1
Chloé G <contact@chlookie.net>
2025-01-08 19:12:57 +0100

    coffee 200 current  2025-01-08 19:12:56  24.11.20240926.1925c60  6.6.52
    M	modules/scripts/boot-windows/boot-windows.sh
    M	modules/scripts/boot-windows/default.nix

--- modules/scripts/boot-windows/boot-windows.sh
+++ modules/scripts/boot-windows/boot-windows.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Using pkexec to elevate the script using the GUI-sudo-thing
+[[ "$EUID" == 0 ]] || exec pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY "$0" "$@"
+
 # Get the ID of the boot manager for windows
 ID=$(sudo efibootmgr | grep -i windows | cut -c 5-8)
 # Set it for the next boot
