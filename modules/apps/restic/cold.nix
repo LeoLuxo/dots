93a6a013ceb0540d8eb3b07891c05a3549cf6a25
Chloé G <contact@leoluxo.eu>
2025-01-26 21:13:16 +0100

    coffee 245 current  2025-01-26 21:13:15  24.11.20240926.1925c60  6.6.52
    M	hosts.nix
    M	hosts/coffee/software.nix
    M	hosts/pancake/software.nix
    D	hosts/restic.nix
    A	modules/apps/restic/cold.nix
    A	modules/apps/restic/default.nix

---
+++ modules/apps/restic/cold.nix
@@ -0,0 +1,41 @@
+{
+  config,
+  constants,
+  lib,
+  extraLib,
+  ...
+}:
+
+let
+  inherit (extraLib) mkBoolDefaultFalse;
+  inherit (constants) user;
+  inherit (lib) options types modules;
+in
+
+{
+  options.restic.coldBackup = {
+    enable = mkBoolDefaultFalse;
+
+    repo = options.mkOption {
+      type = types.path;
+    };
+
+    passwordFile = options.mkOption {
+      type = types.path;
+    };
+  };
+
+  config =
+    let
+      cfg = config.restic.coldBackup;
+    in
+    modules.mkIf cfg.enable {
+      home-manager.users.${user} = {
+        home.shellAliases = {
+          # Add aliases for the cold repo
+          restic3 = "RESTIC_PASSWORD=$(sudo cat ${cfg.passwordFile}) restic --repo ${cfg.repo}";
+          rustic3 = "RUSTIC_PASSWORD=$(sudo cat ${cfg.passwordFile}) rustic --repo ${cfg.repo}";
+        };
+      };
+    };
+}
