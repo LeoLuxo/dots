6d49b475813bbab11c48e1c0b6b32e47622e7103
Chloé G <contact@leoluxo.eu>
2025-02-05 15:25:42 +0100

    coffee 261 current  2025-02-05 14:31:48  24.11.20240926.1925c60  6.6.52
    M	config/vesktop/vencord.json
    M	config/youtube-music/config.json
    M	hosts/coffee/software.nix
    A	modules/apps/llm.nix

---
+++ modules/apps/llm.nix
@@ -0,0 +1,39 @@
+{ ... }:
+
+{
+  services.ollama = {
+    enable = true;
+    loadModels = [
+      "deepseek-r1:8b"
+      "llava:7b"
+      "deepseek-coder-v2:16b"
+
+      # "llama3.2:3b"
+      # "llama3.2-vision:11b"
+      # "phi4:14b"
+      # "dolphin3:8b"
+      # "smallthinker:3b"
+      # "nomic-embed-text"
+    ];
+
+    acceleration = "cuda";
+  };
+
+  services.open-webui = {
+    enable = true;
+    port = 8888;
+    host = "127.0.0.1";
+  };
+
+  # environment.systemPackages = with pkgs; [
+  # oterm
+  # alpaca
+  # aichat
+  # aider-chat
+
+  # tgpt
+  # smartcat
+  # nextjs-ollama-llm-ui
+  # open-webui
+  # ];
+}
