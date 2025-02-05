6bfc0656c45705326922f1762b52c1457b8e48e8
Chloé G <contact@chlookie.net>
2025-02-05 20:26:22 +0100

    coffee 268 current  2025-02-05 20:26:21  24.11.20240926.1925c60  6.6.52
    M	modules/apps/llm.nix

--- modules/apps/llm.nix
+++ modules/apps/llm.nix
@@ -1,13 +1,26 @@
 { pkgs, ... }:
 
 {
+  # home.packages = [
+  #   (pkgs.alpaca.override {
+  #     ollama = pkgs.ollama-cuda;
+  #   })
+
+  # oterm
+  # aichat
+  # aider-chat
+  # tgpt
+  # smartcat
+  # nextjs-ollama-llm-ui
+  # open-webui
+  # ];
+
   services.ollama = {
     enable = true;
     loadModels = [
       "deepseek-r1:8b"
       "llava:7b"
       "deepseek-coder-v2:16b"
-
       # "llama3.2:3b"
       # "llama3.2-vision:11b"
       # "phi4:14b"
@@ -15,39 +28,19 @@
       # "smallthinker:3b"
       # "nomic-embed-text"
     ];
-
     acceleration = "cuda";
   };
 
-  # services.open-webui = {
-  #   enable = true;
-  #   port = 11111;
-  #   host = "127.0.0.1";
-
-  #   environment = {
-  #     WEBUI_AUTH = "False";
-  #     OLLAMA_API_BASE_URL = "http://127.0.0.1:11434";
-
-  #     ANONYMIZED_TELEMETRY = "False";
-  #     DO_NOT_TRACK = "True";
-  #     SCARF_NO_ANALYTICS = "True";
-  #   };
-  # };
-
-  environment.systemPackages = with pkgs; [
-
-    (pkgs.alpaca.override {
-      ollama = pkgs.ollama-cuda;
-    })
-
-    # oterm
-    # alpaca
-    # aichat
-    # aider-chat
-
-    # tgpt
-    # smartcat
-    # nextjs-ollama-llm-ui
-    # open-webui
-  ];
+  services.open-webui = {
+    enable = true;
+    # port = 11111;
+    host = "0.0.0.0";
+    environment = {
+      WEBUI_AUTH = "False";
+      # OLLAMA_API_BASE_URL = "http://127.0.0.1:11434";
+      ANONYMIZED_TELEMETRY = "False";
+      DO_NOT_TRACK = "True";
+      SCARF_NO_ANALYTICS = "True";
+    };
+  };
 }
