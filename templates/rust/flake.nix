344c11adaf1cafdc7cff170eea723412c20690d2
Chloé G <contact@leoluxo.eu>
2025-02-06 20:09:09 +0100

    coffee 275 current  2025-02-06 20:09:08  25.05.20250204.799ba5b  6.12.12
    M	config/vscode/settings.json
    D	templates/python/flake.nix
    D	templates/python/pyproject.toml
    M	templates/rust/Cargo.toml
    M	templates/rust/flake.nix
    M	templates/typst/flake.nix

--- templates/rust/flake.nix
+++ templates/rust/flake.nix
@@ -1,7 +1,12 @@
 {
   inputs = {
+    # Import the latest unstable version of nixpkgs
     nixpkgs.url = "nixpkgs/nixos-unstable";
+    # Import flake-utils for utility functions
     flake-utils.url = "github:numtide/flake-utils";
+
+    # Used to format all kinds of files
+    treefmt-nix.url = "github:numtide/treefmt-nix";
 
     # Used to build the project by parsing the cargo dependencies
     naersk = {
@@ -14,6 +19,7 @@
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
   };
 
   outputs =
@@ -21,40 +27,37 @@
       self,
       nixpkgs,
       flake-utils,
+      treefmt-nix,
       naersk,
       fenix,
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = (import nixpkgs) {
-          inherit system;
-        };
+        # Import nixpkgs for the current system
+        pkgs = nixpkgs.legacyPackages.${system};
 
+        # Define the Rust toolchain using fenix, by reading rust-toolchain.toml
         rustToolchain =
           with fenix.packages.${system};
           fromToolchainFile {
             dir = ./.;
             sha256 = "sha256-s1RPtyvDGJaX/BisLT+ifVfuhDT1nZkZ1NcK8sbwELM=";
           };
-      in
-      rec {
-        # For `nix build` and `nix run`
-        packages.default =
+
+        # Define the package using naersk with the specified Rust toolchain
+        package =
           (naersk.lib.${system}.override {
             cargo = rustToolchain;
             rustc = rustToolchain;
           }).buildPackage
-            {
-              src = ./.;
-            };
+            { src = ./.; };
 
-        # For `nix develop` and `direnv allow`
+        # Define the development shell with necessary tools
         devShell = pkgs.mkShell {
-          inputsFrom = [ packages.default ];
+          inputsFrom = [ package ];
           buildInputs = with pkgs; [
             rustToolchain
-
             pre-commit
             rust-analyzer
           ];
@@ -62,6 +65,31 @@
           # Needed for rust-analyser to work
           RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
         };
+
+        # Setup treefmt with the formatters we need
+        treefmtEval = treefmt-nix.lib.evalModule pkgs (
+          { ... }:
+          {
+            projectRootFile = "flake.nix";
+            programs = {
+              nixfmt.enable = true; # For nix files
+              rustfmt.enable = true; # For rust files
+            };
+          }
+        );
+      in
+      {
+        # For `nix build` and `nix run`
+        packages.default = package;
+
+        # For `nix develop`
+        devShells.default = devShell;
+
+        # For `nix fmt`
+        formatter = treefmtEval.config.build.wrapper;
+
+        # For `nix flake check`
+        checks.formatting = treefmtEval.config.build.check self;
       }
     );
 }
