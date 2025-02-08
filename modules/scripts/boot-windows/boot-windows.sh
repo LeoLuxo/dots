083142e800ec9c2d6db67eabe2f1df1972ea4f80
Chloé G <contact@leoluxo.eu>
2025-02-08 20:28:50 +0100

    Remove everything

--- modules/scripts/boot-windows/boot-windows.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Get the ID of the boot manager for windows
-ID=$(sudo efibootmgr | grep -i windows | cut -c 5-8)
-# Set it for the next boot
-sudo efibootmgr -n $ID
-
-reboot
--- templates/typst/imports.typ
+++
@@ -1,8 +0,0 @@
-
-#import "@preview/dashy-todo:0.0.1": todo
-
-#let appendix(body) = {
-  set heading(numbering: "A", supplement: [Appendix])
-  counter(heading).update(0)
-  body
-}
