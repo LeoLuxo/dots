a4fbb92412677e1423cc0f7cbc3fce3dc25764ac
Chloé G <contact@chlookie.net>
2025-02-20 21:56:56 +0100

    Add NixOS modules for key and secrets management

---
+++ nixosModules/keys.nix
@@ -0,0 +1,44 @@
+{
+  lib,
+  config,
+  inputs,
+  ...
+}:
+
+let
+  lib2 = inputs.lib;
+  inherit (lib) types;
+
+  cfg = config.ext.keys;
+in
+{
+  imports = [
+
+  ];
+
+  options.ext.keys = with lib2.options; {
+    enable = mkEnableOpt "key management";
+
+    keys =
+      mkAttrsSub "keys accessible to the config"
+        {
+          public = mkOpt' "the public key path" types.path;
+          private = mkOpt' "the private key path" types.path;
+        }
+        {
+          user = lib.mkIf (config.ext.user != null) {
+            private = "${config.ext.user.home}/.ssh/id_ed25519";
+            public = "${config.ext.user.home}/.ssh/id_ed25519.pub";
+          };
+
+          host = {
+            private = "/etc/ssh/ssh_host_ed25519_key";
+            public = "/etc/ssh/ssh_host_ed25519_key.pub";
+          };
+        };
+  };
+
+  config = lib.mkIf cfg.enable {
+
+  };
+}
