adf1be3c62601f374b2effb0494c2a38b4268492
LeoLuxo <contact@me.leoluxo.eu>
2025-02-25 20:47:55 +0100

    coffee 285 current  2025-02-25 20:47:54  25.05.20250204.799ba5b  6.12.12
    M	hosts/coffee/system.nix

--- hosts/coffee/system.nix
+++ hosts/coffee/system.nix
@@ -102,7 +102,7 @@
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
   # Enable and configure the X11 windowing system.
