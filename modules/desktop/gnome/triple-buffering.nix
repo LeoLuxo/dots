9fcbdd157b4b9dfa7f26fbdfbc307bd77e6f8402
Chloé G <contact@chlookie.net>
2025-02-25 22:16:23 +0100

    coffee 291 current  2025-02-25 22:16:22  25.05.20250204.799ba5b  6.12.12
    M	modules/desktop/gnome/gnome.nix
    M	modules/desktop/gnome/triple-buffering.nix

--- modules/desktop/gnome/triple-buffering.nix
+++ modules/desktop/gnome/triple-buffering.nix
@@ -1,21 +1,35 @@
 { pkgs, ... }:
+
+let
+  mutterTripleBuffering = pkgs.fetchFromGitLab {
+    domain = "gitlab.gnome.org";
+    owner = "vanvugt";
+    repo = "mutter";
+    rev = "triple-buffering-v4-47";
+    hash = "sha256-ajxm+EDgLYeqPBPCrgmwP+FxXab1D7y8WKDQdR95wLI=";
+  };
+
+  gvdb = pkgs.fetchFromGitLab {
+    domain = "gitlab.gnome.org";
+    owner = "GNOME";
+    repo = "gvdb";
+    rev = "main";
+    hash = "sha256-CIdEwRbtxWCwgTb5HYHrixXi+G+qeE1APRaUeka3NWk=";
+  };
+in
+
 {
   nixpkgs.overlays = [
-    # GNOME 46: triple-buffering-v4-46
+    # GNOME 47: triple-buffering-v4-47
     (final: prev: {
-      gnome = prev.gnome.overrideScope (
-        gnomeFinal: gnomePrev: {
-          mutter = gnomePrev.mutter.overrideAttrs (old: {
-            src = pkgs.fetchFromGitLab {
-              domain = "gitlab.gnome.org";
-              owner = "vanvugt";
-              repo = "mutter";
-              rev = "triple-buffering-v4-46";
-              hash = "sha256-fkPjB/5DPBX06t7yj0Rb3UEuu5b9mu3aS+jhH18+lpI=";
-            };
-          });
-        }
-      );
+      mutter = prev.mutter.overrideAttrs (old: {
+        src = mutterTripleBuffering;
+
+        preConfigure = ''
+          cp -a "${gvdb}" ./subprojects/gvdb
+        '';
+      });
     })
   ];
+
 }
