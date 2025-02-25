67a6fd64b397db8f272569712d6f4ab14a54ce2b
Chloé G <contact@leoluxo.eu>
2025-02-25 20:57:27 +0100

    coffee 287 current  2025-02-25 20:57:26  25.05.20250204.799ba5b  6.12.12
    M	modules/desktop/gnome/gnome.nix

--- modules/desktop/gnome/gnome.nix
+++ modules/desktop/gnome/gnome.nix
@@ -95,6 +95,6 @@ in
       defaults.apps.backupTerminal = lib.mkDefault "kgx";
       defaults.apps.terminal = lib.mkOverride 1050 "kgx";
 
-      #      environment.sessionVariables.NIXOS_OZONE_WL = "1";
+      environment.sessionVariables.NIXOS_OZONE_WL = "1";
     };
 }
