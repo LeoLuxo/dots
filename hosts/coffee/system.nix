34bc5c35343777a4a5ce4f176e591b9ba1f22af8
Chloé G <contact@leoluxo.eu>
2025-03-13 19:40:52 +0100

    coffee 307 current  2025-03-13 19:40:51  25.05.20250204.799ba5b  6.12.12
    M	config/vscode/settings.json
    M	hosts/coffee/system.nix

--- hosts/coffee/system.nix
+++ hosts/coffee/system.nix
@@ -101,7 +101,7 @@
     # accessible via `nvidia-settings`.
     nvidiaSettings = true;
 
-    forceFullCompositionPipeline = false;
+    forceFullCompositionPipeline = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
     package = config.boot.kernelPackages.nvidiaPackages.latest;
