3bebf5f7650354e810886755a6119c72d0e7a390
Chloé G <contact@chlookie.net>
2025-03-20 16:18:38 +0100

    Add gnome touchpad gestures

---
+++ modules/desktop/gnome/extensions/touchpad-gesture-customization.nix
@@ -0,0 +1,35 @@
+{
+  pkgs,
+  constants,
+  ...
+}:
+
+let
+  inherit (constants) user;
+in
+
+{
+  programs.dconf.enable = true;
+
+  home-manager.users.${user} =
+    { lib, ... }:
+    {
+      home.packages = with pkgs; [
+        gnomeExtensions.touchpad-gesture-customization
+      ];
+
+      dconf.settings = {
+        "org/gnome/shell" = {
+          enabled-extensions = [ "touchpad-gesture-customization@coooolapps.com'" ];
+        };
+
+        "org/gnome/shell/extensions/touchpad-gesture-customization" = {
+          allow-minimize-window = true;
+          enable-forward-back-gesture = false;
+          enable-window-manipulation-gesture = false;
+          pinch-3-finger-gesture = "NONE";
+          pinch-4-finger-gesture = "NONE";
+        };
+      };
+    };
+}
