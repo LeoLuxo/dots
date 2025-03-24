a40f1b5e2a7218530c98324ae0d522198e511919
Chloé G <contact@chlookie.net>
2025-03-24 08:25:12 +0100

    Add module to pin Linux kernel

---
+++ nixosModules/system/pinKernel.nix
@@ -0,0 +1,34 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+
+let
+  cfg = config.ext.system.pinKernel;
+in
+{
+  imports = [
+
+  ];
+
+  options.ext.system.pinKernel = {
+    enable = lib.mkEnableOption "pinning the linux kernel to a specific version";
+  };
+
+  config = lib.mkIf cfg.enable {
+    boot.kernelPackages = pkgs.linuxPackagesFor (
+      pkgs.linux_6_12.override {
+        argsOverride = rec {
+          src = pkgs.fetchurl {
+            url = "mirror://kernel/linux/kernel/v6.x/linux-${version}.tar.xz";
+            sha256 = "";
+          };
+          version = "6.12.19";
+          modDirVersion = "6.12.19";
+        };
+      }
+    );
+  };
+}
