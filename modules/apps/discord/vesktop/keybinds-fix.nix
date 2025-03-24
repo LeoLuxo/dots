60c03de5ac7c9bd228e05029261ea033feff846e
LeoLuxo <contact@me.leoluxo.eu>
2025-03-24 08:02:08 +0100

    coffee 309 current  2025-03-24 07:59:10  25.05.20250322.1e5b653  6.12.19
    M	modules/apps/discord/vesktop/keybinds-fix.nix

--- modules/apps/discord/vesktop/keybinds-fix.nix
+++ modules/apps/discord/vesktop/keybinds-fix.nix
@@ -2,6 +2,8 @@
 {
   # Overlay to patch global keybinds
   # TODO: Remove when the PR is merged
+  # https://github.com/Vencord/Vesktop/pull/609/
+  # https://github.com/Vencord/Vesktop/pull/326
   nixpkgs.overlays = [
     (final: prev: {
       vesktop = prev.vesktop.overrideAttrs (
