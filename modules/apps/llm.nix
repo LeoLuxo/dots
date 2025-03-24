37ad47593ac9eb20a34e424489b6899f3ce3b450
LeoLuxo <contact@me.leoluxo.eu>
2025-03-24 07:59:11 +0100

    coffee 309 current  2025-03-24 07:59:10  25.05.20250322.1e5b653  6.12.19
    M	config/youtube-music/config.json
    M	flake.lock
    M	modules/apps/discord/vesktop/keybinds-fix.nix
    M	modules/apps/llm.nix

--- modules/apps/llm.nix
+++ modules/apps/llm.nix
@@ -15,32 +15,32 @@
   # open-webui
   # ];
 
-  services.ollama = {
-    enable = true;
-    loadModels = [
-      "deepseek-r1:8b"
-      "llava:7b"
-      "deepseek-coder-v2:16b"
-      # "llama3.2:3b"
-      # "llama3.2-vision:11b"
-      # "phi4:14b"
-      # "dolphin3:8b"
-      # "smallthinker:3b"
-      # "nomic-embed-text"
-    ];
-    acceleration = "cuda";
-  };
+  # services.ollama = {
+  #   enable = true;
+  #   loadModels = [
+  #     "deepseek-r1:8b"
+  #     "llava:7b"
+  #     "deepseek-coder-v2:16b"
+  #     # "llama3.2:3b"
+  #     # "llama3.2-vision:11b"
+  #     # "phi4:14b"
+  #     # "dolphin3:8b"
+  #     # "smallthinker:3b"
+  #     # "nomic-embed-text"
+  #   ];
+  #   acceleration = "cuda";
+  # };
 
-  services.open-webui = {
-    enable = true;
-    # port = 11111;
-    host = "0.0.0.0";
-    environment = {
-      WEBUI_AUTH = "False";
-      # OLLAMA_API_BASE_URL = "http://127.0.0.1:11434";
-      ANONYMIZED_TELEMETRY = "False";
-      DO_NOT_TRACK = "True";
-      SCARF_NO_ANALYTICS = "True";
-    };
-  };
+  # services.open-webui = {
+  #   enable = true;
+  #   # port = 11111;
+  #   host = "0.0.0.0";
+  #   environment = {
+  #     WEBUI_AUTH = "False";
+  #     # OLLAMA_API_BASE_URL = "http://127.0.0.1:11434";
+  #     ANONYMIZED_TELEMETRY = "False";
+  #     DO_NOT_TRACK = "True";
+  #     SCARF_NO_ANALYTICS = "True";
+  #   };
+  # };
 }
