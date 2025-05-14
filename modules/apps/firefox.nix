dfdaf34394d49dcaae1d0a470b21deb211c42132
Chloé G <contact@leoluxo.eu>
2025-05-14 11:08:56 +0200

    coffee 317 current  2025-05-14 11:08:56  25.05.20250322.1e5b653  6.12.19
    M	modules/apps/firefox.nix

--- modules/apps/firefox.nix
+++ modules/apps/firefox.nix
@@ -29,8 +29,10 @@
     };
   };
 
-  environment.sessionVariables = {
-    MOZ_ENABLE_WAYLAND = "0";
-  };
+  # Fixes fullscreen freezes
+  # environment.sessionVariables = {
+  #   MOZ_ENABLE_WAYLAND = "0";
+  # };
+  # Nevermind, xwayland cannot deal with multiple desktops at all
 
 }
