8156f04ec42ae7214e937fc7f1df602de95067c9
LeoLuxo <contact@me.leoluxo.eu>
2025-05-17 21:37:05 +0200

    Fix recursion in aliases

--- nixosModules/shell/aliases.nix
+++ nixosModules/shell/aliases.nix
@@ -9,68 +9,34 @@ let
   lib2 = inputs.self.lib;
   inherit (lib) types;
 
-  cfg = config.ext.shell.aliases;
+  cfg = config.ext.shell;
 in
 {
-  options.ext.shell = {
-    aliases = with lib2.options; mkOpt "Shell command aliases" types.attrsOf types.str { };
+  options.ext.shell = with lib2.options; {
+    aliases = mkOpt "Shell command aliases" (types.attrsOf types.str) { };
 
-    aliasesWithHistory = lib.options.mkOption {
+    aliasesWithHistory = lib.mkOption {
       default = { };
       # No typechecking because I don't know how to typecheck for functions, but should be:
       # type = types.attrsOf function;
     };
   };
 
-  config = # normal aliases
-    {
-      ext.hm.home.shellAliases = cfg.aliases;
-    }
-    //
-    # aliases with history
-    (lib.mkMerge (
-      lib.mapAttrsToList (
-        name: command:
-        let
-          historyCommands = {
-            fish = ''$history[1]'';
-            bash = ''$(fc -ln -1)'';
-            zsh = ''''${history[@][1]}'';
-          };
-
-          mappedCommands = builtins.mapAttrs (
-            _: lastCommand: command { inherit lastCommand; }
-          ) historyCommands;
-        in
+  config.ext.hm = (
+    lib.mkMerge (
+      [
+        # simple aliases
         {
-          ext.hm = {
-            programs.bash.shellAliases.${name} = mappedCommands.bash;
-            programs.fish.shellAliases.${name} = ''eval ${mappedCommands.fish}'';
-            programs.zsh.shellAliases.${name} = ''eval ${mappedCommands.zsh}'';
-          };
+          home.shellAliases = cfg.aliases;
         }
-      ) cfg.aliasesWithHistory
-    ));
-
-  # // (lib.mapAttrs (
-  #   name: command:
-  #   let
-  #     historyCommands = {
-  #       fish = ''$history[1]'';
-  #       bash = ''$(fc -ln -1)'';
-  #       zsh = ''''${history[@][1]}'';
-  #     };
-
-  #     mappedCommands = builtins.mapAttrs (
-  #       _: lastCommand: command { inherit lastCommand; }
-  #     ) historyCommands;
-  #   in
-  #   {
-  #     # ext.hm = {
-  #     #   programs.bash.shellAliases.${name} = mappedCommands.bash;
-  #     #   programs.fish.shellAliases.${name} = ''eval ${mappedCommands.fish}'';
-  #     #   programs.zsh.shellAliases.${name} = ''eval ${mappedCommands.zsh}'';
-  #     # };
-  #   }
-  # ) (lib.traceVal cfg.aliasesWithHistory));
+      ]
+      ++
+        # aliases with history
+        lib.mapAttrsToList (name: commandFunc: {
+          programs.bash.shellAliases.${name} = commandFunc ''$(fc -ln -1)'';
+          programs.fish.shellAliases.${name} = ''eval ${commandFunc ''$history[1]''}'';
+          programs.zsh.shellAliases.${name} = ''eval ${commandFunc ''''${history[@][1]}''}'';
+        }) cfg.aliasesWithHistory
+    )
+  );
 }
