c5daa0dd61685de07069f172032c90c27f02a7df
Chloé G <contact@chlookie.net>
2025-05-23 09:35:10 +0200

    coffee 348 current  2025-05-23 09:35:09  25.05.20250322.1e5b653  6.12.19
    A	modules/apps/beets/beets-fetchartist.nix
    A	modules/apps/beets/beets.nix
    R098	modules/apps/beets/beets-config.yaml	modules/apps/beets/config.yaml
    M	modules/apps/beets/default.nix
    R051	modules/scripts/automusic/default.nix	modules/scripts/automusic.nix
    D	modules/scripts/automusic/lrclib-fetch.sh
    D	modules/scripts/automusic/lrcput.py

--- modules/apps/beets/default.nix
+++ modules/apps/beets/default.nix
@@ -1,28 +1,17 @@
 {
   pkgs,
   constants,
-  extraLib,
   ...
 }:
 
 let
   inherit (constants) user;
-  inherit (extraLib) writeScriptWithDeps;
 in
 
 {
   home-manager.users.${user} = {
     home.packages = [
-      (writeScriptWithDeps {
-        name = "beet";
-
-        deps = [ pkgs.beets ];
-
-        text = ''
-          #!/usr/bin/env bash
-          beet --config ${./beets-config.yaml} "$@"
-        '';
-      })
+      (pkgs.callPackage ./beets.nix { })
     ];
   };
 }
