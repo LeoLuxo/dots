9c8f0b953e0d699b4f7e72d4033d45c16d9183fa
Chloé G <contact@chlookie.net>
2025-05-23 10:29:45 +0200

    coffee 359 current  2025-05-23 10:29:43  25.05.20250322.1e5b653  6.12.19
    D	modules/apps/beets/beets.nix
    M	modules/apps/beets/config.yaml
    M	modules/apps/beets/default.nix

--- modules/apps/beets/default.nix
+++ modules/apps/beets/default.nix
@@ -1,17 +1,38 @@
 {
   pkgs,
   constants,
+  extraLib,
   ...
 }:
 
 let
   inherit (constants) user;
+  inherit (extraLib) writeScriptWithDeps;
 in
 
+let
+  package = pkgs.beets.override {
+    pluginOverrides = {
+      # fetchartist = {
+      #   enable = true;
+      #   propagatedBuildInputs = [ (pkgs.callPackage ./beets-fetchartist.nix { }) ];
+      # };
+    };
+  };
+in
 {
   home-manager.users.${user} = {
     home.packages = [
-      (pkgs.callPackage ./beets.nix { })
+      (writeScriptWithDeps {
+        name = "beet";
+
+        deps = [ package ];
+
+        text = ''
+          #!/usr/bin/env bash
+          beet --config "${./config.yaml}" "$@"
+        '';
+      })
     ];
   };
 }
