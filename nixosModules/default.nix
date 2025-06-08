6ffd06f5cfd948fd7d9ad4a3835c0da35b3f40a9
Chloé G <contact@leoluxo.eu>
2025-06-08 09:24:51 +0200

    Small stuff

--- nixosModules/default.nix
+++ nixosModules/default.nix
@@ -13,12 +13,18 @@ let
         isDirWithDefault = type == "directory" && lib.pathExists (fullPath + "/default.nix");
       in
       if isDefault then
+        # The path itself is a default.nix file: don't import anything (it should be imported as a directory instead)
         { }
       else if isFile || isDirWithDefault then
+        # Either the path is a file or it is a directory which contains a default.nix file: directly import it as a module
         { ${attrName} = import fullPath; }
       else
+        # The path is a simple directory: recursively import its contents
+        let
+          dirModules = recursivelyImportDir fullPath;
+        in
         # Prepend the directory name to all the modules that were recursively imported from that dir
-        lib.mapAttrs' (n: v: lib.nameValuePair ("${attrName}-${n}") v) (recursivelyImportDir fullPath)
+        lib.mapAttrs' (n: v: lib.nameValuePair ("${attrName}-${n}") v) dirModules
     ) (builtins.readDir path);
 
   modules = recursivelyImportDir ./.;
