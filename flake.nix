76a016ce7d74d821995f13edc785f82da4784e39
LeoLuxo <contact@me.leoluxo.eu>
2025-06-10 20:40:58 +0200

    Start chaning to flakelight

--- flake.nix
+++ flake.nix
@@ -1,31 +1,49 @@
 {
   description = "My NixOS configuration :)";
 
-  inputs = {
-    # Using nixpkgs unstable
-    nixpkgs = {
-      url = "github:nixos/nixpkgs/nixos-unstable";
-    };
+  outputs =
+    { flakelight, ... }@inputs:
+    flakelight ./. (
+      { lib, ... }:
+      {
+        inherit inputs;
 
+        nixDir = ./.;
+
+        systems = [ "x86_64-linux" ];
+
+        # Use a compatibility layer until I've transferred all the old modules to the new module system
+        nixosConfigurations = import ./flakelightCompat.nix { inherit lib inputs; };
+      }
+    );
+
+  inputs = rec {
+    # ----- nixpkgs -------------------------------------------------------------------------------
+    nixpkgs-24-11.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs-24-05.url = "github:nixos/nixpkgs/nixos-24.05";
+
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-stable = nixpkgs-24-11;
+    nixpkgs = nixpkgs-unstable;
+
+    # ----- personal stuff ------------------------------------------------------------------------
+    # My wallpapers
+    # Is an external flake to make sure this repo stays small if the wallpapers aren't used
+    # (The url MUST use git+ssh otherwise it won't properly authenticate and have access to the private repo)
+    wallpapers.url = "git+ssh://git@github.com/LeoLuxo/dots-wallpapers";
+
+    # ----- metaconfig & nix ---------------------------------------------------------------------
     # Manages dotfiles in nix
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # My wallpapers
-    # Is an external flake to make sure this repo stays small if the wallpapers aren't used
-    # (The url MUST use git+ssh otherwise it won't properly authenticate and have access to the private repo)
-    wallpapers = {
-      url = "git+ssh://git@github.com/LeoLuxo/dots-wallpapers";
-    };
+    # A modular Nix flake framework for simplifying flake definitions.
+    # https://github.com/nix-community/flakelight/blob/master/API_GUIDE.md
+    flakelight.url = "github:nix-community/flakelight";
 
-    # Contains certain nixos hardware settings, notably useful for surface laptops
-    nixos-hardware = {
-      url = "github:nixos/nixos-hardware/master";
-    };
-
-    # Encryption thingie, used for secrets in nix
+    # Encryption library, used for secrets in nix
     agenix = {
       url = "github:ryantm/agenix";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -33,16 +51,16 @@
       inputs.darwin.follows = "";
     };
 
-    # Tool for customizing/theming nixos
-    stylix = {
-      url = "github:danth/stylix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # ----- hardware ------------------------------------------------------------------------------
+    # Contains certain nixos hardware settings, notably useful for surface laptops
+    nixos-hardware.url = "github:nixos/nixos-hardware/master";
 
+    # Real-time audio in NixOS
+    musnix.url = "github:musnix/musnix";
+
+    # ----- other ---------------------------------------------------------------------------------
     # Catppuccin themes
-    catppuccin = {
-      url = "github:catppuccin/nix";
-    };
+    catppuccin.url = "github:catppuccin/nix";
 
     # Pre-built database for nix-index, which is an index of which files are provided by which packages
     nix-index-database = {
@@ -50,78 +68,8 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # Real-time audio in NixOS
-    musnix = {
-      url = "github:musnix/musnix";
-    };
-
     # VSCode extensions repository
-    nix-vscode-extensions = {
-      url = "github:nix-community/nix-vscode-extensions";
-    };
+    nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
   };
 
-  outputs =
-    { nixpkgs, ... }@inputs:
-
-    let
-      # Function to create a nixos host config
-      mkHost =
-        hostModules:
-        {
-          user,
-          hostName,
-          system,
-          ...
-        }@hostConstants:
-        let
-          defaultConstants = rec {
-            userHome = "/home/${user}";
-            nixosPath = "/etc/nixos";
-            secretsPath = "${userHome}/misc/secrets";
-
-            userKeyPrivate = "${userHome}/.ssh/id_ed25519";
-            userKeyPublic = "${userKeyPrivate}.pub";
-            hostKeyPrivate = "/etc/ssh/ssh_host_ed25519_key";
-            hostKeyPublic = "${hostKeyPrivate}.pub";
-
-            dotsRepoPath = (nixosPath + "/dots");
-          };
-
-          constants = defaultConstants // hostConstants;
-
-          extraLib = import ./libs.nix {
-            inherit inputs constants;
-          };
-
-          nixosModules = extraLib.findFiles {
-            dir = ./modules;
-            extensions = [ "nix" ];
-            defaultFiles = [ "default.nix" ];
-          };
-
-        in
-        nixpkgs.lib.nixosSystem {
-          inherit (hostConstants) system;
-          modules = hostModules;
-
-          # Additional args passed to the module
-          specialArgs = {
-            inherit
-              inputs
-              extraLib
-              nixosModules
-              constants
-              ;
-          };
-        };
-
-    in
-    {
-      # Define nixos configs
-      nixosConfigurations = import ./hosts.nix mkHost;
-
-      # Define templates
-      templates = import ./templates;
-    };
 }
