cd95f458d172e0abff026c4938f50a4986010194
Chloé G <contact@leoluxo.eu>
2025-06-13 15:04:42 +0200

    coffee 382 current  2025-06-13 14:44:01  25.11.20250607.3e3afe5  6.12.32
    M	old/modules/apps/discord/vesktop/default.nix
    M	old/modules/apps/discord/vesktop/overlays/globalKeybinds.nix

--- old/modules/apps/discord/vesktop/default.nix
+++ old/modules/apps/discord/vesktop/default.nix
@@ -20,18 +20,8 @@ in
   imports = [
     ./overlays/customIconsAndName.nix
     ./overlays/pinPackage.nix
-    # ./overlays/globalKeybinds.nix
+    ./overlays/globalKeybinds.nix
     # ./keybindsFixOld.nix
-
-    (mkQuickPatch {
-      package = "vesktop";
-      patches = [
-        (pkgs.fetchpatch {
-          url = "https://patch-diff.githubusercontent.com/raw/Vencord/Vesktop/pull/326.patch";
-          hash = "";
-        })
-      ];
-    })
 
     # (mkQuickPatch {
     #   package = "vencord";
--- old/modules/apps/discord/vesktop/overlays/globalKeybinds.nix
+++ old/modules/apps/discord/vesktop/overlays/globalKeybinds.nix
@@ -7,12 +7,20 @@
     (final: prev: {
       vesktop = prev.vesktop.overrideAttrs (
         finalAttrs: oldAttrs: {
-          patches = [
-            (prev.fetchpatch {
-              url = "https://patch-diff.githubusercontent.com/raw/Vencord/Vesktop/pull/326.patch";
-              hash = "sha256-UaAYbBmMN3/kYVUwNV0/tH7aNZk32JnaUwjsAaZqXwk=";
-            })
-          ];
+          # patches = [
+          #   (prev.fetchpatch {
+          #     url = "https://patch-diff.githubusercontent.com/raw/Vencord/Vesktop/pull/326.patch";
+          #     hash = "sha256-UaAYbBmMN3/kYVUwNV0/tH7aNZk32JnaUwjsAaZqXwk=";
+          #   })
+          # ];
+
+          # Nix is goddamn iritating and for some reason patching is not working at all :shrug:, so take the whole goddamn source of the fork
+          src = prev.fetchFromGitHub {
+            owner = "tuxinal";
+            repo = "Vesktop";
+            rev = "13199c22b869806afef924ed7bb0266c13062bb0";
+            hash = "";
+          };
         }
       );
     })
