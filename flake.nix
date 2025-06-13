ebf1e9f212fdca935d40ef25e802aeb80ab7cf79
LeoLuxo <contact@me.leoluxo.eu>
2025-06-13 14:44:03 +0200

    coffee 382 current  2025-06-13 14:44:01  25.11.20250607.3e3afe5  6.12.32
    M	flake.lock
    M	flake.nix
    M	old/modules/apps/discord/vesktop/default.nix
    R100	old/modules/apps/discord/vesktop/assets/bongo-cat.gif	old/modules/apps/discord/vesktop/overlays/assets/bongo-cat.gif
    R100	old/modules/apps/discord/vesktop/assets/discord.ico	old/modules/apps/discord/vesktop/overlays/assets/discord.ico
    R100	old/modules/apps/discord/vesktop/assets/discord.png	old/modules/apps/discord/vesktop/overlays/assets/discord.png
    R100	old/modules/apps/discord/vesktop/icons-and-name.nix	old/modules/apps/discord/vesktop/overlays/customIconsAndName.nix
    R100	old/modules/apps/discord/vesktop/keybinds-fix.nix	old/modules/apps/discord/vesktop/overlays/keybindsFixOld.nix
    A	old/modules/apps/discord/vesktop/overlays/pinPackage.nix
    R100	old/modules/apps/discord/vesktop/use-custom-vencord.patch	old/modules/apps/discord/vesktop/patches/use-custom-vencord.patch
    R100	old/modules/apps/discord/vesktop/vencord-disable-update-check.patch	old/modules/apps/discord/vesktop/patches/vencord-disable-update-check.patch
    R100	old/modules/apps/discord/vesktop/vesktop-disable-update-check.patch	old/modules/apps/discord/vesktop/patches/vesktop-disable-update-check.patch

--- flake.nix
+++ flake.nix
@@ -19,11 +19,13 @@
 
   inputs = rec {
     # ----- nixpkgs -------------------------------------------------------------------------------
+    nixpkgs-25-05.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgs-24-11.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-24-05.url = "github:nixos/nixpkgs/nixos-24.05";
 
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable = nixpkgs-24-11;
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+
     nixpkgs = nixpkgs-unstable;
 
     # ----- personal stuff ------------------------------------------------------------------------
