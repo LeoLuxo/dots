49cf494a08529e83a704751b8e3d0061f8d563c7
Chloé G <contact@leoluxo.eu>
2025-06-13 14:44:03 +0200

    coffee 382 current  2025-06-13 14:44:01  25.11.20250607.3e3afe5  6.12.32
    M	flake.lock
    M	flake.nix
    M	old/modules/apps/discord/vesktop/default.nix
    R100	old/modules/apps/discord/vesktop/assets/bongo-cat.gif	old/modules/apps/discord/vesktop/overlays/assets/bongo-cat.gif
    R100	old/modules/apps/discord/vesktop/assets/discord.ico	old/modules/apps/discord/vesktop/overlays/assets/discord.ico
    R100	old/modules/apps/discord/vesktop/assets/discord.png	old/modules/apps/discord/vesktop/overlays/assets/discord.png
    R100	old/modules/apps/discord/vesktop/icons-and-name.nix	old/modules/apps/discord/vesktop/overlays/customIconsAndName.nix
    R100	old/modules/apps/discord/vesktop/keybinds-fix.nix	old/modules/apps/discord/vesktop/overlays/keybindsFixOld.nix
    A	old/modules/apps/discord/vesktop/overlays/pinPackage.nix
    R100	old/modules/apps/discord/vesktop/use-custom-vencord.patch	old/modules/apps/discord/vesktop/patches/use-custom-vencord.patch
    R100	old/modules/apps/discord/vesktop/vencord-disable-update-check.patch	old/modules/apps/discord/vesktop/patches/vencord-disable-update-check.patch
    R100	old/modules/apps/discord/vesktop/vesktop-disable-update-check.patch	old/modules/apps/discord/vesktop/patches/vesktop-disable-update-check.patch

--- old/modules/apps/discord/vesktop/default.nix
+++ old/modules/apps/discord/vesktop/default.nix
@@ -18,28 +18,29 @@ in
 
 {
   imports = [
-    ./icons-and-name.nix
-    # ./keybinds-fix.nix
+    ./overlays/customIconsAndName.nix
+    ./overlays/pinPackage.nix
+    # ./keybindsFixOld.nix
 
-    (mkQuickPatch {
-      package = "vencord";
-      patches = [
-        ./vencord-disable-update-check.patch
-      ];
-    })
+    # (mkQuickPatch {
+    #   package = "vencord";
+    #   patches = [
+    #     ./patches/vencord-disable-update-check.patch
+    #   ];
+    # })
 
-    (mkQuickPatch {
-      package = "vesktop";
-      patches = [
-        ./vesktop-disable-update-check.patch
+    # (mkQuickPatch {
+    #   package = "vesktop";
+    #   patches = [
+    #     ./patches/vesktop-disable-update-check.patch
 
-        # Vencord is being a little annoying so use our custom vencord and patch that to disable updates
-        # (pkgs.substituteAll {
-        #   src = ./use-custom-vencord.patch;
-        #   inherit (pkgs) vencord;
-        # })
-      ];
-    })
+    #     # Vencord is being a little annoying so use our custom vencord and patch that to disable updates
+    #     # (pkgs.substituteAll {
+    #     #   src = ./patches/use-custom-vencord.patch;
+    #     #   inherit (pkgs) vencord;
+    #     # })
+    #   ];
+    # })
 
     (mkGlobalKeybind {
       name = "Discord mute";
@@ -74,11 +75,10 @@ in
 
   home-manager.users.${user} = {
     home.packages = with pkgs; [
-      # (vesktop.override {
-      #   withSystemVencord = true;
-      #   inherit vencord;
-      # })
-      vesktop
+      (vesktop.override {
+        withSystemVencord = true;
+        inherit vencord;
+      })
     ];
   };
 }
