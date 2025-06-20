18aa06dd9d2063e0a2d7ccd61c6b8cf53f374dc9
LeoLuxo <contact@me.leoluxo.eu>
2025-06-20 21:38:00 +0200

    coffee 403 current  2025-06-20 21:37:58  25.11.20250607.3e3afe5  6.12.32
    A	lib/default.nix
    A	lib/options.nix
    A	lib/paths.nix
    A	lib/strings.nix
    M	nixosModules/nix/nix.nix
    A	nixosModules/nix/packages.nix
    M	old/hosts/coffee/software.nix
    M	old/hosts/pancake/software.nix

---
+++ lib/options.nix
@@ -0,0 +1,131 @@
+{ lib, ... }:
+let
+  inherit (lib) types;
+in
+
+rec {
+  enabled = {
+    enable = true;
+  };
+
+  disabled = {
+    enable = false;
+  };
+
+  mkOpt =
+    description: type: default:
+    lib.mkOption { inherit type default description; };
+
+  mkOpt' = description: type: lib.mkOption { inherit type description; };
+
+  mkNullOr = description: type: mkOpt (types.nullOr type) null description;
+
+  mkEnum =
+    description: variants: default:
+    lib.mkOption {
+      type = types.enum variants;
+      inherit description default;
+    };
+
+  mkEnum' =
+    description: variants:
+    lib.mkOption {
+      type = types.enum variants;
+      inherit description;
+    };
+
+  /**
+    Creates a submodule option with specified options.
+
+    # Example
+
+    ```nix
+    mkSubmodule "A submodule" { foo = mkOpt types.str "bar"; }
+    =>
+    Option of type submodule with specified options
+    ```
+
+    # Type
+
+    ```
+    mkSubmodule :: String -> AttrSet -> Option
+    ```
+
+    # Arguments
+
+    description
+    : Documentation string for the submodule option
+
+    opts
+    : The options to include in the submodule
+  */
+  mkSubmodule =
+    description: opts:
+    lib.mkOption {
+      type = types.submodule {
+        inherit opts;
+      };
+      default = { };
+      inherit description;
+    };
+
+  mkSubmoduleNull =
+    description: opts:
+    lib.mkOption {
+      type = types.nullOr (
+        types.submodule {
+          inherit opts;
+        }
+      );
+      default = null;
+      inherit description;
+    };
+
+  /**
+    Creates an attribute set of submodules with specified options.
+
+    # Example
+
+    ```nix
+    mkAttrsSub "A set of submodules" { foo = mkOpt types.str "bar"; }
+    =>
+    Option of type attrsOf(submodule) with specified options
+    ```
+
+    # Type
+
+    ```
+    mkAttrsSub :: String -> AttrSet -> Option
+    ```
+
+    # Arguments
+
+    description
+    : Documentation string for the submodules attribute set
+
+    opts
+    : The options to include in each submodule
+  */
+  mkAttrsSub =
+    description: opts: default:
+    lib.mkOption {
+      type = types.attrsOf (
+        types.submodule {
+          inherit opts;
+        }
+      );
+      inherit description default;
+    };
+
+  mkAttrsSub' =
+    description: opts:
+    lib.mkOption {
+      type = types.attrsOf (
+        types.submodule {
+          inherit opts;
+        }
+      );
+      default = { };
+      inherit description;
+    };
+}
