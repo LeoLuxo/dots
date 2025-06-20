63c0bdf94980ee1864ae64bb516c86a7851fc7b8
Chloé G <contact@chlookie.net>
2025-06-20 22:08:09 +0200

    coffee 407 current  2025-06-20 22:08:06  25.11.20250607.3e3afe5  6.12.32
    M	nixosModules/nix/nix.nix
    M	old/hosts/base.nix

--- nixosModules/nix/nix.nix
+++ nixosModules/nix/nix.nix
@@ -3,9 +3,16 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # Enable the new nix cli tool and flakes
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
+  nix.settings = {
+    # Enable the new nix cli tool and flakes
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+
+    # A list of names of users that have additional rights when connecting to the Nix daemon, such as the ability to specify additional binary caches, or to import unsigned NARs.
+    trusted-users = [
+      "root"
+    ];
+  };
 }
