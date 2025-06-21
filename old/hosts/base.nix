7ba002049f1040c2cba89184bc5e86f038083583
Chloé G <contact@leoluxo.eu>
2025-06-21 21:56:48 +0200

    coffee 411 current  2025-06-21 21:50:26  25.11.20250607.3e3afe5  6.12.32
    A	nixosModules/nix/hm.nix
    M	old/hosts/base.nix

--- old/hosts/base.nix
+++ old/hosts/base.nix
@@ -8,30 +8,30 @@
 {
   imports = [
     # Include home manager module
-    inputs.home-manager.nixosModules.home-manager
+    # inputs.home-manager.nixosModules.home-manager
 
     nixosModulesOld.defaults
   ];
 
   # Home-Manager config
-  home-manager = {
-    useGlobalPkgs = true;
-    useUserPackages = true;
+  # home-manager = {
+  #   useGlobalPkgs = true;
+  #   useUserPackages = true;
 
-    # On activation move existing files by appending the given file extension rather than exiting with an error.
-    # Applies to home.file and also xdg.*File
-    backupFileExtension = "bak";
+  #   # On activation move existing files by appending the given file extension rather than exiting with an error.
+  #   # Applies to home.file and also xdg.*File
+  #   backupFileExtension = "bak";
 
-    users.${config.my.system.user.name} = {
-      # Do not change
-      home.stateVersion = "24.05";
+  #   users.${config.my.system.user.name} = {
+  #     # Do not change
+  #     home.stateVersion = "24.05";
 
-      # Home Manager needs a bit of information about you and the paths it should manage.
-      home.username = config.my.system.user.name;
-      home.homeDirectory = config.my.system.user.home;
+  #     # Home Manager needs a bit of information about you and the paths it should manage.
+  #     home.username = config.my.system.user.name;
+  #     home.homeDirectory = config.my.system.user.home;
 
-      # Let Home Manager install and manage itself.
-      programs.home-manager.enable = true;
-    };
-  };
+  #     # Let Home Manager install and manage itself.
+  #     programs.home-manager.enable = true;
+  #   };
+  # };
 }
