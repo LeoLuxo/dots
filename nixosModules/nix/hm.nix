6da4c5b697a1823651f2a1a30012dbdec4a38af8
Chloé G <contact@chlookie.net>
2025-06-21 21:56:48 +0200

    coffee 411 current  2025-06-21 21:50:26  25.11.20250607.3e3afe5  6.12.32
    A	nixosModules/nix/hm.nix
    M	old/hosts/base.nix

---
+++ nixosModules/nix/hm.nix
@@ -0,0 +1,65 @@
+{
+  lib,
+  options,
+  config,
+  inputs,
+  ...
+}:
+
+let
+  lib2 = inputs.self.lib;
+  inherit (lib) types;
+
+  userCfg = config.my.system.user;
+in
+{
+  imports = [
+    inputs.home-manager.nixosModules.home-manager
+  ];
+
+  options.my.hm =
+    with lib2.options;
+    mkOpt "Options to pass directly to home-manager." types.attrs { };
+
+  config = lib.mkIf (userCfg.enable) {
+    # Home-Manager config
+    home-manager = {
+      useGlobalPkgs = true;
+      useUserPackages = true;
+
+      # On activation move existing files by appending the given file extension rather than exiting with an error.
+      # Applies to home.file and also xdg.*File
+      backupFileExtension = "bak";
+
+      users.${userCfg.name} = lib.mkAliasDefinitions options.my.hm;
+    };
+
+    my.hm = {
+      # Do not change
+      home.stateVersion = "24.05";
+
+      # Home Manager needs a bit of information about you and the paths it should manage.
+      home.username = userCfg.name;
+      home.homeDirectory = userCfg.home;
+
+      # Let Home Manager install and manage itself.
+      programs.home-manager.enable = true;
+
+      # Customize default directories
+      xdg.userDirs = {
+        enable = true;
+        createDirectories = true;
+
+        download = "${userCfg.home}/downloads";
+
+        music = "${userCfg.home}/media";
+        pictures = "${userCfg.home}/media";
+        videos = "${userCfg.home}/media";
+
+        desktop = "${userCfg.home}/misc";
+        documents = "${userCfg.home}/misc";
+        publicShare = "${userCfg.home}/misc";
+      };
+    };
+  };
+}
