d933eff92409800a5c04410785ed0fd28d70a20e
Chloé G <contact@leoluxo.eu>
2025-07-06 10:07:53 +0200

    coffee 479 current  2025-07-06 10:07:50  25.11.20250607.3e3afe5  6.12.32
    M	flakelightCompat.nix
    A	nixosModules/apps/zoxide.nix
    M	old/hosts/coffee/default.nix

---
+++ nixosModules/apps/zoxide.nix
@@ -0,0 +1,32 @@
+{
+  lib,
+  config,
+  inputs,
+  ...
+}:
+
+let
+  lib2 = inputs.self.lib;
+  inherit (lib2) enabled;
+  inherit (lib.options) mkEnableOption;
+  inherit (lib.modules) mkIf;
+
+  cfg = config.my.apps.zoxide;
+in
+{
+  options.my.apps.zoxide = {
+    enable = mkEnableOption "zoxide, the smarter cd command";
+
+    enableAlias = mkEnableOption "the cd='z' alias" // {
+      default = true;
+    };
+  };
+
+  config = mkIf cfg.enable {
+    programs.zoxide = enabled;
+
+    home-manager.users.${config.my.system.user.name}.home.shellAliases = mkIf cfg.enableAlias {
+      cd = "z";
+    };
+  };
+}
