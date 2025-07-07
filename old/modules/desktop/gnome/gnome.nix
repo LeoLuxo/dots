b784d9c13a1eec9b8cb43f9f9344c4c5f286b4df
LeoLuxo <contact@me.leoluxo.eu>
2025-07-07 10:55:08 +0200

    coffee 483 current  2025-07-07 08:50:26  25.05pre-git            6.12.36
    M	lib/modules.nix
    M	lib/options.nix
    M	nixosModules/desktop/keybinds.nix
    M	old/modules/desktop/gnome/gnome.nix

--- old/modules/desktop/gnome/gnome.nix
+++ old/modules/desktop/gnome/gnome.nix
@@ -99,8 +99,8 @@ in
       # Enable the gnome keyring
       services.gnome.gnome-keyring.enable = true;
 
-      my.desktop.defaultApps.backupTerminal = lib.mkDefault "kgx";
-      my.desktop.defaultApps.terminal = lib.mkOverride 1050 "kgx";
+      my.desktop.defaultApps.backupTerminal = lib.mkDefault "kgx"; # Priority 1000
+      my.desktop.defaultApps.terminal = lib.mkOverride 1050 "kgx"; # Even lower priority than mkDefault (smaller = higher priority)
 
       # Fixes "Your GStreamer installation is missing a plug-in." when trying to view audio/video properties
       environment.sessionVariables.GST_PLUGIN_SYSTEM_PATH_1_0 =
