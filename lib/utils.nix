f263839229994a89c3acbe0d7d11d12b8e1203e9
Chloé G <contact@leoluxo.eu>
2025-07-08 17:07:04 +0200

    coffee 513 current  2025-07-08 17:07:00  25.05pre-git            6.12.36
    M	lib/utils.nix
    M	old/modules/scripts/terminal-utils/q.sh

--- lib/utils.nix
+++ lib/utils.nix
@@ -1,28 +1,6 @@
 { lib, ... }:
 
 {
-  /**
-    This function sanitizes a file system path to make it compatible with the Nix store
-
-    # Example
-
-    ```nix
-    sanitizePath "/path/with spaces and special-chars!"
-    =>
-    /nix/store/hash-path-with-spaces-and-special-chars
-    ```
-
-    # Type
-
-    ```
-    sanitizePath :: Path -> Path
-    ```
-
-    # Arguments
-
-    path
-    : The filesystem path to be sanitized
-  */
   sanitizePath =
     path:
     builtins.path {
