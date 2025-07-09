f468e64b8cc7cd582c8ccd16b5c8def17517e0b0
LeoLuxo <contact@me.leoluxo.eu>
2025-07-09 13:02:18 +0200

    coffee 524 current  2025-07-09 12:49:31  25.05pre-git            6.12.36
    A	nixosModules/base/const.nix
    D	nixosModules/base/defaultApps.nix
    M	nixosModules/base/nix.nix
    D	nixosModules/base/packages.nix
    D	nixosModules/base/paths.nix
    D	nixosModules/system/keys.nix

---
+++ nixosModules/base/const.nix
@@ -0,0 +1,91 @@
+{
+  lib,
+  config,
+  ...
+}:
+
+let
+  inherit (lib) types;
+  inherit (lib.my) notNullOr mkAttrs;
+  inherit (lib.options) mkOption;
+  inherit (lib.modules) mkIf;
+in
+
+let
+  mkDefaultAppEntry =
+    description:
+    mkOption {
+      inherit description;
+      type = types.nullOr types.str;
+      default = null;
+    };
+
+  mkPathEntry =
+    args:
+    mkOption {
+      type = types.nullOr types.path;
+      default = null;
+    }
+    // args;
+
+in
+{
+  options.my = {
+
+    # Default applications to use for a certain context
+    defaultApps = {
+      browser = mkDefaultAppEntry "The default internet browsing app.";
+      notes = mkDefaultAppEntry "The default note-taking app.";
+      communication = mkDefaultAppEntry "The default communication app.";
+      codeEditor = mkDefaultAppEntry "The default code editor.";
+      terminal = mkDefaultAppEntry "The default terminal emulator.";
+      backupTerminal = mkDefaultAppEntry "The backup terminal emulator.";
+    };
+
+    # Paths to be references in this config
+    paths = {
+      "home" = mkPathEntry {
+        description = "the default home folder; by default is set as the user's home if this nixos-config is configured as a single-user system (`/home/<user>` by default), `/root` otherwise";
+        default = notNullOr config.my.user.home "/root";
+      };
+
+      "nixos" = mkPathEntry {
+        description = "the nixos config folder";
+        default = "/etc/nixos";
+      };
+
+      "nixosRepo" = mkPathEntry {
+        description = "the working copy of this repository";
+      };
+    };
+
+    # Key pairs
+    keys = mkAttrs {
+      description = "key pairs available on the machine and accessible to the other nixos modules";
+
+      options = {
+        public = mkOption {
+          description = "the path of this pair's public key";
+          type = types.path;
+        };
+        private = mkOption {
+          description = "the path of this pair's private key";
+          type = types.path;
+        };
+      };
+
+      default = {
+        # TODO: Move this to presets
+        user = mkIf (config.my.user != null) {
+          private = "${config.my.user.home}/.ssh/id_ed25519";
+          public = "${config.my.user.home}/.ssh/id_ed25519.pub";
+        };
+
+        host = {
+          private = "/etc/ssh/ssh_host_ed25519_key";
+          public = "/etc/ssh/ssh_host_ed25519_key.pub";
+        };
+      };
+    };
+  };
+}
