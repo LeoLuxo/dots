fd432200cece2baa7696a18cf169af50d07213e9
Chloé G <contact@chlookie.net>
2025-07-10 20:15:28 +0200

    coffee 534 current  2025-07-10 20:05:46  25.05pre-git            6.12.36
    A	nixosModules/hardware/controller/playstation.nix
    A	nixosModules/hardware/gpu/amd.nix
    A	nixosModules/hardware/gpu/nvidia.nix
    M	old/hosts/coffee/default.nix
    M	old/hosts/coffee/old/system.nix

---
+++ nixosModules/hardware/gpu/amd.nix
@@ -0,0 +1,29 @@
+{
+  lib,
+  config,
+  ...
+}:
+
+let
+  inherit (lib.options) mkEnableOption;
+  inherit (lib.modules) mkIf;
+
+  cfg = config.my.hardware.gpu.amd;
+in
+
+{
+  options.my.hardware.gpu.amd = {
+    enable = mkEnableOption "AMD gpu compatibility";
+  };
+
+  config = mkIf cfg.enable {
+    services.xserver.videoDrivers = [ "modesetting" ];
+
+    hardware.graphics = {
+      enable = true;
+      enable32Bit = true;
+    };
+
+    hardware.amdgpu.initrd.enable = true;
+  };
+}
