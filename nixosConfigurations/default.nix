aa408620ac4b8dc3e3fe203f23c84a3e539b065b
Chloé G <contact@leoluxo.eu>
2025-08-14 17:46:06 +0200

    Remove oldCompat in favor of nixosConfigurations/default.nix

---
+++ nixosConfigurations/default.nix
@@ -0,0 +1,94 @@
+{ inputs, ... }:
+let
+  mkPkgsOverlay =
+    name: nixpkgs:
+    (final: prev: {
+      ${name} = import nixpkgs {
+        inherit prev;
+        system = prev.system;
+        config.allowUnfree = true;
+      };
+    });
+
+  hostOverlays = [
+    # Some extra instances of pkgs, so that my nixosConfigurations can pin packages more easily.
+    # Can be used via:
+    # ```
+    #   packages = [ pkgs.unstable.firefox ];
+    # ```
+    (mkPkgsOverlay "stable" inputs.nixpkgs-stable)
+    (mkPkgsOverlay "unstable" inputs.nixpkgs-unstable)
+    (mkPkgsOverlay "25-05" inputs.nixpkgs-25-05)
+    (mkPkgsOverlay "24-11" inputs.nixpkgs-24-11)
+    (mkPkgsOverlay "24-05" inputs.nixpkgs-24-05)
+
+    # Merge my custom libs with the normal nixpkgs.lib
+    (final: prev: {
+      lib = prev.lib // {
+        # TODO rename lib.my to something else
+        my = import ../lib {
+          lib = prev.lib;
+          pkgs = prev;
+        };
+      };
+    })
+  ];
+
+  mkHost =
+    user: hostname: module:
+    inputs.nixpkgs.lib.nixosSystem (
+      let
+        # TODO remove
+        system = "x86_64-linux";
+
+        defaultConstants = rec {
+          # userHome = "/home/${user}";
+          nixosPath = "/etc/nixos";
+          secretsPath = "/home/lili/misc/secrets";
+
+          nixosRepoPath = (nixosPath + "/dots");
+        };
+
+        constants = defaultConstants // {
+          inherit hostname system;
+        };
+
+        extraLib = import ../old/libs.nix {
+          inherit inputs constants;
+        };
+
+        nixosModulesOld = extraLib.findFiles {
+          dir = ./old/modules;
+          extensions = [ "nix" ];
+          defaultFiles = [ "default.nix" ];
+        };
+      in
+      {
+        modules = [
+          module
+          { nixpkgs.overlays = hostOverlays; }
+        ];
+
+        # Additional args passed to the modules
+        specialArgs = {
+          inherit inputs hostname user;
+
+          # TODO remove
+          inherit
+            extraLib
+            nixosModulesOld
+            constants
+            system
+            ;
+        };
+      }
+    );
+
+in
+{
+  # Personal desktop computer
+  coffee = mkHost "lili" "coffee" ./hosts/coffee;
+
+  # Surface Pro 7 laptop
+  pancake = mkHost "lili" "pancake" ./hosts/pancake;
+}
