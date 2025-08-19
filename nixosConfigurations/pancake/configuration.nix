2114398426590bcc8ac836c1602b62d01946ccbe
Chloé G <contact@leoluxo.eu>
2025-08-19 16:38:25 +0200

    pancake 8 current   2025-08-19 16:01:51  25.05.20250705.29e2900   6.12.19
    M	nixosConfigurations/hosts.nix
    M	nixosConfigurations/pancake/configuration.nix
    A	nixosConfigurations/pancake/default.nix

--- nixosConfigurations/pancake/configuration.nix
+++ nixosConfigurations/pancake/configuration.nix
@@ -12,13 +12,6 @@ let
 in
 {
   imports = [
-    ./hardwareConfiguration.nix
-    ./syncthing.nix
-    ./wifi.nix
-
-    # TODO: Remove
-    (import "${inputs.self}/oldNewNixosModules" { inherit lib; }).default
-
     # Include hardware stuff and kernel patches for surface pro 7
     inputs.nixos-hardware.nixosModules.microsoft-surface-pro-intel
 
@@ -194,12 +187,4 @@ in
     # no need to redefine it in your config for now)
     #media-session.enable = true;
   };
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05"; # Did you read the comment?
 }
