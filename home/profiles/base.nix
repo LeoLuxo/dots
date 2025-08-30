cac619f653a540ef35f35a90d2172a68db8bd04b
Chloé G <contact@chlookie.net>
2025-08-30 20:50:39 +0200

    Integrate hm.nix

--- home/profiles/base.nix
+++ home/profiles/base.nix
@@ -1,9 +1,38 @@
 { user, ... }:
+
+let
+  home = "/home/${user}";
+in
 {
   imports = [ ./agenix.nix ];
 
+  # Do not change
+  home.stateVersion = "24.05";
+
+  # Home Manager needs a bit of information about you and the paths it should manage.
   home = {
     username = user;
-    homeDirectory = "/home/${user}";
+    homeDirectory = home;
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # Customize default directories
+  xdg.userDirs = {
+    enable = true;
+    createDirectories = true;
+
+    download = "${home}/downloads";
+
+    music = "${home}/media";
+    pictures = "${home}/media";
+    videos = "${home}/media";
+
+    desktop = "${home}/misc";
+    documents = "${home}/misc";
+
+    templates = null;
+    publicShare = null;
   };
 }
