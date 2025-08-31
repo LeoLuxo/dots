bf890d362203a469293dd24ed53abab46b7101ce
LeoLuxo <contact@me.leoluxo.eu>
2025-08-31 17:51:31 +0200

    Rework hosts structure more

--- hosts.nix
+++ hosts.nix
@@ -1,26 +1,20 @@
 {
   # Personal desktop computer
   "coffee" = {
+    hostname = "coffee";
     os = "nixos";
+    nixosConfig = import ./configs/coffee/nixos;
 
-    nixosConfig = {
-      hostname = "coffee";
-      module = import ./configs/coffee/nixos;
-
-      users = {
-        "lili" = {
-          uid = 1000;
-          module = import ./configs/coffee/home.nix;
-        };
+    users = {
+      "lili" = {
+        uid = 1000;
+        homeConfig = import ./configs/coffee/home.nix;
       };
     };
 
     ip.local = "192.168.0.88";
 
-    ssh = {
-      user = "lili";
-      hostname = "coffee";
-    };
+    ssh.user = "lili";
 
     syncthing.id = "WKZDG5X-W2DJB2N-3A7CS2H-VQDKBN2-RFDLM6P-KGZN4D6-KI2SD3E-3ZMNQAT";
   };
@@ -33,14 +27,12 @@
 
   # Surface Pro 7 laptop
   "pancake" = {
+    hostname = "pancake";
     os = "nixos";
 
     ip.local = "192.168.0.173";
 
-    ssh = {
-      user = "lili";
-      hostname = "pancake";
-    };
+    ssh.user = "lili";
 
     syncthing.id = "DS5FS25-BYJYFF2-TKBNJ4S-6RHZTEK-F4QS4EM-BNOPAPU-ULRHUA7-ORVTNA7";
   };
@@ -53,14 +45,12 @@
 
   # Raspberry Pi Zero
   "strobery" = {
+    hostname = "strobery";
     os = "raspbian";
 
     ip.local = "192.168.0.37";
 
-    ssh = {
-      user = "cake";
-      hostname = "strobery";
-    };
+    ssh.user = "cake";
 
     syncthing.id = "BH4QRX3-AXCRBBK-32KWW2A-33XYEMB-CKDONYH-4KLE4QA-NXE5LIX-QB4Q5AN";
   };
--- mkConfigs.nix
+++ mkConfigs.nix
@@ -2,6 +2,7 @@ args@{
   inputs,
   lib,
   lib2,
+  hosts,
   ...
 }:
 
@@ -13,9 +14,10 @@ in
   mkNixosConfig =
     {
       hostname,
-      users,
-      module,
+      nixosConfig,
+      users ? { },
       autologin ? null,
+      ...
     }:
     inputs.nixpkgs.lib.nixosSystem {
       specialArgs = {
@@ -27,7 +29,7 @@ in
       modules =
         [
           # Include the main module
-          module
+          nixosConfig
 
           inputs.home-manager.nixosModules.home-manager
 
@@ -38,7 +40,7 @@ in
               useUserPackages = true;
               extraSpecialArgs = {
                 inherit inputs lib2;
-                inherit hostname;
+                inherit hostname hosts;
                 homeProfiles = home.profiles;
               };
 
@@ -48,7 +50,7 @@ in
                   imports =
                     [
                       # Include the main module for the user
-                      userCfg.module
+                      userCfg.homeConfig
 
                       # Set up the current user for home-manager, as there's not really any way to pass along the current user to modules other than `config.home.username`
                       { home.username = username; }
@@ -65,19 +67,21 @@ in
         ++ (lib.attrValues nixos.modules);
     };
 
-  mkHomeManagerConfigs =
-    users:
-    lib.concatMapAttrs (username: userCfg: {
+  mkHomeManagerConfig =
+    username:
+    { homeConfig, ... }:
+    inputs.home-manager.lib.homeManagerConfiguration {
       # Additional args passed to home-manager modules
       extraSpecialArgs = {
         inherit (inputs.self) homeModules homeProfiles;
         inherit inputs lib2;
+        inherit hosts;
       };
 
       modules =
         [
           # Include the main module for the user
-          userCfg.module
+          homeConfig
 
           # Auto-include the profile for non-nixos machines
           inputs.self.homeProfiles.nonNixos
@@ -87,84 +91,5 @@ in
 
         # Auto-include all custom home-manager modules
         ++ (lib.attrValues inputs.self.homeModules);
-    }) users;
-
-  # mkHost =
-  #   {
-  #     hostname,
-  #     user,
-  #     nixosModules,
-  #     homeModules,
-  #   }:
-  #   let
-  #     # Additional args passed to ALL modules (both hm and nixos)
-  #     specialArgs = {
-  #       inherit inputs lib2;
-  #       inherit hostname user;
-  #     };
-  #   in
-  #   inputs.nixpkgs.lib.nixosSystem ({
-  #     # Additional args passed to nixos modules
-  #     specialArgs = specialArgs // {
-  #       inherit (inputs.self) nixosModules nixosProfiles;
-  #     };
-
-  #     modules =
-  #       nixosModules
-  #       # Auto-include all custom nixos modules
-  #       ++ (lib.attrValues inputs.self.nixosModules)
-  #       # Include home-manager and its modules
-  #       ++ [
-  #         inputs.home-manager.nixosModules.home-manager
-
-  #         {
-  #           nixpkgs.overlays = [
-  #             inputs.self.overlays.pkgs
-  #             inputs.self.overlays.builders
-  #           ];
-
-  #           home-manager = {
-  #             # useGlobalPkgs = true;
-  #             useUserPackages = true;
-  #             extraSpecialArgs = specialArgs // {
-  #               inherit (inputs.self) homeModules homeProfiles;
-  #             };
-
-  #             users.${user} = {
-  #               imports =
-  #                 homeModules
-  #                 # Auto-include all custom home-manager modules
-  #                 ++ (lib.attrValues inputs.self.homeModules);
-  #             };
-  #           };
-  #         }
-  #       ];
-  #   });
-
-  # mkHome =
-  #   {
-  #     hostname,
-  #     user,
-  #     modules,
-  #   }:
-  #   inputs.home-manager.lib.homeManagerConfiguration {
-  #     # inherit pkgs;
-
-  #     # Additional args passed to home-manager modules
-  #     extraSpecialArgs = {
-  #       inherit (inputs.self) homeModules homeProfiles;
-
-  #       inherit inputs lib2;
-  #       inherit hostname user;
-  #     };
-
-  #     modules =
-  #       modules
-  #       # Auto-include the profile for non-nixos machines
-  #       ++ [
-  #         inputs.self.homeProfiles.nonNixos
-  #       ]
-  #       # Auto-include all custom home-manager modules
-  #       ++ (lib.attrValues inputs.self.homeModules);
-  #   };
+    };
 }
