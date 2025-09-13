e9d2ca89488149aaba450e029713f44eff4b6e9b
Chloé G <contact@leoluxo.eu>
2025-09-13 22:16:32 +0200

    coffee 563 current  2025-09-13 22:16:31  25.05.20250705.29e2900  6.12.36
    M	nixosModules/apps/discord/vesktop/default.nix
    A	nixosModules/apps/discord/vesktop/patches/disableUpdateChecking.patch
    A	nixosModules/apps/discord/vesktop/patches/fixReadOnlySettings.patch
    A	nixosModules/apps/discord/vesktop/patches/globalShortcuts.patch
    A	nixosModules/apps/discord/vesktop/vencord.nix
    A	nixosModules/apps/discord/vesktop/vesktop.nix

--- nixosModules/apps/discord/vesktop/default.nix
+++ nixosModules/apps/discord/vesktop/default.nix
@@ -12,10 +12,15 @@ let
     ;
 in
 
+let
+  vencord = pkgs.callPackage ./vencord.nix { };
+  vesktop = pkgs.callPackage ./vesktop.nix { inherit vencord; };
+in
+
 {
   imports = [
     ./overlays/customIconsAndName.nix
-    ./overlays/globalKeybinds.nix
+    # ./overlays/globalKeybinds.nix
 
     # (mkQuickPatch {
     #   package = "vencord";
@@ -37,35 +42,78 @@ in
     #   ];
     # })
 
-    (mkSyncedPath {
-      xdgPath = "vesktop/settings/settings.json";
-      cfgPath = "vesktop/vencord.json";
-    })
+    # (mkSyncedPath {
+    #   xdgPath = "vesktop/settings/settings.json";
+    #   cfgPath = "vesktop/vencord.json";
+    # })
 
-    (mkSyncedPath {
-      xdgPath = "vesktop/settings.json";
-      cfgPath = "vesktop/vesktop.json";
-    })
+    # (mkSyncedPath {
+    #   xdgPath = "vesktop/settings.json";
+    #   cfgPath = "vesktop/vesktop.json";
+    # })
+
   ];
+
+  # nixpkgs.overlays = [
+  #   (
+  #     final: prev:
+
+  #     let
+  #       pinnedPkgs = import (builtins.fetchTarball {
+  #         url = "https://github.com/NixOS/nixpkgs/archive/fd27151c1e098faa7838bd569f6e5b5681b11ccf.tar.gz";
+  #       }) { };
+  #     in
+  #     {
+  #       vesktop = pinnedPkgs.vesktop.overrideAttrs (
+  #         finalAttrs: oldAttrs: {
+  #           src = prev.fetchFromGitHub {
+  #             owner = "tuxinal";
+  #             repo = "Vesktop";
+  #             rev = "25963c66bed4643377044dea36e44ee68d13a3c8";
+  #             hash = "sha256-7u3kmpgCWCj+0sSxCg8cylulWVv8oAW6H5jy8f4o+tQ=";
+  #           };
+
+  #           pnpmDeps = prev.pnpm_10.fetchDeps {
+  #             inherit (finalAttrs)
+  #               pname
+  #               version
+  #               src
+  #               patches
+  #               ;
+  #             hash = "sha256-Xmcx0hWS5F4nEi83Hc6BvRu7Okw3LTwa7zGU3rJ+Udk=";
+  #           };
+
+  #           # patches = [
+  #           #   (prev.fetchpatch {
+  #           #     url = "https://patch-diff.githubusercontent.com/raw/Vencord/Vesktop/pull/326.patch";
+  #           #     hash = "sha256-CTMETKHDdgj3mp/40Ps49GNC9gfRe1YhNF9KQ7IVxnE=";
+  #           #   })
+  #           # ];
+  #         }
+  #       );
+  #     }
+  #   )
+  # ];
 
   my.defaultApps.communication = lib.mkDefault "vesktop";
 
-  my.packages = with pkgs; [
-    (vesktop.override {
-      withSystemVencord = true;
-      inherit vencord;
-    })
+  my.packages = [
+    # (vesktop.override {
+    #   withSystemVencord = true;
+    #   inherit vencord;
+    # })
+
   ];
 
-  my.keybinds = {
-    "Discord mute" = {
-      binding = "<Super>m";
-      command = "echo \"VCD_TOGGLE_SELF_MUTE\" >> $XDG_RUNTIME_DIR/vesktop-ipc";
-    };
+  # my.keybinds = {
+  #   "Discord mute" = {
+  #     binding = "<Super>m";
+  #     command = "echo \"VCD_TOGGLE_SELF_MUTE\" >> $XDG_RUNTIME_DIR/vesktop-ipc";
+  #   };
 
-    "Discord deafen" = {
-      binding = "<Super><Shift>m";
-      command = "echo \"VCD_TOGGLE_SELF_DEAF\" >> $XDG_RUNTIME_DIR/vesktop-ipc";
-    };
-  };
+  #   "Discord deafen" = {
+  #     binding = "<Super><Shift>m";
+  #     command = "echo \"VCD_TOGGLE_SELF_DEAF\" >> $XDG_RUNTIME_DIR/vesktop-ipc";
+  #   };
+  # };
 }
