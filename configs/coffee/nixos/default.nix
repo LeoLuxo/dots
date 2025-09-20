c04939fac2e83e7807853b8d1576a8e1543e19c5
Chloé G <contact@chlookie.net>
2025-09-20 20:59:59 +0200

    Finish coffee

--- configs/coffee/nixos/default.nix
+++ configs/coffee/nixos/default.nix
@@ -12,6 +12,18 @@
     nixosProfiles.apps.syncthing
   ];
 
+  # 1TB SSD
+  fileSystems."/stuff" = {
+    device = "/dev/disk/by-label/stuff";
+    fsType = "ext4";
+  };
+
+  # 4TB HDD
+  fileSystems."/backup" = {
+    device = "/dev/disk/by-label/backup";
+    fsType = "ntfs";
+  };
+
   system.stateVersion = "24.05";
 
   pinKernel = {
