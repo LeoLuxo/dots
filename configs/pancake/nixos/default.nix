615fcf8d2ec5a4b54ea618982aa5b2180e076039
LeoLuxo <contact@me.leoluxo.eu>
2025-09-20 21:03:34 +0200

    Finish pancake

--- configs/pancake/nixos/default.nix
+++ configs/pancake/nixos/default.nix
@@ -1,4 +1,9 @@
-{ inputs, nixosProfiles, ... }:
+{
+  inputs,
+  nixosProfiles,
+  pkgs,
+  ...
+}:
 {
   imports = [
     ./hardware.nix
@@ -11,6 +16,24 @@
 
     nixosProfiles.apps.syncthing
   ];
+
+  # Pin the kernel with nixos-hardware
+  hardware.microsoft-surface.kernelVersion = "longterm";
+
+  # SD Card
+  fileSystems."/stuff" = {
+    device = "/dev/disk/by-label/stuff";
+    fsType = "btrfs";
+  };
+
+  # Touchscreen support
+  services.xserver = {
+    modules = [ pkgs.xf86_input_wacom ];
+    wacom.enable = true;
+  };
+
+  # Also for touchscreen support (or maybe touchpad? unsure)
+  services.libinput.enable = true;
 
   system.stateVersion = "24.05";
 }
