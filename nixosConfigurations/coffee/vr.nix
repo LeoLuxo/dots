f68587c3744cdfbc0d284fa1e906e5be8169b1d4
LeoLuxo <contact@me.leoluxo.eu>
2025-09-21 11:21:44 +0200

    coffee 577 current  2025-09-21 11:21:43  25.05.20250705.29e2900  6.12.36
    M	nixosConfigurations/coffee/default.nix
    A	nixosConfigurations/coffee/vr.nix

---
+++ nixosConfigurations/coffee/vr.nix
@@ -0,0 +1,63 @@
+{ config, pkgs, ... }:
+{
+  boot.kernelPatches = [
+    {
+      name = "amdgpu-ignore-ctx-privileges";
+      patch = pkgs.fetchpatch {
+        name = "cap_sys_nice_begone.patch";
+        url = "https://github.com/Frogging-Family/community-patches/raw/master/linux61-tkg/cap_sys_nice_begone.mypatch";
+        hash = "sha256-Y3a0+x2xvHsfLax/uwycdJf3xLxvVfkfDVqjkxNaYEo=";
+      };
+    }
+  ];
+
+  # services.monado = {
+  #   enable = true;
+  #   defaultRuntime = true; # Register as default OpenXR runtime
+  # };
+
+  # programs.git = {
+  #   enable = true;
+  #   lfs.enable = true;
+  # };
+
+  # my.scripts.nx.rebuild.postRebuildActions = ''
+  #   echo Fixing hand tracking for monado VR
+  #   mkdir -p ~/.local/share/monado
+  #   cd ~/.local/share/monado
+  #   git clone https://gitlab.freedesktop.org/monado/utilities/hand-tracking-models
+  #   systemctl --user restart monado.service
+  # '';
+
+  # systemd.user.services.monado.environment = {
+  #   STEAMVR_LH_ENABLE = "1";
+  #   XRT_COMPOSITOR_COMPUTE = "1";
+  # };
+
+  # environment.systemPackages = [ pkgs.opencomposite ];
+
+  # home-manager.users.${config.my.user.name} = {
+  #   xdg.configFile."openxr/1/active_runtime.json".source =
+  #     "${pkgs.monado}/share/openxr/1/openxr_monado.json";
+
+  #   xdg.configFile."openvr/openvrpaths.vrpath".text = ''
+  #     {
+  #       "config" :
+  #       [
+  #         "${config.xdg.dataHome}/Steam/config"
+  #       ],
+  #       "external_drivers" : null,
+  #       "jsonid" : "vrpathreg",
+  #       "log" :
+  #       [
+  #         "${config.xdg.dataHome}/Steam/logs"
+  #       ],
+  #       "runtime" :
+  #       [
+  #         "${pkgs.opencomposite}/lib/opencomposite"
+  #       ],
+  #       "version" : 1
+  #     }
+  #   '';
+  # };
+}
