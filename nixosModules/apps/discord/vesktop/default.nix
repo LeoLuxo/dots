ec031a167698a5d075238879f569693fe0a97d86
LeoLuxo <contact@me.leoluxo.eu>
2025-09-22 14:25:05 +0200

    coffee 593 current  2025-09-22 14:25:04  25.05.20250705.29e2900  6.12.36
    M	nixosModules/apps/discord/vesktop/default.nix
    A	nixosModules/apps/discord/vesktop/overlays/customIcons.nix
    A	nixosModules/apps/discord/vesktop/vesktop-1.5.5-patch609.nix
    R081	nixosModules/apps/discord/vesktop/vesktop.nix	nixosModules/apps/discord/vesktop/vesktop-1.5.8-patch326.nix

--- nixosModules/apps/discord/vesktop/default.nix
+++ nixosModules/apps/discord/vesktop/default.nix
@@ -12,6 +12,10 @@ let
     ;
 in
 
+# Relevant issues:
+# https://github.com/Vencord/Vesktop/pull/326
+# https://github.com/Vencord/Vesktop/pull/609
+
 {
   imports = [
     # (mkSyncedPath {
@@ -28,19 +32,24 @@ in
 
   my.defaultApps.communication = lib.mkDefault "vesktop";
 
-  my.packages = [
-    (pkgs.callPackage ./vesktop.nix { })
+  nixpkgs.overlays = [
+    (final: prev: {
+      # vesktop = (prev.callPackage ./vesktop-1.5.8-patch326.nix { });
+      vesktop = (prev.callPackage ./vesktop-1.5.5-patch609.nix { });
+    })
+
+    (import ./overlays/customIcons.nix)
   ];
 
-  # my.keybinds = {
-  #   "Discord mute" = {
-  #     binding = "<Super>m";
-  #     command = "echo \"VCD_TOGGLE_SELF_MUTE\" >> $XDG_RUNTIME_DIR/vesktop-ipc";
-  #   };
+  my.keybinds = {
+    "Discord mute" = {
+      binding = "<Super>m";
+      command = "echo \"VCD_TOGGLE_SELF_MUTE\" >> $XDG_RUNTIME_DIR/vesktop-ipc";
+    };
 
-  #   "Discord deafen" = {
-  #     binding = "<Super><Shift>m";
-  #     command = "echo \"VCD_TOGGLE_SELF_DEAF\" >> $XDG_RUNTIME_DIR/vesktop-ipc";
-  #   };
-  # };
+    "Discord deafen" = {
+      binding = "<Super><Shift>m";
+      command = "echo \"VCD_TOGGLE_SELF_DEAF\" >> $XDG_RUNTIME_DIR/vesktop-ipc";
+    };
+  };
 }
