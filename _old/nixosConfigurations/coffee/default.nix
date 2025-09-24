9d532625b3b9dc4268ff4dc3a9d1ac530abb7dea
LeoLuxo <contact@me.leoluxo.eu>
2025-09-24 09:24:41 +0200

    coffee 598 current  2025-09-24 09:24:40  25.05.20250705.29e2900  6.12.36
    D	_old/nixosConfigurations/coffee/audio.nix
    M	_old/nixosConfigurations/coffee/configuration.nix
    M	_old/nixosConfigurations/coffee/default.nix
    D	_old/nixosConfigurations/default.nix
    D	_old/nixosConfigurations/hosts.nix
    M	_old/nixosConfigurations/pancake/configuration.nix
    M	_old/nixosConfigurations/pancake/default.nix
    D	_old/oldNewNixosModules/system/pinKernel.nix
    A	configs/coffee/audio.nix
    A	configs/coffee/default.nix
    R100	_old/nixosConfigurations/coffee/hardwareConfiguration.nix	configs/coffee/hardware.nix
    A	configs/pancake/default.nix
    R100	_old/nixosConfigurations/pancake/hardwareConfiguration.nix	configs/pancake/hardware.nix
    M	hosts.nix
    M	lib.nix
    M	mkConfigs.nix
    A	modules/pinKernel.nix

--- _old/nixosConfigurations/coffee/default.nix
+++ _old/nixosConfigurations/coffee/default.nix
@@ -1,10 +1,10 @@
 { inputs, lib, ... }:
 {
   imports = [
-    ./audio.nix
+    # ./audio.nix
     ./backups.nix
     ./configuration.nix
-    ./hardwareConfiguration.nix
+    # ./hardwareConfiguration.nix
     ./syncthing.nix
     # ./vr.nix
 
@@ -14,6 +14,7 @@
     { system.stateVersion = "24.05"; }
   ];
 
+  # TODO: move
   # Add our custom overlays
   nixpkgs.overlays = [
     inputs.self.overlays.extraPkgs
