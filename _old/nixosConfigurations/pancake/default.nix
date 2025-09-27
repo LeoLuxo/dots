ce3630ecf6c978fdb019712a8471673f7345549c
LeoLuxo <contact@me.leoluxo.eu>
2025-09-27 17:57:35 +0200

    coffee 605 current  2025-09-24 15:19:45  25.05.20250705.29e2900  6.12.36
    M	_old/nixosConfigurations/coffee/default.nix
    M	_old/nixosConfigurations/pancake/default.nix
    M	configs/coffee/default.nix
    M	flake.nix
    D	mkConfigs.nix
    M	packages/wiiuDownloader.nix

--- _old/nixosConfigurations/pancake/default.nix
+++ _old/nixosConfigurations/pancake/default.nix
@@ -10,11 +10,4 @@
 
     { system.stateVersion = "24.05"; }
   ];
-
-  # Add our custom overlays
-  nixpkgs.overlays = [
-    inputs.self.overlays.extraPkgs
-    inputs.self.overlays.builders
-    inputs.self.overlays.packages
-  ];
 }
