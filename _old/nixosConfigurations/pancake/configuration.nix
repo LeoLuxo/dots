7c958caf0c6db3a148eb47710a36a91853f5b1c9
Chloé G <contact@leoluxo.eu>
2025-10-24 19:39:50 +0200

    coffee 624 current  2025-10-24 19:39:06  25.05.20250705.29e2900  6.12.36
    M	_old/nixosConfigurations/pancake/configuration.nix
    M	_old/oldNewNixosModules/suites/pc.nix
    D	_old/oldNewNixosModules/system/locale.nix
    M	profiles/base.nix

--- _old/nixosConfigurations/pancake/configuration.nix
+++ _old/nixosConfigurations/pancake/configuration.nix
@@ -114,25 +114,6 @@ in
   #   fsType = "btrfs";
   # };
 
-  # Set your time zone.
-  time.timeZone = "Europe/Copenhagen";
-
-  # Select internationalisation properties.
-  # Use english (Ireland); see:
-  # https://unix.stackexchange.com/questions/62316/why-is-there-no-euro-english-locale
-  i18n.defaultLocale = "en_IE.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_IE.UTF-8";
-    LC_IDENTIFICATION = "en_IE.UTF-8";
-    LC_MEASUREMENT = "en_IE.UTF-8";
-    LC_MONETARY = "en_IE.UTF-8";
-    LC_NAME = "en_IE.UTF-8";
-    LC_NUMERIC = "en_IE.UTF-8";
-    LC_PAPER = "en_IE.UTF-8";
-    LC_TELEPHONE = "en_IE.UTF-8";
-    LC_TIME = "en_IE.UTF-8";
-  };
-
   # Enable and configure the X11 windowing system.
   services.xserver = {
     enable = true;
