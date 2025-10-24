516c13d8c3ad752f7d08ce4926a9aed03ddcc3d7
Chloé G <contact@chlookie.net>
2025-10-24 19:07:00 +0200

    coffee 620 current  2025-10-24 19:06:59  25.05.20250705.29e2900  6.12.36
    M	_old/nixosModules/apps/obsidian.nix
    M	_old/nixosModules/apps/vscode.nix
    M	_old/nixosModules/rices/peppy/default.nix
    M	configs/coffee/default.nix
    M	configs/pancake/default.nix
    R080	_old/nixosModules/fonts.nix	profiles/pc.nix

--- _old/nixosModules/apps/obsidian.nix
+++ _old/nixosModules/apps/obsidian.nix
@@ -1,16 +1,10 @@
 {
   lib,
   pkgs,
-  nixosModules,
   ...
 }:
 
 {
-  imports = with nixosModules; [
-    # Require fonts
-    fonts
-  ];
-
   my.defaultApps.notes = lib.mkDefault "obsidian";
 
   environment.systemPackages = with pkgs; [
