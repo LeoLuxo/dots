052798fc69f5d1fea97e021b84b29b7c1e9a9aa8
Chloé G <contact@chlookie.net>
2025-10-24 18:53:11 +0200

    coffee 618 current  2025-10-24 18:53:09  25.05.20250705.29e2900  6.12.36
    D	_old/oldNewNixosModules/system/base.nix
    M	profiles/base.nix

--- profiles/base.nix
+++ profiles/base.nix
@@ -1,6 +1,22 @@
-{ profiles, ... }:
+{
+  profiles,
+  pkgs,
+  hostname,
+  ...
+}:
 {
   imports = [
     profiles.agenix
+  ];
+
+  # Set the hostname
+  networking.hostName = hostname;
+
+  # Essential system packages
+  environment.systemPackages = with pkgs; [
+    nano
+    wget
+    curl
+    git
   ];
 }
