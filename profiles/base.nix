ed264821f64b317c5c2be71c2cea9319707af3e3
Chloé G <contact@chlookie.net>
2025-10-24 19:39:50 +0200

    coffee 624 current  2025-10-24 19:39:06  25.05.20250705.29e2900  6.12.36
    M	_old/nixosConfigurations/pancake/configuration.nix
    M	_old/oldNewNixosModules/suites/pc.nix
    D	_old/oldNewNixosModules/system/locale.nix
    M	profiles/base.nix

--- profiles/base.nix
+++ profiles/base.nix
@@ -103,6 +103,25 @@ in
     --------------------------------------------------------------------------------
   */
 
+  # Set the time zone.
+  time.timeZone = "Europe/Copenhagen";
+
+  # Select internationalisation properties.
+  # A good choice would also be english-(Ireland); see:
+  # https://unix.stackexchange.com/questions/62316/why-is-there-no-euro-english-locale
+  i18n.defaultLocale = "en_DK.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_TIME = "en_IE.UTF-8";
+    # LC_ADDRESS = "en_IE.UTF-8";
+    # LC_IDENTIFICATION = "en_IE.UTF-8";
+    # LC_MEASUREMENT = "en_IE.UTF-8";
+    # LC_MONETARY = "en_IE.UTF-8";
+    # LC_NAME = "en_IE.UTF-8";
+    # LC_NUMERIC = "en_IE.UTF-8";
+    # LC_PAPER = "en_IE.UTF-8";
+    # LC_TELEPHONE = "en_IE.UTF-8";
+  };
+
   # Essential system packages
   environment.systemPackages = with pkgs; [
     nano
