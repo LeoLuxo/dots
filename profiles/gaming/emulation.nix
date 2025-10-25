19c556baafba7aeaf50985a2ff7a4a017ea0cb1d
Chloé G <contact@chlookie.net>
2025-10-25 09:27:02 +0200

    coffee 629 current  2025-10-25 09:20:29  25.05.20250705.29e2900  6.12.36
    M	_old/nixosConfigurations/coffee/configuration.nix
    M	_old/nixosConfigurations/pancake/configuration.nix
    D	_old/oldNewNixosModules/base/symlink.nix
    M	configs/coffee/default.nix
    A	profiles/gaming/emulation.nix

---
+++ profiles/gaming/emulation.nix
@@ -0,0 +1,46 @@
+{ lib2, ... }:
+
+let
+  inherit (lib2.hm) mkHomeSymlink;
+in
+{
+  hm = {
+    imports = [
+      (mkHomeSymlink {
+        xdgDir = "data";
+        target = "sudachi";
+        destination = "/stuff/games/emu/switch/yuzu";
+      })
+
+      (mkHomeSymlink {
+        xdgDir = "data";
+        target = "yuzu";
+        destination = "/stuff/games/emu/switch/yuzu";
+      })
+
+      (mkHomeSymlink {
+        xdgDir = "config";
+        target = "Ryujinx";
+        destination = "/stuff/games/emu/switch/ryujinx";
+      })
+
+      (mkHomeSymlink {
+        xdgDir = "data";
+        target = "Cemu";
+        destination = "/stuff/games/emu/wiiu/Cemu";
+      })
+
+      (mkHomeSymlink {
+        xdgDir = "config";
+        target = "steam-rom-manager/userData";
+        destination = "/stuff/games/emu/steamRomManager/userData";
+      })
+
+      (mkHomeSymlink {
+        xdgDir = "data";
+        target = "ukmm";
+        destination = "/stuff/games/emu/wiiu/ukmm";
+      })
+    ];
+  };
+}
