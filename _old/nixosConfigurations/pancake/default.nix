cae9ad545d8ad0c19d0b5522072deb26669bbb4f
LeoLuxo <contact@me.leoluxo.eu>
2025-11-09 09:05:50 +0100

    coffee 661 current  2025-10-29 21:28:07  25.05.20250705.29e2900  6.12.36
    M	_old/nixosConfigurations/coffee/configuration.nix
    M	_old/nixosConfigurations/coffee/default.nix
    M	_old/nixosConfigurations/pancake/default.nix
    D	_old/oldNewNixosModules/default.nix
    D	_old/oldNewNixosModules/suites/pc.nix
    D	_old/oldNewNixosModules/suites/pcDesktop.nix
    D	_old/oldNewNixosModules/suites/pcLaptop.nix
    D	_old/oldNewNixosModules/suites/server.nix
    M	sync/prismlauncher/settings.cfg

--- _old/nixosConfigurations/pancake/default.nix
+++ _old/nixosConfigurations/pancake/default.nix
@@ -1,15 +1,10 @@
 {
-  inputs,
-  lib,
-  user,
   ...
 }:
 {
   imports = [
     ./configuration.nix
 
-    # TODO: Remove
-    (import "${inputs.self}/_old/oldNewNixosModules" { inherit lib; }).default
 
   ];
 }
