baddb241fcd38f7c54f7785b31a71d435aba949a
Chloé G <contact@leoluxo.eu>
2025-11-13 18:32:08 +0100

    coffee 690 current  2025-11-13 18:32:05  25.05.20251110.c244830  6.12.36
    M	extraPkgs.nix
    M	flake.lock
    M	flake.nix
    M	hosts.nix
    M	modules/pinKernel.nix
    M	profiles/apps/vesktop/default.nix

--- extraPkgs.nix
+++ extraPkgs.nix
@@ -18,13 +18,15 @@ in
   # ```
   "stable" = mkPkgsOverlay inputs.nixpkgs-stable;
   "unstable" = mkPkgsOverlay inputs.nixpkgs-unstable;
+  "pinned" = mkPkgsOverlay inputs.nixpkgs-pinned;
   "25-05" = mkPkgsOverlay inputs.nixpkgs-25-05;
   "24-11" = mkPkgsOverlay inputs.nixpkgs-24-11;
+  "24-05" = mkPkgsOverlay inputs.nixpkgs-24-05;
 
   # The packages defined in ./packages
   "custom" = lib.packagesFromDirectoryRecursive {
     # Pin those to a specific pkgs toolchain
-    inherit (final."25-05") callPackage;
+    inherit (final."pinned") callPackage;
     directory = ./packages;
   };
 }
