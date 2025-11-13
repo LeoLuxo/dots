0ec36e955963b10330900b2da486dbb4a6d569d3
Chloé G <contact@chlookie.net>
2025-11-13 16:18:24 +0100

    coffee 686 current  2025-11-13 16:18:22  25.05.20250705.29e2900  6.12.36
    M	profiles/base.nix
    M	profiles/scripts/terminalUtils/default.nix

--- profiles/base.nix
+++ profiles/base.nix
@@ -37,86 +37,7 @@ in
     --------------------------------------------------------------------------------
   */
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  nix.settings = {
-    # Enable the new nix cli tool and flakes
-    experimental-features = [
-      "nix-command"
-      "flakes"
-    ];
-
-    # A list of names of users that have additional rights when connecting to the Nix daemon, such as the ability to specify additional binary caches, or to import unsigned NARs.
-    trusted-users =
-      [
-        "root"
-      ]
-      # Add all manually-defined users
-      ++ (lib.mapAttrsToList (username: _: username) users);
-  };
-
-  # Enable boot loading
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  /*
-    --------------------------------------------------------------------------------
-    ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-    --------------------------------------------------------------------------------
-  */
-
-  # Set the hostname
-  networking.hostName = hostname;
-
-  # Add all IPs defined in my hosts list to the networking hosts map
-  networking.hosts = lib.concatMapAttrs (
-    name: hostCfg:
-    lib.concatMapAttrs (_: ip: {
-      "${ip}" = [ name ];
-    }) (hostCfg.ip or { })
-  ) hosts;
-
-  services.openssh.enable = true;
-
-  # Define and set up all the manually-defined users
-  users = {
-    mutableUsers = false;
-
-    users = lib.concatMapAttrs (username: userCfg: {
-      ${username} = {
-        home = "/home/${username}";
-        description = username;
-        isNormalUser = true;
-
-        hashedPasswordFile = config.age.secrets."userpwds/${hostname}/${username}".path;
-        extraGroups = [ "wheel" ];
-
-        uid = userCfg.uid;
-
-        # Set default shell (can't be done in home-manager afaik)
-        shell = pkgs.${defaultShell};
-      };
-    }) users;
-  };
-
-  # # Install default shell
-  programs.${defaultShell}.enable = true;
-
-  # # Enable autologin if relevant
-  services.displayManager.autoLogin = lib.mkIf (autologin != null) {
-    enable = true;
-    user = autologin;
-  };
-
-  /*
-    --------------------------------------------------------------------------------
-    ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-    --------------------------------------------------------------------------------
-  */
+  # ---------- Customization ----------
 
   # Set the time zone.
   time.timeZone = "Europe/Copenhagen";
@@ -137,9 +58,6 @@ in
     # LC_TELEPHONE = "en_IE.UTF-8";
   };
 
-  # The keyboard mapping table for the virtual consoles
-  console.keyMap = "us";
-
   # Essential system packages
   environment.systemPackages = with pkgs; [
     nano
@@ -148,15 +66,10 @@ in
     git
   ];
 
+  # Set the default editor
   environment.variables = {
     EDITOR = "nano";
   };
-
-  /*
-    --------------------------------------------------------------------------------
-    ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-    --------------------------------------------------------------------------------
-  */
 
   # Set nix-monitored as the default nix package, which automatically integrates nix-output-monitor into all commands
   nix.monitored.enable = true;
@@ -167,7 +80,110 @@ in
     enableFishIntegration = true;
   };
 
-  # Home-Manager config
+  /*
+    --------------------------------------------------------------------------------
+    ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+    --------------------------------------------------------------------------------
+  */
+
+  # ---------- Nix settings ----------
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  nix.settings = {
+    # Enable the new nix cli tool and flakes
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+
+    # A list of names of users that have additional rights when connecting to the Nix daemon, such as the ability to specify additional binary caches, or to import unsigned NARs.
+    trusted-users =
+      [
+        "root"
+      ]
+      # Add all manually-defined users
+      ++ (lib.mapAttrsToList (username: _: username) users);
+  };
+
+  /*
+    --------------------------------------------------------------------------------
+    ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+    --------------------------------------------------------------------------------
+  */
+
+  # ---------- Host machine important settings ----------
+
+  # Enable boot loading
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Define and set up all the manually-defined users
+  users = {
+    mutableUsers = false;
+
+    users = lib.concatMapAttrs (username: userCfg: {
+      ${username} = {
+        home = "/home/${username}";
+        description = username;
+        isNormalUser = true;
+
+        hashedPasswordFile = config.age.secrets."userpwds/${hostname}/${username}".path;
+        extraGroups = [ "wheel" ];
+
+        uid = userCfg.uid;
+
+        # Set default shell (can't be done in home-manager)
+        shell = pkgs.${defaultShell};
+      };
+    }) users;
+  };
+
+  # Enable default shell
+  programs.${defaultShell}.enable = true;
+
+  # Enable autologin if relevant
+  services.displayManager.autoLogin = lib.mkIf (autologin != null) {
+    enable = true;
+    user = autologin;
+  };
+
+  # The keyboard mapping table for the virtual consoles
+  console.keyMap = "us";
+
+  /*
+    --------------------------------------------------------------------------------
+    ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+    --------------------------------------------------------------------------------
+  */
+
+  # ---------- Networking / SSH settings ----------
+
+  # Set the hostname
+  networking.hostName = hostname;
+
+  # Add all IPs defined in my hosts list to the networking hosts map
+  networking.hosts = lib.concatMapAttrs (
+    name: hostCfg:
+    lib.concatMapAttrs (_: ip: {
+      "${ip}" = [ name ];
+    }) (hostCfg.ip or { })
+  ) hosts;
+
+  services.openssh.enable = true;
+
+  /*
+    --------------------------------------------------------------------------------
+    ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+    --------------------------------------------------------------------------------
+  */
+
+  # ---------- Home-Manager config ----------
+
   home-manager = {
     useGlobalPkgs = true;
     useUserPackages = true;
