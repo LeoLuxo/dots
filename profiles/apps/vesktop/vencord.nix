9b72894aa694b71be30219c1157bcad5b3fb917a
LeoLuxo <contact@me.leoluxo.eu>
2025-11-28 21:13:04 +0100

    coffee 712 current  2025-11-28 21:13:01  25.05.20251110.c244830  6.12.36
    M	profiles/apps/vesktop/default.nix
    M	profiles/apps/vesktop/vencord.nix

--- profiles/apps/vesktop/vencord.nix
+++ profiles/apps/vesktop/vencord.nix
@@ -1,41 +1,57 @@
+# https://github.com/NixOS/nixpkgs/blob/c8828ddbdb209bfdbd8ded9269641cde1a2a9e97/pkgs/by-name/ve/vencord/package.nix
+
 {
   curl,
+  esbuild,
   fetchFromGitHub,
   git,
   jq,
   lib,
   nix-update,
   nodejs,
-  pnpm_10,
+  pnpm_9,
   stdenv,
   writeShellScript,
   buildWebExtension ? false,
 }:
-
 stdenv.mkDerivation (finalAttrs: {
   pname = "vencord";
-  version = "1.12.13";
+  version = "1.11.4";
 
   src = fetchFromGitHub {
     owner = "Vendicated";
     repo = "Vencord";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-mJPUt7+SjszRmscKr0HlW3X2yDwc9cCcJLz8rlg+gNA=";
+    hash = "sha256-7bFn3+mpiXC4+PGhoJ10QN1oBjj7zS5U2MJf8cJm114=";
   };
 
-  pnpmDeps = pnpm_10.fetchDeps {
+  pnpmDeps = pnpm_9.fetchDeps {
     inherit (finalAttrs) pname src;
-    fetcherVersion = 2;
-    hash = "sha256-JP9HOaP3DG+2F89tC77JZFD0ls35u/MzxNmvMCbBo9Y=";
+
+    hash = "sha256-ZUwtNtOmxjhOBpYB7vuytunGBRSuVxdlQsceRmeyhhI=";
   };
 
   nativeBuildInputs = [
     git
     nodejs
-    pnpm_10.configHook
+    pnpm_9.configHook
   ];
 
   env = {
+    ESBUILD_BINARY_PATH = lib.getExe (
+      esbuild.overrideAttrs (
+        final: _: {
+          version = "0.15.18";
+          src = fetchFromGitHub {
+            owner = "evanw";
+            repo = "esbuild";
+            rev = "v${final.version}";
+            hash = "sha256-b9R1ML+pgRg9j2yrkQmBulPuLHYLUQvW+WTyR/Cq6zE=";
+          };
+          vendorHash = "sha256-+BfxCyg0KkDQpHt/wycy/8CTG6YBA/VJvJFhhzUnSiQ=";
+        }
+      )
+    );
     VENCORD_REMOTE = "${finalAttrs.src.owner}/${finalAttrs.src.repo}";
     VENCORD_HASH = "${finalAttrs.version}";
   };
@@ -53,7 +69,6 @@ stdenv.mkDerivation (finalAttrs: {
     runHook preInstall
 
     cp -r dist/${lib.optionalString buildWebExtension "chromium-unpacked/"} $out
-    cp package.json $out # Presence is checked by Vesktop.
 
     runHook postInstall
   '';
@@ -76,16 +91,15 @@ stdenv.mkDerivation (finalAttrs: {
     exec nix-update --version "$latestTag" "$@"
   '';
 
-  meta = {
+  meta = with lib; {
     description = "Vencord web extension";
     homepage = "https://github.com/Vendicated/Vencord";
-    license = lib.licenses.gpl3Only;
-    maintainers = with lib.maintainers; [
-      FlameFlag
+    license = licenses.gpl3Only;
+    maintainers = with maintainers; [
       FlafyDev
-      Gliczy
       NotAShelf
       Scrumplex
+      donteatoreo
     ];
   };
 })
