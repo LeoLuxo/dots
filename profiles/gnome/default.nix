f6f65347dbc886e071bee3cfff1e85c9b7893394
LeoLuxo <contact@me.leoluxo.eu>
2025-12-01 19:43:46 +0100

    coffee
    M	profiles/apps/git/default.nix
    M	profiles/gnome/default.nix

--- profiles/gnome/default.nix
+++ profiles/gnome/default.nix
@@ -203,22 +203,21 @@ in
     systemd.services."getty@tty1".enable = false;
     systemd.services."autovt@tty1".enable = false;
 
-    # Enable the GNOME Desktop Environment.
-    services.xserver = {
-      displayManager.gdm = {
-        enable = true;
-        wayland = true;
-      };
+    # Enable the GNOME Desktop Environment
+    services.desktopManager.gnome = {
+      enable = true;
 
-      desktopManager.gnome = {
-        enable = true;
+      extraGSettingsOverridePackages = [ pkgs.mutter ];
+      extraGSettingsOverrides = ''
+        [org.gnome.mutter]
+        experimental-features=['variable-refresh-rate', 'scale-monitor-framebuffer']
+      '';
+    };
 
-        extraGSettingsOverridePackages = [ pkgs.mutter ];
-        extraGSettingsOverrides = ''
-          [org.gnome.mutter]
-          experimental-features=['variable-refresh-rate', 'scale-monitor-framebuffer']
-        '';
-      };
+    # Enable Gnome's display manager
+    services.displayManager.gdm = {
+      enable = true;
+      wayland = true;
     };
 
     # Kinda random fix for the weird stuff with my second monitor? Is supposed to leave some time for mutter to recognize all screens before initializing gnome
