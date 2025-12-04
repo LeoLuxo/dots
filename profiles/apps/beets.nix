570454565a1df99f504de1d194a67eb216e7243f
LeoLuxo <contact@me.leoluxo.eu>
2025-12-04 09:56:01 +0100

    coffee
    M	profiles/apps/beets.nix
    M	sync/beets/config.yaml

--- profiles/apps/beets.nix
+++ profiles/apps/beets.nix
@@ -27,21 +27,24 @@ in
     })
   ];
 
-  age.secrets."beets/acoustID-key" = {
-    owner = user;
-    group = "users";
-    mode = "400"; # read-only for owner
-  };
+  # age.secrets."beets/acoustID-key" = {
+  #   owner = user;
+  #   group = "users";
+  #   mode = "400"; # read-only for owner
+  # };
 
-  home-manager.users.${user} = {
-    home.file.".config/beets/keys.yaml" = {
-      text = ''
-        acoustid:
-          apikey: $(cat ${config.age.secrets."beets/acoustID-key".path})
-      '';
-      force = true;
-    };
-  };
+  # home-manager.users.${user} = {
+  #   home.file.".config/beets/keys.yaml" = {
+  #     text = ''
+  #       acoustid.apikey: $(cat ${config.age.secrets."beets/acoustID-key".path})
+  #     '';
+  #     force = true;
+  #   };
+  # };
+
+  system.activationScripts.beetsCopyKeys = ''
+    cp --force "${config.age.secrets."beets/keys-yaml".path}" "/home/${user}/.config/beets/keys.yaml"
+  '';
 
   environment.systemPackages = [
     (pkgs.writeScriptWithDeps {
