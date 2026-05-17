4d45867647cba1f7d649a9fb7296210fd4e3a75e
Chloé G <contact@chlookie.net>
2026-05-17 10:46:18 +0200

    pancake 52 25.11.20260128.fa83fd8 6.12.65
    M	configs/pancake/default.nix

--- configs/pancake/default.nix
+++ configs/pancake/default.nix
@@ -27,8 +27,8 @@
     inputs.winapps.packages.${pkgs.stdenv.hostPlatform.system}.winapps
     inputs.winapps.packages.${pkgs.stdenv.hostPlatform.system}.winapps-launcher
     pkgs.wineWow64Packages.stable
-    pkgs.wineWow64Packages.stagingFull
-
+    # pkgs.wineWow64Packages.stagingFull
+    pkgs.bottles
     pkgs.winetricks
     pkgs.samba
   ];
